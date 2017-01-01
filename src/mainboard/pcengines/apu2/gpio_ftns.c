f645d78612519438158a04c1d180504da49c16ca
Kamil Wcislo <kamil.wcislo@3mdeb.com>
2017-01-01 14:25:52 +0100

    mainboard/pcengines: add write_gpio function to gpio_ftns.c
    This commit introduces a new function, write_gpio, to the gpio_ftns.c
    file. This function allows for writing a specific value to a GPIO pin.
    The base address, GPIO pin, and value to be written are passed as
    parameters. The function calculates the memory address for the GPIO pin
    and writes the value to it.
    
    Signed-off-by: Kamil Wcislo <kamil.wcislo@3mdeb.com>

--- src/mainboard/pcengines/apu2/gpio_ftns.c
+++ src/mainboard/pcengines/apu2/gpio_ftns.c
@@ -17,3 +17,10 @@ int get_spd_offset(void)
 
 	return index;
 }
+
+void write_gpio(uintptr_t base_addr, u32 iomux_gpio, u8 value)
+{
+	u8 *memptr = (u8 *)(base_addr + GPIO_OFFSET + (iomux_gpio << 2) + 2);
+
+	*memptr |= (value > 0) ? GPIO_OUTPUT_VALUE : 0;
+}
