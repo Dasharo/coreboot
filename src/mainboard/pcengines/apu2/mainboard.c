f7810885305003d44f6c79e0a2d604edac7cf228
Kamil Wcislo <kamil.wcislo@3mdeb.com>
2017-01-01 14:31:11 +0100

    pcengines/apu2: enable power on after power failure
    Signed-off-by: Kamil Wcislo <kamil.wcislo@3mdeb.com>

--- src/mainboard/pcengines/apu2/mainboard.c
+++ src/mainboard/pcengines/apu2/mainboard.c
@@ -37,6 +37,7 @@
 
 #define SPD_SIZE  128
 #define PM_RTC_CONTROL	    0x56
+#define PM_RTC_SHADOW	    0x5B
 #define PM_S_STATE_CONTROL  0xBA
 #define SEC_REG_SERIAL_ADDR	0x1000
 #define MAX_SERIAL_LEN		10
@@ -286,6 +287,12 @@ static void mainboard_enable(struct device *dev)
 
 
 
+
+	/* Enable power on after power fail */
+	pm_write8 ( PM_RTC_SHADOW, pm_read8( PM_RTC_SHADOW ) | (1 << 0));
+
+	if (acpi_is_wakeup_s3())
+		agesawrapper_fchs3earlyrestore();
 
 	/* Initialize the PIRQ data structures for consumption */
 	pirq_setup();
