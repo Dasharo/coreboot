4e9c4531225a430bb3725952089256616987cfa5
Kamil Wcislo <mek.xgt@gmail.com>
2017-03-30 22:47:23 +0200

    pcengines/apu: add ehci0 enable/disable support
    Signed-off-by: Kamil Wcislo <mek.xgt@gmail.com>

--- src/mainboard/pcengines/apu2/mainboard.c
+++ src/mainboard/pcengines/apu2/mainboard.c
@@ -43,134 +43,13 @@
 #if CONFIG_USE_CBMEM_FILE_OVERRIDE
 #include <boot/cbmemfile.h>
 #endif //CONFIG_USE_CBMEM_FILE_OVERRIDE
-#include <cbfs_core.h>
 #include <spd_cache.h>
 #include <smbios.h>
 #include <string.h>
 #include <cpu/amd/amdfam16.h>
 #include <cpuRegisters.h>
 #include <build.h>
-
-#define BOOTORDER_FILE "bootorder"
-
-//
-// Helper functions
-//
-
-static char * findstr(const char *s, const char *pattern)
-{
-	char *result = (char *) s;
-	char *lpattern = (char *) pattern;
-
-	while (*result && *pattern ) {
-
-		if ( *lpattern == 0)	return result;		// the pattern matches return the pointer
-		if ( *result == 0) 		return NULL;		// We're at the end of the file content but don't have a patter match yet
-		if (*result == *lpattern ) {
-			result++;								// The string matches, simply advance
-			lpattern++;
-		} else {
-			result++;								// The string doesn't match restart the pattern
-			lpattern = (char *) pattern;
-		}
-	}
-
-	return NULL;
-}
-
-static u8 check_knob_value(const char *s)
-{
-	const char *boot_file = NULL;
-	size_t boot_file_len = 0;
-	char * token = NULL;
-
-	boot_file = cbfs_get_file_content(
-		CBFS_DEFAULT_MEDIA, BOOTORDER_FILE, CBFS_TYPE_RAW, &boot_file_len);
-	if (boot_file == NULL)
-		printk(BIOS_EMERG, "file [%s] not found in CBFS\n", BOOTORDER_FILE);
-	if (boot_file_len < 4096)
-		printk(BIOS_EMERG, "Missing bootorder data.\n");
-	if (boot_file == NULL || boot_file_len < 4096)
-		return -1;
-
-	token = findstr( boot_file, s );
-
-	if (token) {
-		if (*token == '0') return 0;
-		if (*token == '1') return 1;
-	}
-
-	return -1;
-}
-
-#if CONFIG_FORCE_CONSOLE
-#else //CONFIG_FORCE_CONSOLE
-
-static bool check_console(void)
-{
-	u8 scon;
-
-	//
-	// Find the serial console item
-	//
-	scon = check_knob_value("scon");
-
-	switch (scon) {
-	case 0:
-		return FALSE;
-		break;
-	case 1:
-		return TRUE;
-		break;
-	default:
-		printk(BIOS_EMERG, "Missing or invalid scon knob, enable console.\n");
-		break;
-	}
-
-	return TRUE;
-}
-#endif //CONFIG_FORCE_CONSOLE
-
-static bool check_uart(char uart_letter)
-{
-	u8 uarten;
-
-	switch (uart_letter) {
-	case 'c':
-		uarten = check_knob_value("uartc");
-		break;
-	case 'd':
-		uarten = check_knob_value("uartd");
-		break;
-	default:
-		uarten = -1;
-		break;
-	}
-
-	switch (uarten) {
-	case 0:
-		return FALSE;
-		break;
-	case 1:
-		return TRUE;
-		break;
-	default:
-		printk(BIOS_EMERG, "Missing or invalid uart knob, disable port.\n");
-		break;
-	}
-
-	return FALSE;
-}
-
-static inline bool check_uartc( void )
-{
-	return check_uart('c');
-}
-
-static inline bool check_uartd( void )
-{
-	return check_uart('d');
-}
+#include "bios_knobs.h"
 
 /**********************************************
  * enable the dedicated function in mainboard.
