1b13aa6a39c01deccfc37bdcaef51fa594234bd0
Michał Żygowski <michal.zygowski@3mdeb.com>
2017-10-26 09:22:53 +0200

    pcengines/apu2: add runtime config for console, ehci, mPCIe2 and uartc/d

---
+++ src/mainboard/pcengines/apu2/bios_knobs.c
@@ -0,0 +1,348 @@
+/*
+ * This file is part of the coreboot project.
+
+ *
+ * Copyright (C) 2017 3mdeb
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+#include <stdint.h>
+#include <console/console.h>
+#include <program_loading.h>
+#include <cbfs.h>
+#include <commonlib/cbfs.h>
+#include <commonlib/region.h>
+#include <commonlib/cbfs_serialized.h>
+#include "bios_knobs.h"
+
+#define BOOTORDER_FILE "bootorder"
+
+static char * findstr(const char *s, const char *pattern)
+{
+	char *result = (char *) s;
+	char *lpattern = (char *) pattern;
+
+	while (*result && *pattern ) {
+		if ( *lpattern == 0)  // the pattern matches return the pointer
+			return result;
+		if ( *result == 0)  // We're at the end of the file content but don't have a patter match yet
+			return NULL;
+		if (*result == *lpattern ) {
+			// The string matches, simply advance
+			result++;
+			lpattern++;
+		} else {
+			// The string doesn't match restart the pattern
+			result++;
+			lpattern = (char *) pattern;
+		}
+	}
+
+	return NULL;
+}
+
+static u8 check_knob_value(const char *s)
+{
+	const char *boot_file = NULL;
+	size_t boot_file_len = 0;
+	char * token = NULL;
+
+	//
+	// This function locates a file in cbfs, maps it to memory and returns
+	// a void* pointer
+	//
+	boot_file = cbfs_boot_map_with_leak(BOOTORDER_FILE, CBFS_TYPE_RAW, &boot_file_len);
+	if (boot_file == NULL)
+		printk(BIOS_ALERT, "file [%s] not found in CBFS\n", BOOTORDER_FILE);
+	if (boot_file_len < 4096)
+		printk(BIOS_ALERT, "Missing bootorder data.\n");
+	if (boot_file == NULL || boot_file_len < 4096)
+		return -1;
+
+	token = findstr( boot_file, s );
+
+	if (token) {
+		if (*token == '0') return 0;
+		if (*token == '1') return 1;
+	}
+
+	return -1;
+}
+
+bool check_console(void)
+{
+
+}
+
+static u8 check_uart(char uart_letter)
+{
+	u8 uarten;
+
+	switch (uart_letter) {
+	case 'c':
+		uarten = check_knob_value("uartc");
+		break;
+	case 'd':
+		uarten = check_knob_value("uartd");
+		break;
+	default:
+		uarten = -1;
+		break;
+	}
+
+	switch (uarten) {
+	case 0:
+		return 0;
+		break;
+	case 1:
+		return 1;
+		break;
+	default:
+		printk(BIOS_EMERG, "Missing or invalid uart knob, disable port.\n");
+		break;
+	}
+
+	return 0;
+}
+
+inline u8 check_uartc(void)
+{
+	return check_uart('c');
+}
+
+inline u8 check_uartd(void)
+{
+	return check_uart('d');
+}
+
+u8 check_ehci0(void)
+{
+	u8 ehci0;
+
+	//
+	// Find the EHCI0 item
+	//
+	ehci0 = check_knob_value("ehcien");
+
+	switch (ehci0) {
+	case 0:
+		return 0;
+		break;
+	case 1:
+		return 1;
+		break;
+	default:
+		printk(BIOS_INFO,
+			"Missing or invalid ehci0 knob, enable ehci0.\n");
+		break;
+	}
+
+	return 1;
+}
+
+u8 check_mpcie2_clk(void)
+{
+	u8 mpcie2_clk;
+
+	//
+	// Find the mPCIe2 clock item
+	//
+	mpcie2_clk = check_knob_value("mpcie2_clk");
+
+	switch (mpcie2_clk) {
+	case 0:
+		return 0;
+		break;
+	case 1:
+		return 1;
+		break;
+	default:
+		printk(BIOS_INFO, "Missing or invalid mpcie2_clk knob, forcing"
+			" CLK of mPCIe2 slot is not enabled.\n");
+		break;
+	}
+
+	return 0;
+}
+
+u8 check_pciepm(void)
+{
+	u8 pciepm;
+
+	//
+	// Find the mPCIe2 clock item
+	//
+	pciepm = check_knob_value("pciepm");
+
+	switch (pciepm) {
+	case 0:
+		return 0;
+		break;
+	case 1:
+		return 1;
+		break;
+	default:
+		printk(BIOS_INFO, "Missing or invalid pciepm knob, disabling"
+			"PCIe power management features.\n");
+		break;
+	}
+
+	return 0;
+}
+
+u8 check_pciereverse(void)
+{
+	u8 pciereverse;
+	pciereverse = check_knob_value("pciereverse");
+
+	switch (pciereverse) {
+	case 0:
+		return 0;
+		break;
+	case 1:
+		return 1;
+		break;
+	default:
+		printk(BIOS_INFO, "Missing or invalid pciereverse knob, "
+			"PCIe order remains unchanged.\n");
+		break;
+	}
+
+	return 0;
+}
+
+u8 check_sd3_mode(void)
+{
+	u8 sd3mode;
+
+	//
+	// Find the SD 3.0 mode item
+	//
+	sd3mode = check_knob_value("sd3mode");
+
+	switch (sd3mode) {
+	case 0:
+		return 0;
+		break;
+	case 1:
+		return 1;
+		break;
+	default:
+		printk(BIOS_INFO, "Missing or invalid sd3mode knob."
+				  " Disable SD3.0 mode.\n");
+		break;
+	}
+
+	return 0;
+}
+
+static int _valid(char ch, int base)
+{
+	char end = (base > 9) ? '9' : '0' + (base - 1);
+
+	/* all bases will be some subset of the 0-9 range */
+
+	if (ch >= '0' && ch <= end)
+		return 1;
+
+	/* Bases > 11 will also have to match in the a-z range */
+
+	if (base > 11) {
+		if (tolower(ch) >= 'a' &&
+		    tolower(ch) <= 'a' + (base - 11))
+			return 1;
+	}
+
+	return 0;
+}
+
+/* Return the "value" of the character in the given base */
+
+static int _offset(char ch, int base)
+{
+	if (ch >= '0' && ch <= '9')
+		return ch - '0';
+	else
+		return 10 + tolower(ch) - 'a';
+}
+
+
+static unsigned long long int strtoull(const char *ptr, char **endptr, int base)
+{
+	unsigned long long int ret = 0;
+
+	if (endptr != NULL)
+		*endptr = (char *) ptr;
+
+	/* Purge whitespace */
+
+	for( ; *ptr && isspace(*ptr); ptr++);
+
+	if (!*ptr)
+		return 0;
+
+	/* Determine the base */
+
+	if (base == 0) {
+		if (ptr[0] == '0' && (ptr[1] == 'x' || ptr[1] == 'X'))
+			base = 16;
+		else if (ptr[0] == '0') {
+			base = 8;
+			ptr++;
+		}
+		else
+			base = 10;
+	}
+
+	/* Base 16 allows the 0x on front - so skip over it */
+
+	if (base == 16) {
+		if (ptr[0] == '0' && (ptr[1] == 'x' || ptr[1] == 'X') &&
+		    _valid(ptr[2], base))
+			ptr += 2;
+	}
+
+	for( ; *ptr && _valid(*ptr, base); ptr++)
+		ret = (ret * base) + _offset(*ptr, base);
+
+	if (endptr != NULL)
+		*endptr = (char *) ptr;
+
+	return ret;
+}
+
+static unsigned long int strtoul(const char *ptr, char **endptr, int base)
+{
+	unsigned long long val = strtoull(ptr, endptr, base);
+	if (val > ULONG_MAX) return ULONG_MAX;
+	return val;
+}
+
+u16 get_watchdog_timeout(void)
+{
+	u16 timeout;
+
+	if (!bootorder_file && !get_bootorder())
+		return -1;
+
+	timeout = (u16) strtoul(findstr(bootorder_file, "watchdog"), NULL, 16);
+
+	if (timeout > 0 && timeout < 60) {
+		printk(BIOS_WARNING, "Watchdog timeout set too low, "
+			"increasing to 60 seconds\n");
+		timeout = 60;
+	}
+
+	return timeout;
+}
