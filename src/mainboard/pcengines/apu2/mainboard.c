1ee0da76f5720b2583f45f9f6cef8b04ceeda364
Michał Żygowski <michal.zygowski@3mdeb.com>
2018-03-27 14:36:30 +0200

    mainboard/pcengines: modify scon knob and save it to flash when S1 pressed
    On apu5 the S1 button is not present. Its GPIO is used as a
    SIMSWAP.
    
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/mainboard/pcengines/apu2/mainboard.c
+++ src/mainboard/pcengines/apu2/mainboard.c
@@ -196,6 +196,69 @@ static void config_gpio_mux(void)
 	}
 }
 
+static size_t get_bootorder_cbfs_offset(const char *name, uint32_t type)
+{
+	struct region_device rdev;
+	const struct region_device *boot_dev;
+	struct cbfs_props props;
+	struct cbfsf fh;
+
+	boot_dev = boot_device_ro();
+
+	if (boot_dev == NULL) {
+		printk(BIOS_WARNING, "Can't init CBFS boot device\n");
+		return 0;
+	}
+
+	if (cbfs_boot_region_properties(&props)) {
+		printk(BIOS_WARNING, "Can't locate CBFS\n");
+		return 0;
+	}
+
+	if (rdev_chain(&rdev, boot_dev, props.offset, props.size)) {
+		printk(BIOS_WARNING, "Rdev chain failed\n");
+		return 0;
+	}
+
+	/* locate bootorder file and calculate its offset */
+
+	if (cbfs_locate(&fh, &rdev, name, &type)) {
+		printk(BIOS_WARNING, "Can't locate file in CBFS\n");
+		return 0;
+	}
+
+	return (size_t) rdev_relative_offset(boot_dev, &fh.data);
+}
+
+static int find_knob_index(const char *s, const char *pattern)
+{
+
+	int pattern_index = 0;
+	char *result = (char *) s;
+	char *lpattern = (char *) pattern;
+
+	while (*result && *pattern ) {
+		if ( *lpattern == 0)  // the pattern matches return the pointer
+			return pattern_index;
+		if ( *result == 0)  // We're at the end of the file content but don't have a patter match yet
+			return -1;
+		if (*result == *lpattern ) {
+			// The string matches, simply advance
+			result++;
+			pattern_index++;
+			lpattern++;
+		} else {
+			// The string doesn't match restart the pattern
+			result++;
+			pattern_index++;
+			lpattern = (char *) pattern;
+		}
+	}
+
+	return -1;
+
+}
+
 /**********************************************
  * enable the dedicated function in mainboard.
  **********************************************/
@@ -355,6 +418,7 @@ static void mainboard_final(void *chip_info)
 	write_gpio(GPIO_58, 1);
 	write_gpio(GPIO_59, 1);
 
+#if CONFIG_BOARD_PCENGINES_APU2 || CONFIG_BOARD_PCENGINES_APU3 || CONFIG_BOARD_PCENGINES_APU4
 	if (!check_console()) {
 
 		//
@@ -362,76 +426,77 @@ static void mainboard_final(void *chip_info)
 		//
 		if (!read_gpio(GPIO_32)) {
 
-			const struct spi_flash *flash = NULL;;
-			struct region_device rdev;
-			const struct region_device *boot_dev;
-			struct cbfs_props props;
-			struct cbfsf fh;
-			size_t fsize;
-			uint32_t ftype = CBFS_TYPE_RAW;
-
 			printk(BIOS_INFO, "S1 PRESSED\n");
 
+			const struct spi_flash *flash = NULL;
+			size_t fsize, offset;
+			char* bootorder_file = NULL;
+			int knob_index;
+			char *bootorder_copy;
+
+			bootorder_file = cbfs_boot_map_with_leak("bootorder", CBFS_TYPE_RAW, &fsize);
+
+			if (bootorder_file == NULL){
+				printk(BIOS_WARNING, "Could not mmap bootorder\n");
+				return;
+			}
+
+			if (fsize & 0xFFF) {
+				printk(BIOS_WARNING,"The bootorder file is not 4k aligned\n");
+				return;
+			}
+
+			offset = get_bootorder_cbfs_offset("bootorder", CBFS_TYPE_RAW);
+
+			bootorder_copy = (char *)malloc(fsize);
+
+			if(bootorder_copy == NULL) {
+				printk(BIOS_WARNING,"Failed to allocate memory for bootorder\n");
+				return;
+			}
+
+			if(memcpy(bootorder_copy, bootorder_file, fsize) == NULL) {
+				printk(BIOS_WARNING,"Copying bootorder failed\n");
+				free(bootorder_copy);
+				return;
+			}
+
+			knob_index = find_knob_index(bootorder_copy, "scon");
+
+			if(knob_index == -1){
+				printk(BIOS_WARNING,"scon knob not found in bootorder\n");
+				free(bootorder_copy);
+				return;
+			}
+
+			*(bootorder_copy + knob_index) = '1';
+
 			flash = boot_device_spi_flash();
+
 			if (flash == NULL) {
 				printk(BIOS_WARNING, "Can't get boot flash device\n");
+				free(bootorder_copy);
+				return;
 			}
 
-			if (cbfs_boot_region_properties(&props))
-				printk(BIOS_WARNING, "Can't locate CBFS\n");
-
-			/* All boot CBFS operations are performed using the RO devie. */
-			boot_dev = boot_device_ro();
-
-			if (boot_dev == NULL)
-				printk(BIOS_WARNING, "Can't init CBFS boot device\n");
-
-			if (rdev_chain(&rdev, boot_dev, props.offset, props.size))
-				printk(BIOS_WARNING, "Rdev chain failed\n");
-
-			if (cbfs_locate(&fh, &rdev, BOOTORDER_FILE, &ftype))
-				printk(BIOS_WARNING, "Can't locate file in CBFS\n");
-
-			fsize = region_device_sz(&fh.data);
-
-			printk(BIOS_WARNING, "Bootorder fsize: %lx, region_offset: %lx, region.size %lx\n",
-			 				fsize, rdev.region.offset, rdev.region.size );
-
-			char* bootorder_file = NULL;
-			spi_flash_read(flash, rdev.region.offset, fsize, bootorder_file);
-			if(bootorder_file != NULL)
-				printk(BIOS_WARNING, "%s\n", bootorder_file);
-			else
-				printk(BIOS_WARNING, "Could not read bootorder from flash\n");
-
-			const char* pattern = "scon";
-			char *result = bootorder_file;
-			char *lpattern = (char *) pattern;
-
-			while (*result && *pattern ) {
-				if ( *lpattern == 0)  // the pattern matches return the pointer
-					break;
-				if ( *result == 0) { // We're at the end of the file content but don't have a patter match yet
-					result = NULL;
-					break;
-				}
-				if (*result == *lpattern ) {
-					// The string matches, simply advance
-					result++;
-					lpattern++;
-				} else {
-					// The string doesn't match restart the pattern
-					result++;
-					lpattern = (char *) pattern;
-				}
+			if (spi_flash_erase(flash, (u32) offset, fsize)) {
+				printk(BIOS_WARNING, "SPI erase failed\n");
+				free(bootorder_copy);
+				return;
 			}
 
-			printk(BIOS_WARNING, "%s %d\n", pattern, (unsigned int) *result );
+			if (spi_flash_write(flash, offset, fsize, bootorder_copy)) {
+				printk(BIOS_WARNING, "SPI write failed\n");
+				free(bootorder_copy);
+				return;
+			} else {
+				printk(BIOS_INFO, "Bootorder write successed\n");
+			}
 
-
-
+			free(bootorder_copy);
 		}
 	}
+#endif
 }
 
 /*
