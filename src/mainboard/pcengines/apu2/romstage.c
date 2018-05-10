fb451d043b4aba1a2f9fbcf2b047561f0c836d33
Michał Żygowski <michal.zygowski@3mdeb.com>
2018-05-11 00:29:48 +0200

    src/mainboard/pcengines/apu2/romstage.c: try to update microcode by default
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/mainboard/pcengines/apu2/romstage.c
+++ src/mainboard/pcengines/apu2/romstage.c
@@ -180,9 +180,8 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 	printk(BIOS_DEBUG, "BSP Family_Model: %08x \n", val);
 	printk(BIOS_DEBUG, "cpu_init_detectedx = %08lx \n", cpu_init_detectedx);
 
-#if CONFIG_CPU_AMD_FAM16H_MICROCODE
 	update_microcode(val);
-#endif
+
 	/*
 	 * This refers to LpcClkDrvSth settling time.  Without this setting, processor
 	 * initialization is slow or incorrect, so this wait has been replicated from
