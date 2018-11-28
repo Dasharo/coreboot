2155e254f156d112e74d0504eb9f9f5ee98188d6
Michał Żygowski <michal.zygowski@3mdeb.com>
2018-11-28 14:33:56 +0100

    src/drivers/uart/uart8250io.c: make uart init runtime configurable
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/drivers/uart/uart8250io.c
+++ src/drivers/uart/uart8250io.c
@@ -20,10 +20,12 @@
 
 #include <rules.h>
 #include <stdlib.h>
+#include <arch/early_variables.h>
 #include <arch/io.h>
 #include <console/uart.h>
 #include <trace.h>
 #include "uart8250reg.h"
+#include "mainboard/pcengines/apu2/bios_knobs.h"
 
 #ifndef __ROMCC__
 #include <boot/coreboot_tables.h>
@@ -45,6 +47,8 @@
  */
 #define SINGLE_CHAR_TIMEOUT	(50 * 1000)
 #define FIFO_TIMEOUT		(16 * SINGLE_CHAR_TIMEOUT)
+
+static int port_index CAR_GLOBAL;
 
 static int uart8250_can_tx_byte(unsigned base_port)
 {
@@ -117,22 +121,28 @@ void uart_init(int idx)
 	unsigned int div;
 	div = uart_baudrate_divisor(default_baudrate(), BAUDRATE_REFCLK,
 		BAUDRATE_OVERSAMPLE);
-	uart8250_init(uart_platform_base(idx), div);
+
+	if ((check_com2() || idx == 1) &&
+	    !IS_ENABLED(CONFIG_BOARD_PCENGINES_APU5))
+		car_set_var(port_index, 1);
+	else
+		car_set_var(port_index, idx);
+	uart8250_init(uart_platform_base(car_get_var(port_index)), div);
 }
 
 void uart_tx_byte(int idx, unsigned char data)
 {
-	uart8250_tx_byte(uart_platform_base(idx), data);
+	uart8250_tx_byte(uart_platform_base(car_get_var(port_index)), data);
 }
 
 unsigned char uart_rx_byte(int idx)
 {
-	return uart8250_rx_byte(uart_platform_base(idx));
+	return uart8250_rx_byte(uart_platform_base(car_get_var(port_index)));
 }
 
 void uart_tx_flush(int idx)
 {
-	uart8250_tx_flush(uart_platform_base(idx));
+	uart8250_tx_flush(uart_platform_base(car_get_var(port_index)));
 }
 
 #if ENV_RAMSTAGE
@@ -140,7 +150,7 @@ void uart_fill_lb(void *data)
 {
 	struct lb_serial serial;
 	serial.type = LB_SERIAL_TYPE_IO_MAPPED;
-	serial.baseaddr = uart_platform_base(CONFIG_UART_FOR_CONSOLE);
+	serial.baseaddr = uart_platform_base(car_get_var(port_index));
 	serial.baud = default_baudrate();
 	lb_add_serial(&serial, data);
 
