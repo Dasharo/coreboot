a4f6704b939e4cfc5d296b08d7d8b044369a0cd4
Michał Żygowski <michal.zygowski@3mdeb.com>
2019-02-04 18:13:04 +0100

    (broken squash ) configs to v4.9.0.5 + ECC
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/mainboard/pcengines/apu2/mainboard.c
+++ src/mainboard/pcengines/apu2/mainboard.c
@@ -29,6 +29,7 @@
 #include <southbridge/amd/pi/hudson/chip.h>
 #include <cpu/amd/mtrr.h>
 #include <spd_bin.h>
+#include <memory_info.h>
 #include <spi_flash.h>
 
 #include "gpio_ftns.h"
@@ -194,6 +195,88 @@ static void config_gpio_mux(void)
 		if (gpio)
 			gpio->enabled = 1;
 	}
+}
+
+static void set_dimm_info(uint8_t *spd, struct dimm_info *dimm)
+{
+	const int spd_capmb[8] = {  1,  2,  4,  8, 16, 32, 64,  0 };
+	const int spd_ranks[8] = {  1,  2,  3,  4, -1, -1, -1, -1 };
+	const int spd_devw[8]  = {  4,  8, 16, 32, -1, -1, -1, -1 };
+	const int spd_busw[8]  = {  8, 16, 32, 64, -1, -1, -1, -1 };
+
+	int capmb = spd_capmb[spd[SPD_DENSITY_BANKS] & 7] * 256;
+	int ranks = spd_ranks[(spd[SPD_ORGANIZATION] >> 3) & 7];
+	int devw  = spd_devw[spd[SPD_ORGANIZATION] & 7];
+	int busw  = spd_busw[spd[SPD_BUS_DEV_WIDTH] & 7];
+
+	switch (spd[12]) {
+		case 0x0a:
+			dimm->ddr_frequency = 1600;
+			break;
+		case 0x0c:
+			dimm->ddr_frequency = 1333;
+			break;
+		default:
+			dimm->ddr_frequency = 0;
+			break;
+	}
+
+	dimm->ddr_type = MEMORY_TYPE_DDR3;
+
+	/* Parse the SPD data to determine the DIMM information */
+
+	dimm->dimm_size = capmb / 8 * busw / devw * ranks;  /* MiB */
+	dimm->mod_type = spd[3] & 0xf;
+	strncpy((char *)&dimm->module_part_number[0], (char *)&spd[0x80],
+		LPDDR3_SPD_PART_LEN);
+	dimm->module_part_number[LPDDR3_SPD_PART_LEN] = 0;
+	dimm->mod_id = *(uint16_t *)&spd[117];
+
+	switch (busw) {
+	default:
+	case 8:
+		dimm->bus_width = MEMORY_BUS_WIDTH_8;
+		break;
+
+	case 16:
+		dimm->bus_width = MEMORY_BUS_WIDTH_16;
+		break;
+
+	case 32:
+		dimm->bus_width = MEMORY_BUS_WIDTH_32;
+		break;
+
+	case 64:
+		dimm->bus_width = MEMORY_BUS_WIDTH_64;
+		break;
+	}
+}
+
+static void mainboard_get_dimm_info(void)
+{
+	struct dimm_info *dimm;
+	struct memory_info *mem_info;
+
+	/*
+	 * Allocate CBMEM area for DIMM information used to populate SMBIOS
+	 * table 17
+	 */
+	mem_info = cbmem_add(CBMEM_ID_MEMINFO, sizeof(*mem_info));
+	printk(BIOS_DEBUG, "CBMEM entry for DIMM info: 0x%p\n", mem_info);
+	if (mem_info == NULL)
+		return;
+	memset(mem_info, 0, sizeof(*mem_info));
+
+	u8 spd_index = get_spd_offset();
+	u8 spd_buffer[CONFIG_DIMM_SPD_SIZE];
+	if (read_ddr3_spd_from_cbfs(spd_buffer, spd_index) < 0) {
+		return;
+	}
+
+	/* Describe the first channel memory */
+	dimm = &mem_info->dimm[0];
+	set_dimm_info(spd_buffer, dimm);
+	mem_info->dimm_cnt = 1;
 }
 
 /**********************************************
@@ -517,6 +600,43 @@ const char *smbios_system_sku(void)
 	else
 		snprintf(sku, sizeof(sku), "4 GB");
 	return sku;
+}
+
+int fill_mainboard_smbios_type16(unsigned long *current, int *handle)
+{
+	u8 spd_index = get_spd_offset();
+	u8 spd_buffer[CONFIG_DIMM_SPD_SIZE];
+	if (read_ddr3_spd_from_cbfs(spd_buffer, spd_index) < 0) {
+		return 0;
+	}
+
+	struct smbios_type16 *t = (struct smbios_type16 *)*current;
+	int len = sizeof(struct smbios_type16) - 2;
+	memset(t, 0, sizeof(struct smbios_type16));
+
+	t->handle = *handle;
+	t->length = len;
+	t->type = SMBIOS_PHYS_MEMORY_ARRAY;
+	t->use = MEMORY_ARRAY_USE_SYSTEM;
+	t->location = MEMORY_ARRAY_LOCATION_SYSTEM_BOARD;
+	t->maximum_capacity = 4 * 1024 * 1024; // 4GB (in kB) due to board design
+	t->extended_maximum_capacity = 0;
+	t->memory_error_information_handle = 0xFFFE;
+	t->number_of_memory_devices = 1; // only 1 device soldered down to 1 channel
+
+	switch(spd_buffer[3]){
+		case 0x08:
+			t->memory_error_correction = MEMORY_ARRAY_ECC_MULTI_BIT;
+			break;
+		case 0x03:
+			t->memory_error_correction = MEMORY_ARRAY_ECC_NONE;
+			break;
+		default:
+			t->memory_error_correction = MEMORY_ARRAY_ECC_UNKNOWN;
+			break;
+	}
+	len += smbios_string_table_len(t->eos);
+	return len;
 }
 
 struct chip_operations mainboard_ops = {
