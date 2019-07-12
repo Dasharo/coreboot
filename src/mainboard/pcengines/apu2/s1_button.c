21b5cd83e53cbf8bef6733a65e1ce77720b27d8f
Michał Żygowski <michal.zygowski@3mdeb.com>
2019-07-12 17:08:56 +0200

    src/mainboard/pcengines/apu2/s1_button.c: rewrite S1 button functions
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/mainboard/pcengines/apu2/s1_button.c
+++ src/mainboard/pcengines/apu2/s1_button.c
@@ -23,6 +23,7 @@
 #include <spi-generic.h>
 #include <commonlib/region.h>
 #include <console/console.h>
+#include <drivers/vpd/lib_vpd.h>
 #include <drivers/vpd/vpd.h>
 #include <drivers/vpd/vpd_tables.h>
 
@@ -39,28 +40,95 @@ struct cbmem_vpd {
 	 */
 };
 
-static int flash_vpd(size_t offset, size_t fsize, char *buffer)
+
+static int find_scon_offset(void *vpd_buffer, struct google_vpd_info *info,
+			    uint32_t *scon_offset)
 {
-	const struct spi_flash *flash;
+	uint32_t offset = 0x600 + sizeof(struct google_vpd_info);
+	uint8_t type;
+	int32_t key_len, value_len, decoded_len;
+	const uint8_t *key, *value;
+	uint32_t consumed = 0;
+	uint32_t strings_offset = offset;
 
-	flash = boot_device_spi_flash();
+	while (consumed < info->size) {
+		offset = strings_offset + consumed;
+		type = *(uint8_t *)(vpd_buffer + offset);
+		if (type == 0) {
+			printk(BIOS_WARNING, "VPD is empty or end of VPD\n");
+			break;
+		} else if (type == 1) {
+			consumed++;
+			if (decodeLen(info->size - consumed,
+				       vpd_buffer + strings_offset + consumed,
+				       &key_len, &decoded_len)) {
+				if (consumed + decoded_len >= info->size) {
+					printk(BIOS_WARNING, "String too long\n");
+					return -1;
+				}
+			}
+			consumed += decoded_len;
+			key = (uint8_t *)(vpd_buffer + strings_offset +
+					  consumed);
+			consumed += key_len;
+			if (decodeLen(info->size - consumed,
+				       vpd_buffer + strings_offset + consumed,
+				       &value_len, &decoded_len)) {
+				if (consumed + decoded_len >= info->size) {
+					printk(BIOS_WARNING, "Value too long\n");
+					return -1;
+				}
+			}
+			consumed += decoded_len;
+			value = (uint8_t *)(vpd_buffer + strings_offset +
+					    consumed);
+			consumed += value_len;
 
-	if (flash == NULL) {
-		printk(BIOS_WARNING, "Can't get boot flash device\n");
-		return -1;
+			if (!memcmp("scon", key, strlen("scon")) &&
+			    strlen("scon") == key_len)
+				break;
+			else
+				continue;
+		} else {
+			printk(BIOS_DEBUG, "Unknown entry %x\n", type);
+			return -1;
+		}
 	}
+	*scon_offset = offset;
+	return consumed;
+}
 
-	if (spi_flash_erase(flash, (u32) offset, fsize)) {
-		printk(BIOS_WARNING, "SPI erase failed\n");
+static int replace_scon_tag(void *vpd_buffer, void *output_buffer,
+			     struct google_vpd_info *info,
+			     struct region vpd_region)
+{
+	uint32_t strings_offset, offset = 0; 
+	int consumed;
+	consumed = find_scon_offset(vpd_buffer, info, &offset);
+	if (consumed == -1)
 		return -1;
-	}
 
-	if (spi_flash_write(flash, offset, fsize, buffer)) {
-		printk(BIOS_WARNING, "SPI write failed\n");
-		return -1;
-	}
+	strings_offset = 0x600 + sizeof(struct google_vpd_info);
+	char scon_entry[2 + strlen("scon") + 1 + strlen("enabled")];
+	scon_entry[0] = 0x01;
+	scon_entry[1] = strlen("scon");
+	memcpy(&scon_entry[2], "scon", strlen("scon"));
+	scon_entry[2 + strlen("scon")] = strlen("enabled");
+	memcpy(&scon_entry[3 + strlen("scon")], "enabled", strlen("enabled"));
 
-	printk(BIOS_INFO, "VPD write successed\n");
+	if (*(char *)(vpd_buffer + offset) == 0x00) {
+		memcpy(output_buffer + offset, scon_entry,
+		       sizeof(scon_entry));
+		*(char *)(output_buffer + offset + sizeof(scon_entry)) = 0;
+	} else {
+		memset(output_buffer + offset, 0xFF,
+		       vpd_region.size - offset);
+		memcpy(output_buffer + offset, scon_entry,
+		       sizeof(scon_entry));
+		strcpy(output_buffer + offset + sizeof(scon_entry),
+		       vpd_buffer + offset + consumed);
+	}
+	*(uint32_t *)(output_buffer + 0x600 + 12) = info->size - 1;
 	return 0;
 }
 
@@ -70,6 +138,7 @@ void enable_console(void)
 	struct region vpd_region;
 	void *vpd_buffer, *output_buffer;
 	struct google_vpd_info info;
+	struct cbmem_vpd *vpd;
 
 	if (!vpd_find("scon", &tag_size, VPD_RW)) {
 		printk(BIOS_WARNING, "scon tag not found in RW_VPD\n");
@@ -81,13 +150,16 @@ void enable_console(void)
 		return;
 	}
 
-	free(vpd_buffer);
 	vpd_buffer = malloc(vpd_region.size);
 	output_buffer = malloc(vpd_region.size);
 	if (!vpd_buffer || !output_buffer) {
 		printk(BIOS_WARNING, "Could not allocate memory\n");
 		return;
 	}
+
+	vpd = (struct cbmem_vpd *)cbmem_find(CBMEM_ID_VPD);
+	if (!vpd || !vpd->ro_size || !vpd->rw_size)
+		return;
 
 	fmap_read_area("RW_VPD", vpd_buffer, vpd_region.size);
 	memcpy(output_buffer, vpd_buffer, vpd_region.size);
@@ -97,73 +169,29 @@ void enable_console(void)
 	           sizeof(info.header.magic))) {
 		printk(BIOS_WARNING, "Bad VPD info header magic\n");
 		free(vpd_buffer);
+		free(output_buffer);
 		return;
 	}
 
-	uint32_t offset = 0x600 + sizeof(struct google_vpd_info);
-	uint8_t type, key_len, value_len;
-
-	struct cbmem_vpd *vpd;
-
-	vpd = (struct cbmem_vpd *)cbmem_find(CBMEM_ID_VPD);
-	if (!vpd || !vpd->ro_size)
+	if (replace_scon_tag(vpd_buffer, output_buffer, &info,
+			     vpd_region) == -1) {
+		printk(BIOS_DEBUG, "Replacing scon tag failed\n");
+		free(vpd_buffer);
+		free(output_buffer);
 		return;
-
-	while (offset < vpd_region.size) {
-		type = *(uint8_t *)(vpd_buffer + offset);
-		key_len = *(uint8_t *)(vpd_buffer + offset + 1);
-		if (type == 0) {
-			printk(BIOS_WARNING, "VPD is empty\n");
-			free(vpd_buffer);
-			free(output_buffer);
-			return;
-		}
-		else if (type == 1) {
-			if (memcmp("scon", (vpd_buffer + offset + 2),
-			           key_len)) {
-				offset += key_len;
-				value_len = *(uint8_t *)(vpd_buffer + offset);
-				offset += value_len;
-				continue;
-			} else
-				break;
-		}
 	}
-	uint32_t offset_scon = offset;
 
-	type = *(uint8_t *)(vpd_buffer + offset++);
-	key_len = *(uint8_t *)(vpd_buffer + offset++);
-	value_len = *(uint8_t *)(vpd_buffer + key_len + offset++);
-	offset += key_len;
-	offset += value_len;
-
-	char scon_entry[2 + strlen("scon") + 1 + strlen("enabled")];
-	scon_entry[0] = 0x01;
-	scon_entry[1] = strlen("scon");
-	memcpy(&scon_entry[2], "scon", strlen("scon"));
-	scon_entry[2 + strlen("scon")] = strlen("enabled");
-	memcpy(&scon_entry[3 + strlen("scon")], "enabled", strlen("enabled"));
-
-	if (*(char *)(vpd_buffer + offset) == 0x00) {
-		memcpy(output_buffer + offset_scon, scon_entry,
-		       sizeof(scon_entry));
-		*(char *)(output_buffer + offset_scon + sizeof(scon_entry)) = 0;
-	} else {
-		memset(output_buffer + offset_scon, 0xFF,
-		       vpd_region.size - offset_scon);
-		memcpy(output_buffer + offset_scon, scon_entry,
-		       sizeof(scon_entry));
-		strcpy(output_buffer + offset_scon +
-					sizeof(scon_entry),
-					vpd_buffer + offset);
-	}
-	*(uint32_t *)(output_buffer + 0x600 + 12) = info.size - 1;
 	vpd->rw_size = info.size - 1;
 	memcpy(&vpd->blob[vpd->ro_size],
 	       output_buffer + 0x600 + sizeof(struct google_vpd_info),
-	       info.size - 1);
+	       info.size);
 
-	if(flash_vpd(vpd_region.offset, vpd_region.size, output_buffer)) {
-		printk(BIOS_WARNING, "Failed to flash VPD\n");
-	}
+	hexdump(vpd, vpd->ro_size + vpd->rw_size + 16);
+	hexdump(output_buffer, vpd_region.size);
+
+	if (fmap_overwrite_area("RW_VPD", output_buffer, vpd_region.size) == -1)
+		printk(BIOS_ERR, "Flashing VPD failed\n");
+
+	free(vpd_buffer);
+	free(output_buffer);
 }
