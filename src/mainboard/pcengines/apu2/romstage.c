38aaef5913f36f3d00222f9534aa0cec42756fb9
Krystian Hebel <krystian.hebel@3mdeb.com>
2019-12-31 20:30:21 +0100

    mainboard/apu2: fix watchdog issues
    Platform hanged without setting RstToCpuPwrGdEn.
    
    Code for enabling watchdog is now in board_BeforeInitEarly, as hardware
    is not enabled before InitReset. It didn't work on first boot after
    power cycle.
    
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- src/mainboard/pcengines/apu2/romstage.c
+++ src/mainboard/pcengines/apu2/romstage.c
@@ -15,7 +15,10 @@
 #include <console/uart.h>
 #include <gpio.h>
 #include <northbridge/amd/agesa/state_machine.h>
+#include <Fch/Fch.h>
+#include <smp/node.h>
 
+#include "bios_knobs.h"
 #include "gpio_ftns.h"
 
 #define SIO_PORT 0x2e
@@ -23,6 +26,9 @@
 #define SERIAL2_DEV PNP_DEV(SIO_PORT, NCT5104D_SP2)
 
 static void early_lpc_init(void);
+static void print_sign_of_life(void);
+extern char coreboot_dmi_date[];
+extern char coreboot_version[];
 
 void board_BeforeAgesa(struct sysinfo *cb)
 {
@@ -82,3 +88,64 @@ static void early_lpc_init(void)
 	if (CONFIG(BOARD_PCENGINES_APU5))
 		gpio_configure_pads(gpio_apu5, ARRAY_SIZE(gpio_apu5));
 }
+
+static const char *mainboard_bios_version(void)
+{
+	if (strlen(CONFIG_LOCALVERSION))
+		return CONFIG_LOCALVERSION;
+	else
+		return coreboot_version;
+}
+
+static void print_sign_of_life(void)
+{
+	char tmp[9];
+	strncpy(tmp,   coreboot_dmi_date+6, 4);
+	strncpy(tmp+4, coreboot_dmi_date+3, 2);
+	strncpy(tmp+6, coreboot_dmi_date,   2);
+	tmp[8] = '\0';
+	printk(BIOS_ALERT, CONFIG_MAINBOARD_VENDOR " "
+	                   CONFIG_MAINBOARD_PART_NUMBER "\n");
+	printk(BIOS_ALERT, "coreboot build %s\n", tmp);
+	printk(BIOS_ALERT, "BIOS version %s\n", mainboard_bios_version());
+}
+
+void board_BeforeInitReset(struct sysinfo *cb, AMD_RESET_PARAMS *Reset)
+{
+	u32 val, data;
+
+	if (boot_cpu()) {
+		pm_write8(FCH_PMIOA_REGC5, 0);
+
+		/* Check if cold boot was requested */
+		val = pci_read_config32(PCI_DEV(0, 0x18, 0), 0x6C);
+		if (val & (1 << 4)) {
+			printk(BIOS_ALERT, "Forcing cold boot path\n");
+			val &= ~(0x630); // ColdRstDet[4], BiosRstDet[10:9, 5]
+			pci_write_config32(PCI_DEV(0, 0x18, 0), 0x6C, val);
+
+			pm_write32(0xc0, 0x3fff003f); // Write-1-to-clear resets
+
+			/* FullRst, SysRst, RstCmd */
+			pm_write8(FCH_PMIOA_REGC5, 0xe);
+			printk(BIOS_ALERT, "Did not reset (yet)\n");
+		}
+
+		// do not print SOL if reset will take place in FchInit
+		if (check_console() &&
+		    !(pm_read32(0xc0) & FCH_PMIOxC0_S5ResetStatus_All_Status))
+			print_sign_of_life();
+
+		if ((check_mpcie2_clk() || CONFIG(FORCE_MPCIE2_CLK)) &&
+		     CONFIG(BOARD_PCENGINES_APU2)) {
+			// make GFXCLK to ignore CLKREQ# input
+			// force it to be always on
+			data = misc_read32(FCH_MISC_REG04);
+			data &= 0xFFFFFF0F;
+			data |= 0xF << (1 * 4); // CLKREQ GFX to GFXCLK
+			misc_write32(FCH_MISC_REG04, data);
+			printk(BIOS_DEBUG, "force mPCIe clock enabled\n");
+		}
+	}
+}
+
