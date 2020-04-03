ed8e9ede6572186879bf7fb3d44f99d537598246
Michał Żygowski <michal.zygowski@3mdeb.com>
2020-04-03 17:29:51 +0200

    src/mainboard/pcengines/apu2/bios_knobs.c: fix watchdog runtime check
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/mainboard/pcengines/apu2/bios_knobs.c
+++ src/mainboard/pcengines/apu2/bios_knobs.c
@@ -443,19 +443,28 @@ u16 get_watchdog_timeout(void)
 {
 	const char *boot_file = NULL;
 	size_t boot_file_len = 0;
+	struct region_device bootorder_area;
 	u16 timeout;
 
-	//
-	// This function locates a file in cbfs, maps it to memory and returns
-	// a void* pointer
-	//
-	boot_file = (const char *) cbfs_map(BOOTORDER_FILE, &boot_file_len);
-	if (boot_file == NULL)
-		printk(BIOS_INFO, "file [%s] not found in CBFS\n",
-			BOOTORDER_FILE);
-	if (boot_file_len < 4096)
-		printk(BIOS_INFO, "Missing bootorder data.\n");
-	if (boot_file == NULL || boot_file_len < 4096)
+	if (CONFIG(SEABIOS_BOOTORDER_IN_FMAP)) {
+		if (fmap_locate_area_as_rdev("BOOTORDER", &bootorder_area)) {
+			printk(BIOS_WARNING, "Could not find bootorder area\n");
+			return -1;
+		}
+		boot_file_len = region_device_sz(&bootorder_area);
+		boot_file = rdev_mmap_full(&bootorder_area);
+	} else {
+		boot_file = cbfs_boot_map_with_leak(BOOTORDER_FILE,
+						    CBFS_TYPE_RAW,
+						    &boot_file_len);
+	}
+
+	if (boot_file == NULL) {
+		printk(BIOS_INFO, "Could not get bootorder content\n");
+	}
+	if (boot_file_len != 4096)
+		printk(BIOS_INFO, "Wrong bootorder size.\n");
+	if (boot_file == NULL || boot_file_len != 4096)
 		return -1;
 
 	timeout = (u16) strtoul(findstr(boot_file, "watchdog"), NULL, 16);
