ec0b9ee02670a7e23e0feb9256fbd5a9a4e886f2
Michał Żygowski <michal.zygowski@3mdeb.com>
2020-04-26 13:36:55 +0200

    src/mainboard/pcengines/apu2: add pciepm runtime option
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/mainboard/pcengines/apu2/OemCustomize.c
+++ src/mainboard/pcengines/apu2/OemCustomize.c
@@ -8,7 +8,7 @@
 #include <Fch/Fch.h>
 #include <amdblocks/acpimmio.h>
 
-static const PCIe_PORT_DESCRIPTOR PortList[] = {
+static const PCIe_PORT_DESCRIPTOR PortListAspm[] = {
 	{
 		0,
 		PCIE_ENGINE_DATA_INITIALIZER(PciePortEngine, 3, 3),
@@ -70,6 +70,68 @@ static const PCIe_PORT_DESCRIPTOR PortList[] = {
 	}
 };
 
+static const PCIe_PORT_DESCRIPTOR PortList[] = {
+	{
+		0,
+		PCIE_ENGINE_DATA_INITIALIZER(PciePortEngine, 3, 3),
+		PCIE_PORT_DATA_INITIALIZER_V2(PortEnabled, ChannelTypeExt6db, 2, 5,
+				HotplugDisabled,
+				PcieGenMaxSupported,
+				PcieGenMaxSupported,
+				AspmDisabled,
+				0x01,
+				0)
+	},
+	/* Initialize Port descriptor (PCIe port, Lanes 1, PCI Device Number 2, ...) */
+	{
+		0,
+		PCIE_ENGINE_DATA_INITIALIZER(PciePortEngine, 2, 2),
+		PCIE_PORT_DATA_INITIALIZER_V2(PortEnabled, ChannelTypeExt6db, 2, 4,
+				HotplugDisabled,
+				PcieGenMaxSupported,
+				PcieGenMaxSupported,
+				AspmDisabled,
+				0x02,
+				0)
+	},
+	/* Initialize Port descriptor (PCIe port, Lanes 2, PCI Device Number 2, ...) */
+	{
+		0,
+		PCIE_ENGINE_DATA_INITIALIZER(PciePortEngine, 1, 1),
+		PCIE_PORT_DATA_INITIALIZER_V2(PortEnabled, ChannelTypeExt6db, 2, 3,
+				HotplugDisabled,
+				PcieGenMaxSupported,
+				PcieGenMaxSupported,
+				AspmDisabled,
+				0x03,
+				0)
+	},
+	/* Initialize Port descriptor (PCIe port, Lanes 3, PCI Device Number 2, ...) */
+	{
+		0,
+		PCIE_ENGINE_DATA_INITIALIZER(PciePortEngine, 0, 0),
+		PCIE_PORT_DATA_INITIALIZER_V2(PortEnabled, ChannelTypeExt6db, 2, 2,
+				HotplugDisabled,
+				PcieGenMaxSupported,
+				PcieGenMaxSupported,
+				AspmDisabled,
+				0x04,
+				0)
+	},
+	/* Initialize Port descriptor (PCIe port, Lanes 4-7, PCI Device Number 4, ...) */
+	{
+		DESCRIPTOR_TERMINATE_LIST,
+		PCIE_ENGINE_DATA_INITIALIZER(PciePortEngine, 4, 7),
+		PCIE_PORT_DATA_INITIALIZER_V2(PortEnabled, ChannelTypeExt6db, 2, 1,
+				HotplugDisabled,
+				PcieGenMaxSupported,
+				PcieGenMaxSupported,
+				AspmDisabled,
+				0x05,
+				0)
+	}
+};
+
 static const PCIe_COMPLEX_DESCRIPTOR PcieComplex = {
 	.Flags        = DESCRIPTOR_TERMINATE_LIST,
 	.SocketId     = 0,
@@ -77,9 +139,20 @@ static const PCIe_COMPLEX_DESCRIPTOR PcieComplex = {
 	.DdiLinkList  = NULL,
 };
 
+static const PCIe_COMPLEX_DESCRIPTOR PcieComplexAspm = {
+	.Flags        = DESCRIPTOR_TERMINATE_LIST,
+	.SocketId     = 0,
+	.PciePortList = PortListAspm,
+	.DdiLinkList  = NULL,
+};
+
 void board_BeforeInitEarly(struct sysinfo *cb, AMD_EARLY_PARAMS *InitEarly)
 {
-	InitEarly->GnbConfig.PcieComplexList = &PcieComplex;
+	if (check_pciepm())
+		InitEarly->GnbConfig.PcieComplexList = &PcieComplexAspm;
+	else
+		InitEarly->GnbConfig.PcieComplexList = &PcieComplex;
+
 	if (check_boost()) {
 		InitEarly->PlatformConfig.CStateMode = CStateModeC6;
 		InitEarly->PlatformConfig.CpbMode = CpbModeAuto;
