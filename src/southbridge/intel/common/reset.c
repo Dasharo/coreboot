c97f3f246a3112d29a0c93c7af37f09281d6535f
Michał Żygowski <michal.zygowski@3mdeb.com>
2020-06-04 13:08:18 +0200

    src/southbridge/intel/common/reset.c: implement global reset hook
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/southbridge/intel/common/reset.c
+++ src/southbridge/intel/common/reset.c
@@ -2,8 +2,21 @@
 
 #include <cf9_reset.h>
 #include <reset.h>
+#include <device/pci_def.h>
+#include <device/pci_ops.h>
 
 void do_board_reset(void)
 {
 	system_reset();
+}
+
+void cf9_global_reset_prepare(void)
+{
+#ifdef __SIMPLE_DEVICE__
+	pci_devfn_t dev = PCI_DEV(0, 0x1f, 0);
+#else
+	struct device *dev = pcidev_on_root(0x1f, 0);
+#endif
+	/* Set bit 20 for global reset */
+	pci_write_config32(dev, 0xac, pci_read_config32(dev, 0xac) | BIT(20));
 }
