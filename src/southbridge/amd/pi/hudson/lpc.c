f49e0cd23d36cc65b1753549750a8cf2b8604170
Michał Żygowski <michal.zygowski@3mdeb.com>
2020-11-04 15:57:51 +0100

    src/southbridge/amd/pi/hudson/lpc.c: add MCFG table
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/southbridge/amd/pi/hudson/lpc.c
+++ src/southbridge/amd/pi/hudson/lpc.c
@@ -330,7 +330,13 @@ static void hudson_lpc_enable_resources(struct device *dev)
 
 unsigned long acpi_fill_mcfg(unsigned long current)
 {
-	/* Just a dummy */
+
+	current += acpi_create_mcfg_mmconfig((acpi_mcfg_mmconfig_t *)current,
+					     CONFIG_MMCONF_BASE_ADDRESS,
+					     0,
+					     0,
+					     CONFIG_MMCONF_BUS_NUMBER - 1);
+
 	return current;
 }
 
