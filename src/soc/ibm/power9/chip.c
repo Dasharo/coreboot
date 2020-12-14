3a1d43009a5c8b61b91e1c307c32de25dc18b45b
Igor Bagnucki <igor.bagnucki@3mdeb.com>
2020-12-14 14:52:50 +0100

    src/soc/ibm/*: Add file structure to soc, implement SCOM access
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>

---
+++ src/soc/ibm/power9/chip.c
@@ -0,0 +1,6 @@
+#include <device/device.h>
+
+struct chip_operations soc_ibm_power9_ops = {
+  CHIP_NAME("POWER9")
+};
+
---
+++ src/soc/ibm/power9/timer.c
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <delay.h>
+
+void init_timer(void)
+{
+  // no need to do anything here
+}
