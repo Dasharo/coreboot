904a5620cc1074300c560a66fd26762e3244bd4f
Jeremy Soller <jeremy@system76.com>
2020-12-29 11:15:18 -0700

    soc/intel/tigerlake: Add i915_gpu_controller_info
    Change-Id: Iadb530d7113a48549779ea3baa499f9f7a772f32
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>

--- src/soc/intel/tigerlake/graphics.c
+++ src/soc/intel/tigerlake/graphics.c
@@ -1,11 +1,11 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
+/* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <intelblocks/graphics.h>
 #include <soc/ramstage.h>
 
 const struct i915_gpu_controller_info *
-intel_igd_get_controller_info(const struct device *const dev)
+intel_igd_get_controller_info(const struct device *device)
 {
-	const struct soc_intel_tigerlake_config *const chip = dev->chip_info;
+	struct soc_intel_tigerlake_config *chip = device->chip_info;
 	return &chip->gfx;
 }
