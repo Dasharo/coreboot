c01a047d866393cb8f9c3d9177cf1bd1eb0065ba
Krystian Hebel <krystian.hebel@3mdeb.com>
2021-02-23 16:56:16 +0100

    soc/power9/: implement istep 13.2
    13.2 mem_pll_reset: Reset PLL for MCAs in async
    
    a) p9_mem_pll_reset.C (proc chip)
       - This step is a no-op on cumulus as the centaur is already has its
         PLLs setup in step 11
       - This step is a no-op if memory is running in synchronous mode since
         the MCAs are using the nest PLL, HWP detect and exits
       - If in async mode then this HWP will put the PLL into bypass, reset
         mode
       - Disable listen_to_sync for MEM chiplet, whenever MEM is not in sync
         to NEST
    
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>
    Change-Id: I7cf4d5a566d428c3326af26a720905096b97b3c8

--- src/soc/ibm/power9/Makefile.mk
+++ src/soc/ibm/power9/Makefile.mk
@@ -8,6 +8,7 @@ bootblock-y += rom_media.c
 romstage-y += cbmem.c
 romstage-y += ccs.c
 romstage-y += i2c.c
+romstage-y += istep_13_2.c
 romstage-y += rom_media.c
 romstage-y += romstage.c
 romstage-y += vpd.c
