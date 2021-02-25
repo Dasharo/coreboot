ab6af2325833e406b216851111e7a13a494bd60f
Krystian Hebel <krystian.hebel@3mdeb.com>
2021-02-25 17:09:39 +0100

    soc/power9/: implement istep 13.6
    13.6 mem_startclocks: Start clocks on MBA/MCAs
    
    a) p9_mem_startclocks.C (proc chip)
       - This step is a no-op on cumulus
       - This step is a no-op if memory is running in synchronous mode since
         the MCAs are using the nest PLL, HWP detect and exits
       - Drop fences and tholds on MBA/MCAs to start the functional clocks
    
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>
    Change-Id: I1e2fdf6a3113a3f23fc8af441cbfac0b3320972b

--- src/soc/ibm/power9/Makefile.mk
+++ src/soc/ibm/power9/Makefile.mk
@@ -11,6 +11,7 @@ romstage-y += i2c.c
 romstage-y += istep_13_2.c
 romstage-y += istep_13_3.c
 romstage-y += istep_13_4.c
+romstage-y += istep_13_6.c
 romstage-y += rom_media.c
 romstage-y += romstage.c
 romstage-y += vpd.c
