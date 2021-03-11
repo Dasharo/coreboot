f68c221b8cae867c34da3d890d0a1728eec3e47b
Michał Żygowski <michal.zygowski@3mdeb.com>
2021-03-11 19:09:12 +0100

    src/soc/intel/apollolake/chip.c: add IOAPIC and HPET parameters
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/soc/intel/apollolake/chip.c
+++ src/soc/intel/apollolake/chip.c
@@ -696,6 +696,19 @@ void platform_fsp_silicon_init_params_cb(FSPS_UPD *silupd)
 	/* Set VTD feature according to devicetree */
 	silconfig->VtdEnable = cfg->enable_vtd;
 
+	/* High Precision Timer options */
+	silconfig->HpetBdfValid = 1;
+	silconfig->HpetBusNumber = 0xFA;
+	silconfig->HpetDeviceNumber = 0x0F;
+	silconfig->HpetFunctionNumber = 0;
+
+	/* IO-APIC options */
+	silconfig->IoApicId = 1;
+	silconfig->IoApicBdfValid = 1;
+	silconfig->IoApicBusNumber = 0xFA;
+	silconfig->IoApicDeviceNumber = 0x1F;
+	silconfig->IoApicFunctionNumber = 0;
+
 	dev = pcidev_path_on_root(SA_DEVFN_IGD);
 	silconfig->PeiGraphicsPeimInit = CONFIG(RUN_FSP_GOP) && is_dev_enabled(dev);
 
