5992b72d21191cec857e4680f5407a8aa061e41f
Michał Żygowski <michal.zygowski@3mdeb.com>
2021-03-11 19:10:06 +0100

    src/mainboard/protectli/vault_glk/mainboard.c: add beep and IPC settings
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/mainboard/protectli/vault_glk/mainboard.c
+++ src/mainboard/protectli/vault_glk/mainboard.c
@@ -1,16 +1,48 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <arch/io.h>
+#include <arch/mmio.h>
 #include <device/device.h>
+#include <soc/iomap.h>
+#include <fsp/api.h>
+#include <soc/ramstage.h>
 
-static void mainboard_init(void *chip_info)
+static void beep(unsigned int frequency)
 {
+
+	unsigned int count = 1193180 / frequency;
+
+	// Switch on the speaker
+	outb(inb(0x61)|3, 0x61);
+
+	// Set command for counter 2, 2 byte write
+	outb(0xb6, 0x43);
+
+	// Select desired Hz
+	outb(count & 0xff, 0x42);
+	outb((count >> 8) & 0xff, 0x42);
+
+	// Block for 100 milioseconds
+	int i;
+	for (i = 0; i < 10000; i++) inb(0x61);
+
+	// Switch off the speaker
+	outb(inb(0x61) & 0xfc, 0x61);
 }
 
-static void mainboard_enable(struct device *dev)
+static void mainboard_final(void *chip_info)
 {
+	beep(1500);
+}
+
+void mainboard_silicon_init_params(FSP_S_CONFIG *silconfig)
+{
+	silconfig->IPC[0] = 0x00ff4000;
+	silconfig->IPC[1] = 0x00000007;
+	silconfig->IPC[2] = 0x00000000;
+	silconfig->IPC[3] = 0x00000000;
 }
 
 struct chip_operations mainboard_ops = {
-	.init = mainboard_init,
-	.enable_dev = mainboard_enable,
+	.final = mainboard_final,
 };
