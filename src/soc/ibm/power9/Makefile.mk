84060c0cfce2d8a798e0f144574bbd4e2657308d
Krystian Hebel <krystian.hebel@3mdeb.com>
2021-03-29 17:23:24 +0200

    soc/power9/istep_13_11.c: implementation
    13.11 mss_draminit_training: Dram training
    
    a) p9_mss_draminit_training.C (mcbist) -- Nimbus
    b) p9c_mss_draminit_training.C (mba) -- Cumulus
       - Prior to running this procedure will apply known DQ bad bits to
         prevent them from participating in training. This information is
         extracted from the bad DQ attribute and applied to Hardware
         - Marks the calibration fail array
       - External ZQ Calibration
       - Execute initial dram calibration (7 step - handled by HW)
       - This procedure will update the bad DQ attribute for each dimm based
         on its findings
    
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>
    Change-Id: Ia1250dd49c0f780c38da8ca264a984083fea7767

--- src/soc/ibm/power9/Makefile.mk
+++ src/soc/ibm/power9/Makefile.mk
@@ -15,6 +15,7 @@ romstage-y += istep_13_6.c
 romstage-y += istep_13_8.c
 romstage-y += istep_13_9.c
 romstage-y += istep_13_10.c
+romstage-y += istep_13_11.c
 romstage-y += rom_media.c
 romstage-y += romstage.c
 romstage-y += vpd.c
