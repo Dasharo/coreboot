61598d46d6f4beb0d9473c595a8a632f399e371e
Krystian Hebel <krystian.hebel@3mdeb.com>
2021-04-26 09:01:04 +0200

    mb/raptor-cs/talos-2: add timestamps and CBMEM initialization
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>
    Change-Id: I0a36912aaaa9b3fd75f5f62ccd6d4ea689f7d066

--- src/arch/ppc64/stages.c
+++ src/arch/ppc64/stages.c
@@ -14,6 +14,7 @@
 #include <cbmem.h>
 #include <arch/stages.h>
 #include <cpu/power/spr.h>
+#include <timestamp.h>
 
 void stage_entry(uintptr_t stage_arg)
 {
@@ -23,6 +24,8 @@ void stage_entry(uintptr_t stage_arg)
 
 	if (!ENV_ROMSTAGE_OR_BEFORE)
 		_cbmem_top_ptr = stage_arg;
+	else
+		timestamp_init(read_spr(SPR_TB));
 
 #if ENV_RAMSTAGE
 	hrmor = read_spr(SPR_HRMOR);
