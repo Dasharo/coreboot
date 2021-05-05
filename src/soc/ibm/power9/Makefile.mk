3c4851a3a0c54c37c43827f586087539efa17198
Krystian Hebel <krystian.hebel@3mdeb.com>
2021-05-05 09:05:41 +0200

    soc/power9/istep_14_5.c: implementation
    14.5 proc_setup_bars: Setup Memory BARs
    
    a) p9_mss_setup_bars.C (proc chip) -- Nimbus
    b) p9c_mss_setup_bars.C (proc chip) -- Cumulus
       - Same HWP interface for both Nimbus and Cumulus, input target is
         TARGET_TYPE_PROC_CHIP; HWP is to figure out if target is a
         Nimbus (MCS) or Cumulus (MI) internally.
       - Prior to setting the memory bars on each processor chip, this
         procedure needs to set the centaur security protection bit
         - TCM_CHIP_PROTECTION_EN_DC is SCOM Addr 0x03030000
         - TCN_CHIP_PROTECTION_EN_DC is SCOM Addr 0x02030000
         - Both must be set to protect Nest and Mem domains
       - Based on system memory map
         - Each MCS has its mirroring and non mirrored BARs
         - Set the correct checkerboard configs. Note that chip flushes to
           checkerboard
         - need to disable memory bar on slave otherwise base flush values
           will ack all memory accesses
    c) p9_setup_bars.C
       - Sets up Powerbus/MCD, L3 BARs on running core
         - Other cores are setup via winkle images
       - Setup dSMP and PCIe Bars
         - Setup PCIe outbound BARS (doing stores/loads from host core)
           - Addresses that PCIE responds to on powerbus (PCI init 1-7)
         - Informing PCIe of the memory map (inbound)
           - PCI Init 8-15
       - Set up Powerbus Epsilon settings
         - Code is still running out of L3 cache
         - Use this procedure to setup runtime epsilon values
         - Must be done before memory is viable
    
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>
    Change-Id: I6ebf16b171d0be0daf0776b2bbf6e8fff4ed77ce

--- src/soc/ibm/power9/Makefile.mk
+++ src/soc/ibm/power9/Makefile.mk
@@ -18,6 +18,7 @@ romstage-y += istep_13_10.c
 romstage-y += istep_13_11.c
 romstage-y += istep_13_13.c
 romstage-y += istep_14_1.c
+romstage-y += istep_14_5.c
 romstage-y += rom_media.c
 romstage-y += romstage.c
 romstage-y += vpd.c
