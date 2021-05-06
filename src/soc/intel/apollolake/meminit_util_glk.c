5d3ff619ac8a29f0cbc91992ed4ac4628aade258
Michał Żygowski <michal.zygowski@3mdeb.com>
2021-05-06 17:54:40 +0200

    src/soc/intel/apollolake/meminit_util_glk.c: add DRAM info for DDR4
    Change-Id: If91bb2d9dc10ffdeead8d800d1c4b074e622fa74
    Upstream-Status: Pending
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/soc/intel/apollolake/meminit_util_glk.c
+++ src/soc/intel/apollolake/meminit_util_glk.c
@@ -7,6 +7,9 @@
 #include <soc/intel/common/smbios.h>
 #include <soc/meminit.h>
 #include <string.h>
+#include <smbios.h>
+
+#include <FspmUpd.h>
 
 #define FSP_SMBIOS_MEMORY_INFO_GUID	\
 {	\
@@ -111,4 +114,110 @@ void save_lpddr4_dimm_info(const struct lpddr4_cfg *lp4cfg, size_t mem_sku)
 	}
 
 	save_lpddr4_dimm_info_part_num(part_num);
+}
+
+/* Save the DIMM information for SMBIOS table 17 */
+void save_ddr4_dimm_info(void)
+{
+	int channel, dimm, dimm_max, index, node;
+	size_t hob_size;
+	const CONTROLLER_INFO *ctrlr_info;
+	const CHANNEL_INFO *channel_info;
+	const DIMM_INFO *src_dimm;
+	struct dimm_info *dest_dimm;
+	struct memory_info *mem_info;
+	const FSP_SMBIOS_MEMORY_INFO *memory_info_hob;
+	const uint8_t smbios_memory_info_guid[16] =
+			FSP_SMBIOS_MEMORY_INFO_GUID;
+
+	/* Locate the memory info HOB, presence validated by raminit */
+	memory_info_hob =
+		fsp_find_extension_hob_by_guid(smbios_memory_info_guid,
+						&hob_size);
+	if (memory_info_hob == NULL || hob_size == 0) {
+		printk(BIOS_ERR, "SMBIOS MEMORY_INFO_DATA_HOB not found\n");
+		return;
+	}
+
+	/*
+	 * Allocate CBMEM area for DIMM information used to populate SMBIOS
+	 * table 17
+	 */
+	mem_info = cbmem_add(CBMEM_ID_MEMINFO, sizeof(*mem_info));
+	if (mem_info == NULL) {
+		printk(BIOS_ERR, "CBMEM entry for DIMM info missing\n");
+		return;
+	}
+	memset(mem_info, 0, sizeof(*mem_info));
+
+	/* Describe the first N DIMMs in the system */
+	index = 0;
+	dimm_max = ARRAY_SIZE(mem_info->dimm);
+	for (node = 0; node < MAX_NODE_NUM; node++) {
+		ctrlr_info = &memory_info_hob->Controller[node];
+
+		if (!ctrlr_info->ChannelCount) {
+			printk(BIOS_DEBUG, "No valid channels on controller %d\n", node);
+			continue;
+		}
+
+		for (channel = 0; channel < MAX_CHANNELS_NUM; channel++) {
+			channel_info = &ctrlr_info->ChannelInfo[channel];
+
+			if (index >= dimm_max)
+				break;
+
+			if (!channel_info->Status) {
+				printk(BIOS_DEBUG, "Channel %d not present\n", channel);
+				continue;
+			}
+
+			if (!channel_info->DimmCount) {
+				printk(BIOS_DEBUG, "No valid DIMMs on controller %d channel %d\n", node, channel);
+				continue;
+			}
+
+			for (dimm = 0; dimm < MAX_DIMMS_NUM; dimm++) {
+				if (index >= dimm_max)
+					break;
+
+				src_dimm = &channel_info->DimmInfo[dimm];
+				dest_dimm = &mem_info->dimm[index];
+
+				if (!src_dimm->Status) {
+					printk(BIOS_DEBUG, "DIMM %d/%d/%d not present\n", node, channel, dimm);
+					continue;
+				}
+
+				if (!src_dimm->DimmCapacity) {
+					printk(BIOS_DEBUG, "DIMM %d/%d/%d has 0 capacity\n", node, channel, dimm);
+					continue;
+				}
+
+				u8 memProfNum = memory_info_hob->MemoryProfile;
+
+				/* Populate the DIMM information */
+				dimm_info_fill(dest_dimm,
+					src_dimm->DimmCapacity,
+					memory_info_hob->MemoryType,
+					memory_info_hob->ConfiguredMemoryClockSpeed,
+					src_dimm->RankInDimm,
+					channel_info->ChannelId,
+					src_dimm->DimmId,
+					(const char *)src_dimm->ModulePartNum,
+					sizeof(src_dimm->ModulePartNum),
+					src_dimm->SpdSave + SPD_SAVE_OFFSET_SERIAL,
+					memory_info_hob->DataWidth,
+					memory_info_hob->VddVoltage[memProfNum],
+					memory_info_hob->EccSupport,
+					src_dimm->MfgId,
+					src_dimm->SpdModuleType,
+					node,
+					memory_info_hob->MaximumMemoryClockSpeed);
+				index++;
+			} // for dimm
+		} // for channel
+	} // for node
+	mem_info->dimm_cnt = index;
+	printk(BIOS_DEBUG, "%d DIMMs found\n", mem_info->dimm_cnt);
 }
