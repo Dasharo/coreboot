82df7d854bcac814316aa2c351427df82dce9e2b
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2021-06-01 15:30:18 +0200

    soc/power9: add missing SPDX license headers
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

--- src/soc/ibm/power9/chip.c
+++ src/soc/ibm/power9/chip.c
@@ -1,3 +1,5 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
 #include <device/device.h>
 
 struct chip_operations soc_ibm_power9_ops = {
--- src/soc/ibm/power9/istep_14_2.c
+++ src/soc/ibm/power9/istep_14_2.c
@@ -1,3 +1,5 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
 #include <cpu/power/istep_14.h>
 #include <cpu/power/istep_13.h>
 
