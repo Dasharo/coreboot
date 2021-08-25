85ab081c507094b2f2f97710ce93758bede17dbe
Michał Żygowski <michal.zygowski@3mdeb.com>
2021-08-25 11:25:44 +0200

    src/ec/clevo/it5570: add SMI handler for ACPI mode
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/ec/clevo/it5570/ec.c
+++ src/ec/clevo/it5570/ec.c
@@ -5,24 +5,19 @@
 #include <cpu/x86/smm.h>
 #include <ec/acpi/ec.h>
 
-#include "ec.h"
-
 #define CLEVO_EC_CMD_ENABLE_ACPI_MODE	0x90
 #define CLEVO_EC_CMD_DISABLE_ACPI_MODE	0x91
 
-void clevo_it5570_ec_smi_apmc(int apmc)
+int clevo_it5570_ec_smi_apmc(int apmc)
 {
 	switch (apmc) {
 	case APM_CNT_ACPI_ENABLE:
-		send_ec_command(CLEVO_EC_CMD_ENABLE_ACPI_MODE);
-		send_ec_command(0x98);
-		ec_clr_bit(0xe8, 2);
-		break;
+		return send_ec_command(CLEVO_EC_CMD_ENABLE_ACPI_MODE);
 	case APM_CNT_ACPI_DISABLE:
-		send_ec_command(CLEVO_EC_CMD_DISABLE_ACPI_MODE);
-		break;
+		return send_ec_command(CLEVO_EC_CMD_DISABLE_ACPI_MODE);
 	default:
 		break;
 	}
 
+	return 0;
 }
--- src/ec/clevo/it5570/ec.h
+++ src/ec/clevo/it5570/ec.h
@@ -1,3 +1,3 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
-void clevo_it5570_ec_smi_apmc(int apmc);
+int clevo_it5570_ec_smi_apmc(int apmc);
