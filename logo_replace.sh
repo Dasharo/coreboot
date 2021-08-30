acc88cedf2442406c9c341785a92cadc82d5c1a2
Michał Kopeć <michal.kopec@3mdeb.com>
2021-08-30 15:29:45 +0200

    Add script to replace tianocore bootsplash
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>

---
+++ logo_replace.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# A script to replace the boot logo for coreboot with tianocore payload
+
+if [ $# -ne 1 ]; then 
+	echo "usage: $0 [path to image]"
+	exit 0
+fi
+
+path=custom_bootsplash.bmp
+cp $1 $path
+
+if [[ $(grep "CONFIG_TIANOCORE_BOOTSPLASH_IMAGE" .config; echo $?) == 1 ]]; then
+	echo "CONFIG_TIANOCORE_BOOTSPLASH_IMAGE=y" >> .config
+else
+	sed -i "/CONFIG_TIANOCORE_BOOTSPLASH_IMAGE/c\CONFIG_TIANOCORE_BOOTSPLASH_IMAGE=y" .config
+fi
+
+if [[ $(grep "CONFIG_TIANOCORE_BOOTSPLASH_FILE" .config; echo $?) == 1 ]]; then
+	echo "CONFIG_TIANOCORE_BOOTSPLASH_FILE=\"$path\"" >> .config
+else
+	sed -i "/CONFIG_TIANOCORE_BOOTSPLASH_FILE/c\CONFIG_TIANOCORE_BOOTSPLASH_FILE=\"$path\"" .config
+fi
+
+# compile on all available threads
+CPUS=$(( $(cat /proc/cpuinfo | grep processor | wc -l) + 1 ))
+make
+
