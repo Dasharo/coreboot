510ef6d45f60e2cadbdf7ade8638f1345c8a6bae
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2021-08-31 16:20:52 +0200

    build.sh: rename HASH_FILE
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

--- build.sh
+++ build.sh
@@ -27,7 +27,7 @@ BOARD="clevo_nv41mz"
 DEFCONFIG="configs/config.${BOARD}"
 FW_VERSION=$(cat ${DEFCONFIG} | grep CONFIG_LOCALVERSION | cut -d '=' -f 2 | tr -d '"')
 FW_FILE="dasharo_${BOARD}_${FW_VERSION}.rom"
-HASH_FILE="${FW_FILE%.*}.SHA256"
+HASH_FILE="${FW_FILE}.SHA256"
 SIG_FILE="${HASH_FILE}.sig"
 ARTIFACTS_DIR="artifacts"
 
@@ -43,7 +43,9 @@ build() {
 }
 
 sign() {
-  gpg --default-key "${GPG_FINGERPRINT}" --armor --output "${ARTIFACTS_DIR}/${SIG_FILE}" --detach-sig "${ARTIFACTS_DIR}/${HASH_FILE}"
+  cd "${ARTIFACTS_DIR}"
+  gpg --default-key "${GPG_FINGERPRINT}" --armor --output "${SIG_FILE}" --detach-sig "${HASH_FILE}"
+  cd -
 }
 
 CMD="$1"
