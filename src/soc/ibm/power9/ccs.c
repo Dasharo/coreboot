232318a7e3d93c66cdcc3c26cab5abf5ad2dfebb
Krystian Hebel <krystian.hebel@3mdeb.com>
2021-09-08 17:17:12 +0200

    include/cpu/power/scom.h: introduce DEBUG_SCOM config option
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>
    Change-Id: Ib16a313db9697d6fe1f447ce3dc53de85abfdff1

--- src/soc/ibm/power9/ccs.c
+++ src/soc/ibm/power9/ccs.c
@@ -1,5 +1,8 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+/* Debug is too slow here, hits timeouts */
+#define SKIP_SCOM_DEBUG
+
 #include <cpu/power/istep_13.h>
 #include <timer.h>
 #include <endian.h>
