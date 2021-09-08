d4ddbd3e77f73f800ae37aacc9242d5f5d5d5293
Michał Żygowski <michal.zygowski@3mdeb.com>
2021-09-08 16:40:44 +0200

    src/drivers/pc80/tpm: preserve CONFIG_TPM_PIRQ logic
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/drivers/pc80/tpm/tis.c
+++ src/drivers/pc80/tpm/tis.c
@@ -823,12 +823,13 @@ static void lpc_tpm_fill_ssdt(const struct device *dev)
 	/* Use either Interrupt() or GpioInt() */
 	if (config->irq_gpio.pin_count) {
 		acpi_device_write_gpio(&config->irq_gpio);
-	} else if (config->irq.pin) {
+	} else if (CONFIG_TPM_PIRQ) {
 		/*
 		 * PIRQ: Update interrupt vector with configured PIRQ
 		 * Active-Low Level-Triggered Shared
 		 */
-		acpi_device_write_interrupt(&config->irq);
+		struct acpi_irq tpm_irq_a = ACPI_IRQ_LEVEL_LOW(CONFIG_TPM_PIRQ);
+		acpi_device_write_interrupt(&tpm_irq_a);
 	} else if (tpm_read_int_vector(0) > 0) {
 		u8 int_vec = tpm_read_int_vector(0);
 		u8 int_pol = tpm_read_int_polarity(0);
