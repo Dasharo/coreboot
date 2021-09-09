bbfa658ea207d0a5489f55a5ae86a95635feab2d
Krystian Hebel <krystian.hebel@3mdeb.com>
2021-09-09 14:32:46 +0200

    soc/power9/istep_18_{11,12}.c: fix order and values of SCOM operations
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>
    Change-Id: Ifec3198d08c24d673c861c3dcaddcc6661b2b9d2

--- src/soc/ibm/power9/istep_18_11.c
+++ src/soc/ibm/power9/istep_18_11.c
@@ -3,85 +3,184 @@
 #include <console/console.h>
 #include <cpu/power/istep_18.h>
 
-// TODO: this one will cange if we add more than one processor
+/* TODO: this one will change if we add more than one processor */
 #define MDMT (1)
 
 static uint32_t calculate_topology_delay(void)
 {
-  // in simple topology with one proc it is enough to assign 0
-  // with multiple processors this will get more complicated
-  // see calculate_node_link_delay() in Hosboot
+	/*
+	 * In simple topology with one proc it is enough to assign 0.
+	 * With multiple processors this will get more complicated,
+	 * see calculate_node_link_delay() in Hostboot
+	 */
 
-  if(MDMT)
-  {
-    uint32_t node_link_delay = 0;
-    uint32_t longest_topology_delay = 0;
+	if(MDMT)
+		return MDMT_TOD_GRID_CYCLE_STAGING_DELAY;
 
-    return
-      - longest_topology_delay
-      + node_link_delay
-      + MDMT_TOD_GRID_CYCLE_STAGING_DELAY;
-  }
+	/* TODO: check again if this really is write, not RMW */
+	write_scom(PU_PB_ELINK_RT_DELAY_CTL_REG, PPC_BITMASK(2, 3));
+	uint64_t l_bus_mode_reg = read_scom(PU_PB_ELINK_DLY_0123_REG);
 
-  write_scom(PU_PB_ELINK_RT_DELAY_CTL_REG, PPC_BITMASK(2, 3));
-  uint64_t l_bus_mode_reg = read_scom(PU_PB_ELINK_DLY_0123_REG);
+	uint32_t bus_delay = ((l_bus_mode_reg & BUS_DELAY_47) >> 16) +
+	                     (l_bus_mode_reg & BUS_DELAY_63);
 
-  uint32_t bus_delay =
-    ((l_bus_mode_reg & BUS_DELAY_47) >> 16)
-  + (l_bus_mode_reg & BUS_DELAY_63);
-
-  return (uint32_t)(1 + (
-      (double)(bus_delay * 8 * 1000000)
-    / (double)(4 * FREQ_X_MHZ * TOD_GRID_PS)));
+	/*
+	 * FIXME: floating point wasn't fully configured, see if we can skip it.
+	 * Testing requires bigger topology, i.e. more CPUs.
+	 */
+	return (uint32_t)(1 + ((double)(bus_delay * 8 * 1000000)
+	                        / (double)(4 * FREQ_X_MHZ * TOD_GRID_PS)));
 }
 
-static int64_t calculate_m_path(uint64_t value)
+static void calculate_m_path(void)
 {
-  value |=
-    (read_scom(PERV_ROOT_CTRL8_SCOM) & P9A_PERV_ROOT_CTRL8_TP_PLL_CLKIN_SEL9_DC)
-    ? PPC_BIT(PERV_TOD_M_PATH_CTRL_REG_STEP_CREATE_DUAL_EDGE_DISABLE)
-    : 0;
-  if(MDMT)
-  {
-    value &=
-        ~PPC_BIT(M_PATH_0_OSC_NOT_VALID)
-      & ~PPC_BIT(M_PATH_0_STEP_ALIGN_DISABLE)
-      & ~(4ull << M_PATH_0_STEP_CHECK_VALIDITY_COUNT_OFFSET)
-      & ~(7ull << M_PATH_SYNC_CREATE_SPS_SELECT_OFFSET)
-      & ~(15ull << M_PATH_0_STEP_CHECK_CPS_DEVIATION_OFFSET)
-      & ~(3ull << M_PATH_STEP_CHECK_CPS_DEVIATION_FACTOR);
-    value |=
-        PPC_BIT(M_PATH_1_OSC_NOT_VALID)
-      | (15ull << M_PATH_0_STEP_CHECK_CPS_DEVIATION_OFFSET)
-      | (3ull << M_PATH_0_STEP_CHECK_VALIDITY_COUNT_OFFSET);
-  }
-  return value;
+	uint64_t dual_edge_disable =
+	    (read_scom(PERV_ROOT_CTRL8_SCOM) & PPC_BIT(P9A_PERV_ROOT_CTRL8_TP_PLL_CLKIN_SEL9_DC))
+	    ? PPC_BIT(PERV_TOD_M_PATH_CTRL_REG_STEP_CREATE_DUAL_EDGE_DISABLE)
+	    : 0;
+
+	if(MDMT) {
+		scom_and_or(PERV_TOD_M_PATH_CTRL_REG,
+		            ~(PPC_BIT(M_PATH_0_OSC_NOT_VALID) |
+		              PPC_BIT(M_PATH_0_STEP_ALIGN_DISABLE) |
+		              PPC_BIT(PERV_TOD_M_PATH_CTRL_REG_STEP_CREATE_DUAL_EDGE_DISABLE) |
+		              PPC_SHIFT(0x7, M_PATH_0_STEP_CHECK_VALIDITY_COUNT_OFFSET) |
+		              PPC_SHIFT(0x7, M_PATH_SYNC_CREATE_SPS_SELECT_OFFSET) |
+		              PPC_SHIFT(0xF, M_PATH_0_STEP_CHECK_CPS_DEVIATION_OFFSET) |
+		              PPC_SHIFT(0x3, M_PATH_STEP_CHECK_CPS_DEVIATION_FACTOR)),
+		            PPC_BIT(M_PATH_1_OSC_NOT_VALID) |
+		            PPC_SHIFT(0x8, M_PATH_0_STEP_CHECK_CPS_DEVIATION_OFFSET) |
+		            PPC_SHIFT(0x3, M_PATH_0_STEP_CHECK_VALIDITY_COUNT_OFFSET) |
+		            dual_edge_disable);
+	} else {
+		scom_and_or(PERV_TOD_M_PATH_CTRL_REG,
+		            ~PPC_BIT(PERV_TOD_M_PATH_CTRL_REG_STEP_CREATE_DUAL_EDGE_DISABLE),
+		            dual_edge_disable);
+	}
 }
 
 void istep_18_11(void)
 {
-    printk(BIOS_EMERG, "starting istep 18.11\n");
-    report_istep(18,11);
-    write_scom(PERV_TOD_S_PATH_CTRL_REG, PPC_BITMASK(27, 32));
-    write_scom(PERV_TOD_PRI_PORT_0_CTRL_REG, calculate_topology_delay());
-    write_scom(
-        PERV_TOD_PSS_MSS_CTRL_REG,
-        (MDMT ? PPC_BIT(PRI_M_S_TOD_SELECT) : 0)
-      | PPC_BIT(PRI_M_S_DRAWER_SELECT)
-      | PPC_BIT(SEC_M_S_TOD_SELECT)
-      | PPC_BIT(SEC_M_S_DRAWER_SELECT));
-    write_scom(PERV_TOD_SEC_PORT_1_CTRL_REG, 0);
-    write_scom(PERV_TOD_PRI_PORT_1_CTRL_REG, 0);
-    write_scom(PERV_TOD_M_PATH_CTRL_REG, calculate_m_path(read_scom(PERV_TOD_M_PATH_CTRL_REG)));
-    write_scom(PERV_TOD_SEC_PORT_0_CTRL_REG, calculate_topology_delay());
-    write_scom(
-        PERV_TOD_I_PATH_CTRL_REG,
-        PPC_BITMASK(8, 11)
-      | PPC_BITMASK(14, 15)
-      | PPC_BIT(29)
-      | PPC_BITMASK(34, 36)
-      | PPC_BITMASK(38, 39));
+	printk(BIOS_EMERG, "starting istep 18.11\n");
+	report_istep(18,11);
 
-    write_scom(PERV_TOD_CHIP_CTRL_REG, PPC_BITMASK(10, 15));
-    printk(BIOS_EMERG, "ending istep 18.11\n");
+	/* Clear previous primary topology */
+	write_scom(PERV_TOD_PRI_PORT_0_CTRL_REG, 0);
+	write_scom(PERV_TOD_SEC_PORT_0_CTRL_REG, 0);
+
+	/* Workaround for HW480181: Init remote sync checker tolerance to maximum
+	 *   [26-27] REMOTE_SYNC_CHECK_CPS_DEVIATION_FACTOR = 0x3 (factor 8)
+	 *   [28-31] REMOTE_SYNC_CHECK_CPS_DEVIATION        = 0xF (93.75%)
+	 */
+	scom_or(PERV_TOD_S_PATH_CTRL_REG, PPC_SHIFT(0x3, 27) | PPC_SHIFT(0xF, 31));
+
+	/*
+	 * Set PSS_MSS_CTRL_REG for primary configuration, assumptions:
+	 * - MDMT = 1
+	 * - valid oscillator is attached to master path-0, but not path-1
+	 *   [0] PRI_M_PATH_SELECT     = 0 (path-0 selected)
+	 *   [1] PRI_M_S_TOD_SELECT    = 1 (TOD is master)
+	 *   [2] PRI_M_S_DRAWER_SELECT = 1 (drawer is master)
+	 */
+	scom_and_or(PERV_TOD_PSS_MSS_CTRL_REG, ~PPC_BIT(0),
+	            PPC_BIT(1) | PPC_BIT(2));
+
+	/* Configure PORT_CTRL_REGs (primary) */
+	/*
+	 * TODO: this touches XBUS/OBUS, but Hostboot doesn't modify registers so
+	 * skip it for now and fix if needed.
+	 */
+	//scom_and_or(PERV_TOD_PRI_PORT_0_CTRL_REG, ???, ???);
+	//scom_and_or(PERV_TOD_SEC_PORT_0_CTRL_REG, ???, ???);
+
+	/* Configure M_PATH_CTRL_REG */
+	/*
+	 * TODO: check this again. Value is correct, not sure whether fields are
+	 * correctly cleared. Also comment the values written.
+	 */
+	calculate_m_path();
+
+	/* Configure I_PATH_CTRL_REG (primary) */
+	/* PERV_TOD_PRI_PORT_0_CTRL_REG:
+	 *   [32-39] PRI_I_PATH_DELAY_VALUE = calculate
+	 * PERV_TOD_I_PATH_CTRL_REG:
+	 *   [0]     I_PATH_DELAY_DISABLE                   = 0
+	 *   [1]     I_PATH_DELAY_ADJUST_DISABLE            = 0
+	 *   [6-7]   I_PATH_STEP_CHECK_CPS_DEVIATION_FACTOR = 0 (factor = 1)
+	 *   [8-11]  I_PATH_STEP_CHECK_CPS_DEVIATION        = 0xF (93.75%)
+	 *   [13-15] I_PATH_STEP_CHECK_VALIDITY_COUNT       = 0x3 (count = 8)
+	 */
+	scom_and_or(PERV_TOD_PRI_PORT_0_CTRL_REG, ~PPC_BITMASK(32, 39),
+	            PPC_SHIFT(calculate_topology_delay(), 39));
+	scom_and_or(PERV_TOD_I_PATH_CTRL_REG,
+	            ~(PPC_BIT(0) | PPC_BIT(1) | PPC_BITMASK(6, 11) | PPC_BITMASK(13, 15)),
+	            PPC_SHIFT(0xF, 11) | PPC_SHIFT(0x3, 15));
+
+	/* Configure INIT_CHIP_CTRL_REG (primary) */
+	/*   [1-3]   I_PATH_CORE_SYNC_PERIOD_SELECT   = 0 (core sync period is 8us)
+	 *   [4]     I_PATH_SYNC_CHECK_DISABLE        = 0 (enable internal path sync check)
+	 *   [7]     MOVE_TOD_TO_TB_ON_2X_SYNC_ENABLE = 0 (1x sync boundaries)
+	 *   [8]     USE_TB_SYNC_MECHANISM            = 0
+	 *   [9]     USE_TB_STEP_SYNC                 = 0 (use TB step sync from internal path)
+	 *   [10-15] LOW_ORDER_STEP_VALUE             = 0x3F (4-bit WOF counter is incremented with each 200MHz clock cycle)
+	 *   [30]    XSTOP_GATE                       = 0 (stop TOD on checkstop)
+	 */
+	scom_and_or(PERV_TOD_CHIP_CTRL_REG,
+	            ~(PPC_BITMASK(1, 4) | PPC_BITMASK(7, 15) | PPC_BIT(30)),
+	            PPC_SHIFT(0x3F, 15));
+
+
+	/* TODO: test if we can skip repeated writes (M_PATH, I_PATH, CHIP) */
+
+	/* Clear previous secondary topology */
+	/* NOTE: order is swapped wrt primary, does it matter? */
+	write_scom(PERV_TOD_SEC_PORT_1_CTRL_REG, 0);
+	write_scom(PERV_TOD_PRI_PORT_1_CTRL_REG, 0);
+
+	/*
+	 * Set PSS_MSS_CTRL_REG for secondary configuration, assumptions as before
+	 *   [8]  SEC_M_PATH_SELECT     = 0 (path-0 selected)
+	 *   [9]  SEC_M_S_TOD_SELECT    = 1 (TOD is master)
+	 *   [10] SEC_M_S_DRAWER_SELECT = 1 (drawer is master)
+	 */
+	scom_and_or(PERV_TOD_PSS_MSS_CTRL_REG, ~PPC_BIT(8),
+	            PPC_BIT(9) | PPC_BIT(10));
+
+	/* Configure PORT_CTRL_REGs (secondary) */
+	//scom_and_or(PERV_TOD_SEC_PORT_1_CTRL_REG, ???, ???);
+	//scom_and_or(PERV_TOD_PRI_PORT_1_CTRL_REG, ???, ???);
+
+	/* Configure M_PATH_CTRL_REG */
+	calculate_m_path();
+
+	/* Configure I_PATH_CTRL_REG (secondary) */
+	/* PERV_TOD_SEC_PORT_0_CTRL_REG:
+	 *   [32-39] SEC_I_PATH_DELAY_VALUE = calculate
+	 * PERV_TOD_I_PATH_CTRL_REG:
+	 *   [0]     I_PATH_DELAY_DISABLE                   = 0
+	 *   [1]     I_PATH_DELAY_ADJUST_DISABLE            = 0
+	 *   [6-7]   I_PATH_STEP_CHECK_CPS_DEVIATION_FACTOR = 0 (factor = 1)
+	 *   [8-11]  I_PATH_STEP_CHECK_CPS_DEVIATION        = 0xF (93.75%)
+	 *   [13-15] I_PATH_STEP_CHECK_VALIDITY_COUNT       = 0x3 (count = 8)
+	 */
+	scom_and_or(PERV_TOD_SEC_PORT_0_CTRL_REG, ~PPC_BITMASK(32, 39),
+	            PPC_SHIFT(calculate_topology_delay(), 39));
+	scom_and_or(PERV_TOD_I_PATH_CTRL_REG,
+	            ~(PPC_BIT(0) | PPC_BIT(1) | PPC_BITMASK(6, 11) | PPC_BITMASK(13, 15)),
+	            PPC_SHIFT(0xF, 11) | PPC_SHIFT(0x3, 15));
+
+	/* Configure INIT_CHIP_CTRL_REG (secondary) */
+	/*   [1-3]   I_PATH_CORE_SYNC_PERIOD_SELECT   = 0 (core sync period is 8us)
+	 *   [4]     I_PATH_SYNC_CHECK_DISABLE        = 0 (enable internal path sync check)
+	 *   [7]     MOVE_TOD_TO_TB_ON_2X_SYNC_ENABLE = 0 (1x sync boundaries)
+	 *   [8]     USE_TB_SYNC_MECHANISM            = 0
+	 *   [9]     USE_TB_STEP_SYNC                 = 0 (use TB step sync from internal path)
+	 *   [10-15] LOW_ORDER_STEP_VALUE             = 0x3F (4-bit WOF counter is incremented with each 200MHz clock cycle)
+	 *   [30]    XSTOP_GATE                       = 0 (stop TOD on checkstop)
+	 */
+	scom_and_or(PERV_TOD_CHIP_CTRL_REG,
+	            ~(PPC_BITMASK(1, 4) | PPC_BITMASK(7, 15) | PPC_BIT(30)),
+	            PPC_SHIFT(0x3F, 15));
+
+	printk(BIOS_EMERG, "ending istep 18.11\n");
 }
--- src/soc/ibm/power9/istep_18_12.c
+++ src/soc/ibm/power9/istep_18_12.c
@@ -4,48 +4,82 @@
 #include <cpu/power/istep_18.h>
 #include <timer.h>
 
+/* TODO: this one will change if we add more than one processor */
+#define MDMT (1)
+
+/* TODO: this will be much more complicated for different topology */
 static void init_tod_node(void)
 {
+	uint64_t error_reg;
 
-    write_scom(PERV_TOD_TX_TTYPE_2_REG, PPC_BIT(PERV_TOD_TX_TTYPE_REG_TRIGGER));
-    write_scom(
-        PERV_TOD_LOAD_TOD_MOD_REG,
-        PPC_BIT(PERV_TOD_LOAD_TOD_MOD_REG_FSM_TRIGGER));
-    write_scom(PERV_TOD_TX_TTYPE_5_REG, PPC_BIT(PERV_TOD_TX_TTYPE_REG_TRIGGER));
-    write_scom(
-        PERV_TOD_LOAD_TOD_REG,
-        (0x3FF << LOAD_TOD_VALUE_OFFSET) | (0xF << WOF_OFFSET));
-    write_scom(
-        PERV_TOD_START_TOD_REG,
-        PPC_BIT(PERV_TOD_START_TOD_REG_FSM_TRIGGER));
-    write_scom(PERV_TOD_TX_TTYPE_4_REG, PPC_BIT(PERV_TOD_TX_TTYPE_REG_TRIGGER));
+	/* Clear the TOD error register by writing all bits to 1 */
+	/*
+	 * Probably documentation issue, all bits in this register are described as
+	 * RW, but code treats them as if they were write-1-to-clear.
+	 */
+	write_scom(PERV_TOD_ERROR_REG, ~0);
 
-    // wait until TOD is running
-    wait_us(
-        1000,
-        read_scom(PERV_TOD_FSM_REG) & PPC_BIT(PERV_TOD_FSM_REG_IS_RUNNING));
-    if(!(read_scom(PERV_TOD_FSM_REG) & PPC_BIT(PERV_TOD_FSM_REG_IS_RUNNING)))
-    {
-        printk(BIOS_EMERG, "Error: TOD is not running!\n");
-    }
-    write_scom(PERV_TOD_ERROR_REG,
-        PPC_BIT(PERV_TOD_ERROR_REG_RX_TTYPE_0 + 2)
-      | PPC_BIT(PERV_TOD_ERROR_REG_RX_TTYPE_0 + 4)
-      | PPC_BIT(PERV_TOD_ERROR_REG_RX_TTYPE_0 + 5));
+	/* Assumption: node is MDMT */
+	if (MDMT) {
+		/* Chip TOD step checkers enable */
+		write_scom(PERV_TOD_TX_TTYPE_2_REG,
+		           PPC_BIT(PERV_TOD_TX_TTYPE_REG_TRIGGER));
 
-    write_scom(PERV_TOD_ERROR_MASK_REG,
-        PPC_BIT(PERV_TOD_ERROR_MASK_REG_RX_TTYPE_0)
-      | PPC_BIT(PERV_TOD_ERROR_MASK_REG_RX_TTYPE_0 + 1)
-      | PPC_BIT(PERV_TOD_ERROR_MASK_REG_RX_TTYPE_0 + 2)
-      | PPC_BIT(PERV_TOD_ERROR_MASK_REG_RX_TTYPE_0 + 3)
-      | PPC_BIT(PERV_TOD_ERROR_MASK_REG_RX_TTYPE_0 + 4)
-      | PPC_BIT(PERV_TOD_ERROR_MASK_REG_RX_TTYPE_0 + 5));
+		/* Switch local Chip TOD to 'Not Set' state */
+		write_scom(PERV_TOD_LOAD_TOD_MOD_REG,
+				   PPC_BIT(PERV_TOD_LOAD_TOD_MOD_REG_FSM_TRIGGER));
+
+		/* Switch all Chip TOD in the system to 'Not Set' state */
+		write_scom(PERV_TOD_TX_TTYPE_5_REG,
+		           PPC_BIT(PERV_TOD_TX_TTYPE_REG_TRIGGER));
+
+		/* Chip TOD load value (move TB to TOD) */
+		write_scom(PERV_TOD_LOAD_TOD_REG,
+		           PPC_SHIFT(0x3FF, 59) | PPC_SHIFT(0xC, 63));
+
+		/* Chip TOD start_tod (switch local Chip TOD to 'Running' state) */
+		write_scom(PERV_TOD_START_TOD_REG,
+		           PPC_BIT(PERV_TOD_START_TOD_REG_FSM_TRIGGER));
+
+		/* Send local Chip TOD value to all Chip TODs */
+		write_scom(PERV_TOD_TX_TTYPE_4_REG,
+		           PPC_BIT(PERV_TOD_TX_TTYPE_REG_TRIGGER));
+	}
+
+	/* Wait until TOD is running */
+	if (!wait_us(1000,
+	             read_scom(PERV_TOD_FSM_REG) & PPC_BIT(PERV_TOD_FSM_REG_IS_RUNNING))) {
+		printk(BIOS_ERR, "PERV_TOD_ERROR_REG = %#16.16llx\n",
+		       read_scom(PERV_TOD_ERROR_REG));
+		die("Error: TOD is not running!\n");
+	}
+
+	/* Clear TTYPE#2, TTYPE#4, and TTYPE#5 status */
+	write_scom(PERV_TOD_ERROR_REG,
+	           PPC_BIT(PERV_TOD_ERROR_REG_RX_TTYPE_0 + 2) |
+	           PPC_BIT(PERV_TOD_ERROR_REG_RX_TTYPE_0 + 4) |
+	           PPC_BIT(PERV_TOD_ERROR_REG_RX_TTYPE_0 + 5));
+
+	/* Check for real errors */
+	error_reg = read_scom(PERV_TOD_ERROR_REG);
+	if (error_reg != 0) {
+		printk(BIOS_ERR, "PERV_TOD_ERROR_REG = %#16.16llx\n",
+		       read_scom(PERV_TOD_ERROR_REG));
+		die("Error: TOD initialization failed!\n");
+	}
+
+	/* Set error mask to runtime configuration (mask TTYPE informational bits) */
+	write_scom(PERV_TOD_ERROR_MASK_REG,
+	           PPC_BITMASK(PERV_TOD_ERROR_MASK_REG_RX_TTYPE_0,
+	                       PERV_TOD_ERROR_MASK_REG_RX_TTYPE_0 + 5));
+
+	/* In case of multinode system, configure child nodes recursively here */
 }
 
 void istep_18_12(void)
 {
-    printk(BIOS_EMERG, "starting istep 18.12\n");
-    report_istep(18,12);
-    init_tod_node();
-    printk(BIOS_EMERG, "ending istep 18.12\n");
+	printk(BIOS_EMERG, "starting istep 18.12\n");
+	report_istep(18,12);
+	init_tod_node();
+	printk(BIOS_EMERG, "ending istep 18.12\n");
 }
