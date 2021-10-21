01c9eadcd19913ed9f310b2452a3bc5b802b6e42
Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
2021-10-22 01:02:32 +0300

    drivers/ipmi: prepare for adding more interfaces
    Deduplicate common initialization code (self-test and device
    identification) and put it in a new ipmi_if.c unit, which is
    supposed to work with any underlying IPMI interface.
    
    Change-Id: Ia99da6fb63adb7bf556d3d6f7964b34831be8a2f
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>

--- src/drivers/ipmi/ipmi_kcs.h
+++ src/drivers/ipmi/ipmi_kcs.h
@@ -3,62 +3,8 @@
 #ifndef __IPMI_KCS_H
 #define __IPMI_KCS_H
 
-#define IPMI_NETFN_CHASSIS 0x00
-#define IPMI_NETFN_BRIDGE 0x02
-#define IPMI_NETFN_SENSOREVENT 0x04
-#define IPMI_NETFN_APPLICATION 0x06
-#define  IPMI_BMC_GET_DEVICE_ID 0x01
-#define   IPMI_IPMI_VERSION_MINOR(x) ((x) >> 4)
-#define   IPMI_IPMI_VERSION_MAJOR(x) ((x) & 0xf)
-#define  IPMI_BMC_GET_SELFTEST_RESULTS 0x04
-#define   IPMI_APP_SELFTEST_RESERVED             0xFF
-#define   IPMI_APP_SELFTEST_NO_ERROR             0x55
-#define   IPMI_APP_SELFTEST_NOT_IMPLEMENTED      0x56
-#define   IPMI_APP_SELFTEST_ERROR                0x57
-#define   IPMI_APP_SELFTEST_FATAL_HW_ERROR       0x58
-
-#define IPMI_NETFN_FIRMWARE 0x08
-#define IPMI_NETFN_STORAGE 0x0a
-#define   IPMI_READ_FRU_DATA 0x11
-#define   IPMI_ADD_SEL_ENTRY 0x44
-#define IPMI_NETFN_TRANSPORT 0x0c
-
-#define IPMI_CMD_ACPI_POWERON 0x06
-
-extern int ipmi_kcs_message(int port, int netfn, int lun, int cmd,
-			    const unsigned char *inmsg, int inlen,
-			    unsigned char *outmsg, int outlen);
-
-/* Run basic IPMI init functions in romstage from the provided PnP device,
- * returns CB_SUCCESS on success and CB_ERR if an error occurred. */
-enum cb_err ipmi_kcs_premem_init(const u16 port, const u16 device);
+#include <stdint.h>
 
 void ipmi_bmc_version(uint8_t *ipmi_bmc_major_revision, uint8_t *ipmi_bmc_minor_revision);
-
-struct ipmi_rsp {
-	uint8_t lun;
-	uint8_t cmd;
-	uint8_t completion_code;
-} __packed;
-
-/* Get Device ID */
-struct ipmi_devid_rsp {
-	struct ipmi_rsp resp;
-	uint8_t device_id;
-	uint8_t device_revision;
-	uint8_t fw_rev1;
-	uint8_t fw_rev2;
-	uint8_t ipmi_version;
-	uint8_t additional_device_support;
-	uint8_t manufacturer_id[3];
-	uint8_t product_id[2];
-} __packed;
-
-/* Get Self Test Results */
-struct ipmi_selftest_rsp {
-	struct ipmi_rsp resp;
-	uint8_t result;
-	uint8_t param;
-} __packed;
 
 #endif
---
+++ src/drivers/ipmi/ipmi_ops_premem.c
@@ -0,0 +1,65 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <arch/io.h>
+#include <console/console.h>
+#include <device/pnp.h>
+#include <delay.h>
+#include <timer.h>
+
+#include "ipmi_if.h"
+#include "chip.h"
+
+#if CONFIG(IPMI_BT)
+#include "ipmi_bt.h"
+#endif
+
+enum cb_err ipmi_premem_init(const u16 port, const u16 device)
+{
+	const struct drivers_ipmi_config *conf = NULL;
+	struct ipmi_selftest_rsp selftestrsp = {0};
+	uint8_t retry_count;
+	const struct device *dev;
+
+	/* Find IPMI PNP device from devicetree in romstage */
+	dev = dev_find_slot_pnp(port, device);
+
+	if (!dev) {
+		printk(BIOS_ERR, "IPMI: Cannot find PNP device port: %x, device %x\n",
+			port, device);
+		return CB_ERR;
+	}
+	if (!dev->enabled) {
+		printk(BIOS_ERR, "IPMI: device is not enabled\n");
+		return CB_ERR;
+	}
+	printk(BIOS_DEBUG, "IPMI: romstage PNP %s 0x%x\n",
+	       CONFIG(IPMI_KCS) ? "KCS" : "BT", dev->path.pnp.port);
+	if (dev->chip_info)
+		conf = dev->chip_info;
+
+	if (conf && conf->wait_for_bmc && conf->bmc_boot_timeout) {
+		struct stopwatch sw;
+		stopwatch_init_msecs_expire(&sw, conf->bmc_boot_timeout * 1000);
+		printk(BIOS_DEBUG, "IPMI: Waiting for BMC...\n");
+
+		while (!stopwatch_expired(&sw)) {
+			if (inb(dev->path.pnp.port) != 0xff)
+				break;
+			mdelay(100);
+		}
+		if (stopwatch_expired(&sw)) {
+			printk(BIOS_INFO, "IPMI: Waiting for BMC timed out\n");
+			return CB_ERR;
+		}
+	}
+
+	if (ipmi_process_self_test_result(dev))
+		return CB_ERR;
+
+#if CONFIG(IPMI_BT)
+	if (ipmi_bt_clear(dev->path.pnp.port))
+		dev->enabled = 0;
+#endif
+
+	return CB_SUCCESS;
+}
