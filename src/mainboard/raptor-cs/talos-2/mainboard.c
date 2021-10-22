7c02f3e33b85c1b47f476bf889d508139a89a1b7
Krystian Hebel <krystian.hebel@3mdeb.com>
2021-10-22 11:09:25 +0200

    soc/power9/chip.c: add code for filling unique phandles for all nodes
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>
    Change-Id: Ie7630762f1d53cdd1f782b03b8eb607c7dbacc17

--- src/mainboard/raptor-cs/talos-2/mainboard.c
+++ src/mainboard/raptor-cs/talos-2/mainboard.c
@@ -8,14 +8,6 @@ static void mainboard_enable(struct device *dev)
 {
 	if (!dev)
 		die("No dev0; die\n");
-
-	/*
-	 * Smallest reported to be working (but not officially supported) DIMM is
-	 * 4GB. This means that we always have at least as much available. Last
-	 * 256MB of first 4GB are reserved for hostboot/coreboot, which is also
-	 * included in device tree we are currently passing.
-	 */
-	reserved_ram_resource_kb(dev, 0, 4 * 1024 * 1024 - 256 * 1024, 256 * 1024);
 }
 
 struct chip_operations mainboard_ops = {
