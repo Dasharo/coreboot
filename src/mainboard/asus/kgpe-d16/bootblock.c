4c31bf8da37b29e36f9f521c4f0d59f98f8f9257
Michał Żygowski <michal.zygowski@3mdeb.com>
2021-10-26 13:48:51 +0200

    src/mainboard/asus/kgpe-d16/bootblock.c: print CPU INIT detect status
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/mainboard/asus/kgpe-d16/bootblock.c
+++ src/mainboard/asus/kgpe-d16/bootblock.c
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
 #include <bootblock_common.h>
+#include <console/console.h>
 #include <device/pci_ops.h>
 #include <superio/winbond/common/winbond.h>
 #include <superio/winbond/w83667hg-a/w83667hg-a.h>
@@ -11,8 +12,16 @@
 void bootblock_mainboard_early_init(void)
 {
 	/* Configure secondary serial port pin mux */
-	winbond_set_pinmux(SERIAL_1_DEV, 0x2a, W83667HG_SPI_PINMUX_GPIO4_SERIAL_B_MASK, W83667HG_SPI_PINMUX_SERIAL_B);
+	winbond_set_pinmux(SERIAL_1_DEV, 0x2a,
+			   W83667HG_SPI_PINMUX_GPIO4_SERIAL_B_MASK,
+			   W83667HG_SPI_PINMUX_SERIAL_B);
 
 	/* Initialize early serial */
 	winbond_enable_serial(SERIAL_0_DEV, CONFIG_TTYS0_BASE);
+}
+
+void bootblock_mainboard_init(void)
+{
+	printk(BIOS_DEBUG, "CPU INIT detected %08x\n",
+	       pci_read_config32(PCI_DEV(0,0,0), 0x80));
 }
