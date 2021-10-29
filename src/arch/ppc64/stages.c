4982893506d21b672d33c06d36e73ae163707c0e
Krystian Hebel <krystian.hebel@3mdeb.com>
2021-10-29 12:27:27 +0200

    ppc64/bootblock_crt0.S: always zero HRMOR, link against real address
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>

--- src/arch/ppc64/stages.c
+++ src/arch/ppc64/stages.c
@@ -20,29 +20,10 @@
 
 void stage_entry(uintptr_t stage_arg)
 {
-	if (!ENV_ROMSTAGE_OR_BEFORE) {
-		/*
-		 * This works if:
-		 * - this stage haven't use stack so far
-		 * - this function (stage_entry) doesn't use a stack - preparing a stack
-		 *   frame for main() doesn't count
-		 * - main() doesn't take arguments on stack
-		 * - main() doesn't return
-		 * - stage is linked at proper address that includes HRMOR so we don't
-		 *   have to play with "ignore HRMOR" bit
-		 *
-		 * Also, 'sync' is for changing HRMOR, not stack.
-		 */
-		register uint64_t hrmor = read_spr(SPR_HRMOR);
-		asm volatile("sync; isync" ::: "memory");
-		write_spr(SPR_HRMOR, 0);
-		asm volatile("or 1,1,%0; slbia 7; sync; isync" :: "r"(hrmor) : "memory");
-
+	if (!ENV_ROMSTAGE_OR_BEFORE)
 		_cbmem_top_ptr = stage_arg;
-	}
-	else {
+	else
 		timestamp_init(read_spr(SPR_TB));
-	}
 
 	main();
 }
