0d15415443f81fab47205a141a7777a7eed14aeb
Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
2021-11-22 00:23:04 +0200

    soc/power9: update SCOM accesses in OCC code
    Change-Id: Ic33d97fa41ee88dd1833084cbca670ec984700c1
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>

--- src/include/cpu/power/occ.h
+++ src/include/cpu/power/occ.h
@@ -3,17 +3,18 @@
 #ifndef CPU_PPC64_OCC_H
 #define CPU_PPC64_OCC_H
 
+#include <stdbool.h>
 #include <stdint.h>
 
 struct homer_st;
 
-void clear_occ_special_wakeups(uint64_t cores);
-void special_occ_wakeup_disable(uint64_t cores);
-void occ_start_from_mem(void);
+void clear_occ_special_wakeups(uint8_t chip, uint64_t cores);
+void special_occ_wakeup_disable(uint8_t chip, uint64_t cores);
+void occ_start_from_mem(uint8_t chip);
 /* Moves OCC to active state */
-void activate_occ(struct homer_st *homer);
+void activate_occ(uint8_t chip, struct homer_st *homer, bool is_master);
 
-void pm_occ_fir_init(void);
-void pm_pba_fir_init(void);
+void pm_occ_fir_init(uint8_t chip);
+void pm_pba_fir_init(uint8_t chip);
 
 #endif /* CPU_PPC64_OCC_H */
