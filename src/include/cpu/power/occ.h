513992dc60537a76561677d911d21d001b447ff0
Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
2021-11-22 00:23:04 +0200

    soc/power9: update SCOM accesses in OCC code
    Change-Id: Ic33d97fa41ee88dd1833084cbca670ec984700c1
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>

--- src/include/cpu/power/occ.h
+++ src/include/cpu/power/occ.h
@@ -5,14 +5,14 @@
 
 #include <stdint.h>
 
-void writeOCCSRAM(uint32_t address, uint64_t *buffer, size_t data_length);
-void readOCCSRAM(uint32_t address, uint64_t *buffer, size_t data_length);
-void write_occ_command(uint64_t write_data);
-void clear_occ_special_wakeups(uint64_t cores);
-void special_occ_wakeup_disable(uint64_t cores);
-void occ_start_from_mem(void);
+void writeOCCSRAM(uint8_t chip, uint32_t address, uint64_t *buffer, size_t data_length);
+void readOCCSRAM(uint8_t chip, uint32_t address, uint64_t *buffer, size_t data_length);
+void write_occ_command(uint8_t chip, uint64_t write_data);
+void clear_occ_special_wakeups(uint8_t chip, uint64_t cores);
+void special_occ_wakeup_disable(uint8_t chip, uint64_t cores);
+void occ_start_from_mem(uint8_t chip);
 
-void pm_occ_fir_init(void);
-void pm_pba_fir_init(void);
+void pm_occ_fir_init(uint8_t chip);
+void pm_pba_fir_init(uint8_t chip);
 
 #endif /* CPU_PPC64_OCC_H */
