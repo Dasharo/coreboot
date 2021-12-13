6a9502562ffb07f273241ed791b519392a48073a
Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
2021-12-14 01:16:19 +0200

    soc/power9: require chip number on MVPD accesses
    Chip number is hard-coded to be zero for now, this should change when we
    start constructing HOMER for the second CPU.
    
    Change-Id: I90f2f26581eebad423fbb8a42cc452c2b13e1faa
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>

--- src/soc/ibm/power9/pstates.c
+++ src/soc/ibm/power9/pstates.c
@@ -711,7 +711,8 @@ void build_parameter_blocks(struct homer_st *homer, uint64_t functional_cores)
 
 		record[3] = '0' + quad;
 		size = sizeof(buf);
-		if (!mvpd_extract_keyword(record, "#V", buf, &size)) {
+		/* TODO: don't hard-code chip if values are not the same among them */
+		if (!mvpd_extract_keyword(/*chip=*/0, record, "#V", buf, &size)) {
 			die("Failed to read %s record from MVPD", record);
 		}
 
@@ -873,7 +874,8 @@ void build_parameter_blocks(struct homer_st *homer, uint64_t functional_cores)
 	 * first parses/writes, then tests if bucket ID even match.
 	 */
 	size = sizeof(buf);
-	if (!mvpd_extract_keyword("CRP0", "#W", buf, &size)) {
+	/* TODO: don't hard-code chip if values are not the same among them */
+	if (!mvpd_extract_keyword(/*chip=*/0, "CRP0", "#W", buf, &size)) {
 		die("Failed to read %s record from MVPD", "CRP0");
 	}
 
@@ -995,7 +997,8 @@ void build_parameter_blocks(struct homer_st *homer, uint64_t functional_cores)
 	 * in struct definition.
 	 */
 	size = sizeof(buf);
-	if (!mvpd_extract_keyword("CRP0", "IQ", buf, &size)) {
+	/* TODO: don't hard-code chip if values are not the same among them */
+	if (!mvpd_extract_keyword(/*chip=*/0, "CRP0", "IQ", buf, &size)) {
 		die("Failed to read %s record from MVPD", "CRP0");
 	}
 	assert(size >= sizeof(IddqTable));
