1485f7b2d31488e696a09d1828a785cc869deb3a
Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
2021-12-14 23:22:22 +0200

    soc/power9/fsi.c: return CPU mask on initialization
    Change-Id: Ic808c276d7e3aa5c880604d322b64eff86c869ff
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>

--- src/soc/ibm/power9/fsi.h
+++ src/soc/ibm/power9/fsi.h
@@ -9,7 +9,8 @@
 /* Base FSI address for registers of a FSI I2C master */
 #define I2C_FSI_MASTER_BASE_ADDR 0x01800
 
-void fsi_init(void);
+/* Returns mask of available CPU chips (either 0x01 or 0x03) */
+uint8_t fsi_init(void);
 
 void fsi_i2c_init(uint8_t chips);
 
