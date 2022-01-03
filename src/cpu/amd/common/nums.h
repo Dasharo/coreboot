7b39497339255c5c103fd28714b7bcb60e8f8c9b
Krystian Hebel <krystian.hebel@3mdeb.com>
2022-01-03 19:20:51 +0100

    nb/amdfam10: remove support for root bus number other than 0
    This was used only when board supported more than 8 nodes. There are
    no such platforms in the tree currently so there is no way of testing
    it, nor reason to keep this code untested. Also, HyperTransport code
    is limited to 8 nodes anyway.
    
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>

--- src/cpu/amd/common/nums.h
+++ src/cpu/amd/common/nums.h
@@ -6,15 +6,7 @@
 #include <device/pci_def.h>
 #include <device/pci_ops.h>
 
-#if CONFIG_MAX_PHYSICAL_CPUS > 8
-	#if CONFIG_MAX_PHYSICAL_CPUS > 32
-		#define NODE_NUMS 64
-	#else
-		#define NODE_NUMS 32
-	#endif
-#else
-	#define NODE_NUMS 8
-#endif
+#define NODE_NUMS 8
 
 // max HC installed at the same time. ...could be bigger than (48+24) if we have 3x4x4
 #define HC_NUMS 32
@@ -22,22 +14,10 @@
 //it could be more bigger
 #define HC_POSSIBLE_NUM 32
 
-#if NODE_NUMS == 64
 #if ENV_PCI_SIMPLE_DEVICE
-	#define NODE_PCI(x, fn) ((x < 32) ? \
-			(PCI_DEV(CONFIG_CBB, (CONFIG_CDB + x), fn)) : \
-			(PCI_DEV((CONFIG_CBB - 1),(CONFIG_CDB + x - 32), fn)))
+	#define NODE_PCI(x, fn) PCI_DEV(0, CONFIG_CDB + x, fn)
 #else
-	#define NODE_PCI(x, fn) ((x < 32) ? \
-			(pcidev_path_on_bus(CONFIG_CBB, PCI_DEVFN((CONFIG_CDB + x), fn))) : \
-			(pcidev_path_on_bus((CONFIG_CBB - 1), PCI_DEVFN((CONFIG_CDB + x - 32), fn))))
-#endif
-#else
-#if ENV_PCI_SIMPLE_DEVICE
-	#define NODE_PCI(x, fn) PCI_DEV(CONFIG_CBB, (CONFIG_CDB + x), fn)
-#else
-	#define NODE_PCI(x, fn) pcidev_path_on_bus(CONFIG_CBB, PCI_DEVFN((CONFIG_CDB + x), fn))
-#endif
+	#define NODE_PCI(x, fn) pcidev_on_root(CONFIG_CDB + x, fn)
 #endif
 
 #endif
