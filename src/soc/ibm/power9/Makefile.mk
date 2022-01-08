931257efad01f0ee9ac10137cba3f613a477be8f
Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
2022-01-08 18:27:32 +0200

    soc/power9/istep_9_2.c: XBus calibration
    Change-Id: I373bd7b931be73a58634db3390d0739b1d881789
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>

--- src/soc/ibm/power9/Makefile.mk
+++ src/soc/ibm/power9/Makefile.mk
@@ -16,6 +16,7 @@ romstage-y += istep_8_4.c
 romstage-y += istep_8_9.c
 romstage-y += istep_8_10.c
 romstage-y += istep_8_11.c
+romstage-y += istep_9_2.c
 romstage-y += istep_10_10.c
 romstage-y += istep_10_12.c
 romstage-y += istep_10_13.c
