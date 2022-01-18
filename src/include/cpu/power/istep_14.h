6d1d35091a45b926ff9cb7d5dd072d84f33ed771
Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
2022-01-18 19:07:29 +0200

    soc/power9/istep_14_3.c: update for second CPU
    Chip-specific SCOM accesses and values computed in istep 10.10.
    
    Change-Id: Ic5b4932cfe3fe960ea5464b0df71603a528ca4ed
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>

--- src/include/cpu/power/istep_14.h
+++ src/include/cpu/power/istep_14.h
@@ -5,9 +5,11 @@
 
 #include <stdint.h>
 
+struct pci_info;
+
 void istep_14_1(void);
 void istep_14_2(void);
-void istep_14_3(uint8_t phb_active_mask, const uint8_t *iovalid_enable);
+void istep_14_3(uint8_t chips, const struct pci_info *pci_info);
 void istep_14_5(void);
 
 #endif /* CPU_PPC64_ISTEP_14_H */
