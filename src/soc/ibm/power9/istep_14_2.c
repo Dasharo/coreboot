73b8a95086e95210ba7b0a61ba1044036c719421
Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
2022-01-23 00:52:59 +0200

    soc/power9/istep_14_2.c: update for second CPU
    Also fix naming/formatting while at it.
    
    Change-Id: Ie0fa480eb06faafa68d8b7da99fd7337665aab3e
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>

--- src/soc/ibm/power9/istep_14_2.c
+++ src/soc/ibm/power9/istep_14_2.c
@@ -3,6 +3,7 @@
 #include <console/console.h>
 #include <cpu/power/istep_14.h>
 #include <cpu/power/istep_13.h>
+#include <cpu/power/proc.h>
 #include <cpu/power/scom.h>
 
 #define MCS_MCMODE0 0x5010811
@@ -23,41 +24,46 @@ static void thermal_init(uint8_t chip)
 				   ~PPC_BITMASK(0, 45),
 				   PPC_BIT(10) | PPC_BIT(25) | PPC_BIT(37));
 		}
-		scom_and_for_chiplet(mcs_to_nest[mcs_ids[mcs_i]],
-				     MCS_MCMODE0 + 0x80 * mcs_i,
-				     PPC_BIT(21));
+		rscom_and_for_chiplet(chip, mcs_to_nest[mcs_ids[mcs_i]],
+				      MCS_MCMODE0 + 0x80 * mcs_i,
+				      PPC_BIT(21));
 	}
 }
 
-static void prog_mc_mode0(chiplet_id_t nest_target, size_t index)
+static void prog_mc_mode0(uint8_t chip, chiplet_id_t nest_target, size_t index)
 {
 	uint64_t mask = PPC_BIT(MCS_MCMODE0_DISABLE_MC_SYNC)
 		      | PPC_BIT(MCS_MCMODE0_DISABLE_MC_PAIR_SYNC);
 	uint64_t data = PPC_BIT(MCS_MCMODE0_DISABLE_MC_SYNC)
 		      | PPC_BIT(MCS_MCMODE0_DISABLE_MC_PAIR_SYNC);
-	scom_and_or_for_chiplet(nest_target, MCS_MCMODE0 + 0x80 * index, ~mask,
-				data & mask);
+	rscom_and_or_for_chiplet(chip, nest_target, MCS_MCMODE0 + 0x80 * index, ~mask,
+				 data & mask);
 }
 
-static void throttle_sync(void)
+static void throttle_sync(uint8_t chip)
 {
 	for (size_t mcs_i = 0; mcs_i < MCS_PER_PROC; ++mcs_i)
-		prog_mc_mode0(mcs_to_nest[mcs_ids[mcs_i]], mcs_i);
-	scom_and_for_chiplet(N3_CHIPLET_ID, MCS_MCSYNC,
-			     ~PPC_BIT(MCS_MCSYNC_SYNC_GO_CH0));
-	scom_and_or_for_chiplet(N3_CHIPLET_ID, MCS_MCSYNC, ~PPC_BIT(SUPER_SYNC_BIT),
-				PPC_BITMASK(0, 16));
-	scom_and_for_chiplet(N3_CHIPLET_ID, MCS_MCSYNC, ~PPC_BIT(MBA_REFRESH_SYNC_BIT));
+		prog_mc_mode0(chip, mcs_to_nest[mcs_ids[mcs_i]], mcs_i);
+	rscom_and_for_chiplet(chip, N3_CHIPLET_ID, MCS_MCSYNC,
+			      ~PPC_BIT(MCS_MCSYNC_SYNC_GO_CH0));
+	rscom_and_or_for_chiplet(chip, N3_CHIPLET_ID, MCS_MCSYNC, ~PPC_BIT(SUPER_SYNC_BIT),
+				 PPC_BITMASK(0, 16));
+	rscom_and_for_chiplet(chip, N3_CHIPLET_ID, MCS_MCSYNC, ~PPC_BIT(MBA_REFRESH_SYNC_BIT));
 }
 
-void istep_14_2(void)
+void istep_14_2(uint8_t chips)
 {
-	uint8_t chip = 0; // TODO: support second CPU
+	uint8_t chip;
+
 	report_istep(14, 2);
 	printk(BIOS_EMERG, "starting istep 14.2\n");
 
-	thermal_init(chip);
-	throttle_sync();
+	for (chip = 0; chip < MAX_CHIPS; chip++) {
+		if (chips & (1 << chip)) {
+			thermal_init(chip);
+			throttle_sync(chip);
+		}
+	}
 
 	printk(BIOS_EMERG, "ending istep 14.2\n");
 }
