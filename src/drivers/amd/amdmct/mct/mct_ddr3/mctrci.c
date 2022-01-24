39887964a9c367bc5aa5bc227909063d8c37a307
Igor Bagnucki <igor.bagnucki@3mdeb.com>
2022-01-24 13:55:33 +0100

    mct_ddr3/mctrci.c: Fix names
    Signed-off-by: Igor Bagnucki <igor.bagnucki@3mdeb.com>

--- src/drivers/amd/amdmct/mct/mct_ddr3/mctrci.c
+++ src/drivers/amd/amdmct/mct/mct_ddr3/mctrci.c
@@ -9,13 +9,13 @@
 
 static u8 fam15h_rdimm_rc2_ibt_code(struct DCTStatStruc *p_dct_stat, u8 dct)
 {
-	u8 MaxDimmsInstallable = mct_get_nv_bits(NV_MAX_DIMMS_PER_CH);
+	u8 max_dimms_installable = mct_get_nv_bits(NV_MAX_DIMMS_PER_CH);
 
 	u8 package_type;
 	u8 control_code = 0;
 
 	package_type = mct_get_nv_bits(NV_PACK_TYPE);
-	u16 MemClkFreq = Get_NB32_DCT(p_dct_stat->dev_dct, dct, 0x94) & 0x1f;
+	u16 mem_clk_freq = Get_NB32_DCT(p_dct_stat->dev_dct, dct, 0x94) & 0x1f;
 
 	/* Obtain number of DIMMs on channel */
 	u8 dimm_count = p_dct_stat->ma_dimms[dct];
@@ -28,28 +28,28 @@ static u8 fam15h_rdimm_rc2_ibt_code(struct DCTStatStruc *p_dct_stat, u8 dct)
 	if (package_type == PT_GR) {
 		/* Socket G34 */
 		/* Fam15h BKDG Rev. 3.14 section 2.10.5.7.1.2.1 Table 85 */
-		if (MaxDimmsInstallable == 1) {
-			if ((MemClkFreq == 0x4) || (MemClkFreq == 0x6)) {
+		if (max_dimms_installable == 1) {
+			if ((mem_clk_freq == 0x4) || (mem_clk_freq == 0x6)) {
 				/* DDR3-667 - DDR3-800 */
 				control_code = 0x1;
-			} else if ((MemClkFreq == 0xa) || (MemClkFreq == 0xe)) {
+			} else if ((mem_clk_freq == 0xa) || (mem_clk_freq == 0xe)) {
 				/* DDR3-1066 - DDR3-1333 */
 				if (num_registers == 1) {
 					control_code = 0x0;
 				} else {
 					control_code = 0x1;
 				}
-			} else if ((MemClkFreq == 0x12) || (MemClkFreq == 0x16)) {
+			} else if ((mem_clk_freq == 0x12) || (mem_clk_freq == 0x16)) {
 				/* DDR3-1600 - DDR3-1866 */
 				control_code = 0x0;
 			}
-		} else if (MaxDimmsInstallable == 2) {
+		} else if (max_dimms_installable == 2) {
 			if (dimm_count == 1) {
 				/* 1 DIMM detected */
-				if ((MemClkFreq == 0x4) || (MemClkFreq == 0x6)) {
+				if ((mem_clk_freq == 0x4) || (mem_clk_freq == 0x6)) {
 					/* DDR3-667 - DDR3-800 */
 					control_code = 0x1;
-				} else if ((MemClkFreq >= 0xa) && (MemClkFreq <= 0x12)) {
+				} else if ((mem_clk_freq >= 0xa) && (mem_clk_freq <= 0x12)) {
 					/* DDR3-1066 - DDR3-1600 */
 					if (num_registers == 1) {
 						control_code = 0x0;
@@ -65,7 +65,7 @@ static u8 fam15h_rdimm_rc2_ibt_code(struct DCTStatStruc *p_dct_stat, u8 dct)
 					control_code = 0x8;
 				}
 			}
-		} else if (MaxDimmsInstallable == 3) {
+		} else if (max_dimms_installable == 3) {
 			/* TODO
 			 * 3 DIMM/channel support unimplemented
 			 */
@@ -73,28 +73,28 @@ static u8 fam15h_rdimm_rc2_ibt_code(struct DCTStatStruc *p_dct_stat, u8 dct)
 	} else if (package_type == PT_C3) {
 		/* Socket C32 */
 		/* Fam15h BKDG Rev. 3.14 section 2.10.5.7.1.2.1 Table 86 */
-		if (MaxDimmsInstallable == 1) {
-			if ((MemClkFreq == 0x4) || (MemClkFreq == 0x6)) {
+		if (max_dimms_installable == 1) {
+			if ((mem_clk_freq == 0x4) || (mem_clk_freq == 0x6)) {
 				/* DDR3-667 - DDR3-800 */
 				control_code = 0x1;
-			} else if ((MemClkFreq == 0xa) || (MemClkFreq == 0xe)) {
+			} else if ((mem_clk_freq == 0xa) || (mem_clk_freq == 0xe)) {
 				/* DDR3-1066 - DDR3-1333 */
 				if (num_registers == 1) {
 					control_code = 0x0;
 				} else {
 					control_code = 0x1;
 				}
-			} else if ((MemClkFreq == 0x12) || (MemClkFreq == 0x16)) {
+			} else if ((mem_clk_freq == 0x12) || (mem_clk_freq == 0x16)) {
 				/* DDR3-1600 - DDR3-1866 */
 				control_code = 0x0;
 			}
-		} else if (MaxDimmsInstallable == 2) {
+		} else if (max_dimms_installable == 2) {
 			if (dimm_count == 1) {
 				/* 1 DIMM detected */
-				if ((MemClkFreq == 0x4) || (MemClkFreq == 0x6)) {
+				if ((mem_clk_freq == 0x4) || (mem_clk_freq == 0x6)) {
 					/* DDR3-667 - DDR3-800 */
 					control_code = 0x1;
-				} else if ((MemClkFreq >= 0xa) && (MemClkFreq <= 0x12)) {
+				} else if ((mem_clk_freq >= 0xa) && (mem_clk_freq <= 0x12)) {
 					/* DDR3-1066 - DDR3-1600 */
 					if (num_registers == 1) {
 						control_code = 0x0;
@@ -110,7 +110,7 @@ static u8 fam15h_rdimm_rc2_ibt_code(struct DCTStatStruc *p_dct_stat, u8 dct)
 					control_code = 0x8;
 				}
 			}
-		} else if (MaxDimmsInstallable == 3) {
+		} else if (max_dimms_installable == 3) {
 			/* TODO
 			 * 3 DIMM/channel support unimplemented
 			 */
@@ -169,108 +169,108 @@ static u32 rc_word_value_to_ctl_bits(u32 value) {
 }
 
 static u32 mct_ControlRC(struct MCTStatStruc *p_mct_stat,
-			struct DCTStatStruc *p_dct_stat, u8 dct, u32 MrsChipSel, u32 CtrlWordNum)
+			struct DCTStatStruc *p_dct_stat, u8 dct, u32 mrs_chip_sel, u32 ctrl_word_num)
 {
-	u8 Dimms, DimmNum;
+	u8 dimms, dimm_num;
 	u32 val;
 	u8 ddr_voltage_index;
 	u16 mem_freq;
 	u8 package_type = mct_get_nv_bits(NV_PACK_TYPE);
-	u8 MaxDimmsInstallable = mct_get_nv_bits(NV_MAX_DIMMS_PER_CH);
+	u8 max_dimms_installable = mct_get_nv_bits(NV_MAX_DIMMS_PER_CH);
 
-	DimmNum = (MrsChipSel >> rc_word_chip_select_lower_bit()) & 0x7;
+	dimm_num = (mrs_chip_sel >> rc_word_chip_select_lower_bit()) & 0x7;
 
 	if (dct == 1)
-		DimmNum++;
+		dimm_num++;
 
 	mem_freq = memclk_to_freq(p_dct_stat->dimm_auto_speed);
-	Dimms = p_dct_stat->ma_dimms[dct];
+	dimms = p_dct_stat->ma_dimms[dct];
 
 	ddr_voltage_index = dct_ddr_voltage_index(p_dct_stat, dct);
 
 	val = 0;
-	if (CtrlWordNum == 0)
+	if (ctrl_word_num == 0)
 		val = 0x2;
-	else if (CtrlWordNum == 1) {
-		if (!((p_dct_stat->dimm_dr_present | p_dct_stat->dimm_qr_present) & (1 << DimmNum)))
+	else if (ctrl_word_num == 1) {
+		if (!((p_dct_stat->dimm_dr_present | p_dct_stat->dimm_qr_present) & (1 << dimm_num)))
 			val = 0xc; /* if single rank, set DBA1 and DBA0 */
-	} else if (CtrlWordNum == 2) {
+	} else if (ctrl_word_num == 2) {
 		if (is_fam15h()) {
 			val = (fam15h_rdimm_rc2_ibt_code(p_dct_stat, dct) & 0x1) << 2;
 		} else {
 			if (package_type == PT_GR) {
 				/* Socket G34 */
-				if (MaxDimmsInstallable == 2) {
-					if (Dimms > 1)
+				if (max_dimms_installable == 2) {
+					if (dimms > 1)
 						val = 0x4;
 				}
 			}
 			else if (package_type == PT_C3) {
 				/* Socket C32 */
-				if (MaxDimmsInstallable == 2) {
-					if (Dimms > 1)
+				if (max_dimms_installable == 2) {
+					if (dimms > 1)
 						val = 0x4;
 				}
 			}
 		}
-	} else if (CtrlWordNum == 3) {
-		val = (p_dct_stat->ctrl_wrd_3 >> (DimmNum << 2)) & 0xff;
-	} else if (CtrlWordNum == 4) {
-		val = (p_dct_stat->ctrl_wrd_4 >> (DimmNum << 2)) & 0xff;
-	} else if (CtrlWordNum == 5) {
-		val = (p_dct_stat->ctrl_wrd_5 >> (DimmNum << 2)) & 0xff;
-	} else if (CtrlWordNum == 6) {
-		val = ((p_dct_stat->spd_data.spd_bytes[DimmNum][72] & 0xf) >> (DimmNum << 2)) & 0xff;
-	} else if (CtrlWordNum == 7) {
-		val = (((p_dct_stat->spd_data.spd_bytes[DimmNum][72] >> 4) & 0xf) >> (DimmNum << 2)) & 0xff;
-	} else if (CtrlWordNum == 8) {
+	} else if (ctrl_word_num == 3) {
+		val = (p_dct_stat->ctrl_wrd_3 >> (dimm_num << 2)) & 0xff;
+	} else if (ctrl_word_num == 4) {
+		val = (p_dct_stat->ctrl_wrd_4 >> (dimm_num << 2)) & 0xff;
+	} else if (ctrl_word_num == 5) {
+		val = (p_dct_stat->ctrl_wrd_5 >> (dimm_num << 2)) & 0xff;
+	} else if (ctrl_word_num == 6) {
+		val = ((p_dct_stat->spd_data.spd_bytes[dimm_num][72] & 0xf) >> (dimm_num << 2)) & 0xff;
+	} else if (ctrl_word_num == 7) {
+		val = (((p_dct_stat->spd_data.spd_bytes[dimm_num][72] >> 4) & 0xf) >> (dimm_num << 2)) & 0xff;
+	} else if (ctrl_word_num == 8) {
 		if (is_fam15h()) {
 			val = (fam15h_rdimm_rc2_ibt_code(p_dct_stat, dct) & 0xe) >> 1;
 		} else {
 			if (package_type == PT_GR) {
 				/* Socket G34 */
-				if (MaxDimmsInstallable == 2) {
+				if (max_dimms_installable == 2) {
 					val = 0x0;
 				}
 			}
 			else if (package_type == PT_C3) {
 				/* Socket C32 */
-				if (MaxDimmsInstallable == 2) {
+				if (max_dimms_installable == 2) {
 					val = 0x0;
 				}
 			}
 		}
-	} else if (CtrlWordNum == 9) {
+	} else if (ctrl_word_num == 9) {
 		val = 0xd;	/* DBA1, DBA0, DA3 = 0 */
-	} else if (CtrlWordNum == 10) {
+	} else if (ctrl_word_num == 10) {
 		val = 0x0;	/* Lowest operating frequency */
-	} else if (CtrlWordNum == 11) {
+	} else if (ctrl_word_num == 11) {
 		if (ddr_voltage_index & 0x4)
 			val = 0x2;	/* 1.25V */
 		else if (ddr_voltage_index & 0x2)
 			val = 0x1;	/* 1.35V */
 		else
 			val = 0x0;	/* 1.5V */
-	} else if (CtrlWordNum == 12) {
-		val = ((p_dct_stat->spd_data.spd_bytes[DimmNum][75] & 0xf) >> (DimmNum << 2)) & 0xff;
-	} else if (CtrlWordNum == 13) {
-		val = (((p_dct_stat->spd_data.spd_bytes[DimmNum][75] >> 4) & 0xf) >> (DimmNum << 2)) & 0xff;
-	} else if (CtrlWordNum == 14) {
-		val = ((p_dct_stat->spd_data.spd_bytes[DimmNum][76] & 0xf) >> (DimmNum << 2)) & 0xff;
-	} else if (CtrlWordNum == 15) {
-		val = (((p_dct_stat->spd_data.spd_bytes[DimmNum][76] >> 4) & 0xf) >> (DimmNum << 2)) & 0xff;
+	} else if (ctrl_word_num == 12) {
+		val = ((p_dct_stat->spd_data.spd_bytes[dimm_num][75] & 0xf) >> (dimm_num << 2)) & 0xff;
+	} else if (ctrl_word_num == 13) {
+		val = (((p_dct_stat->spd_data.spd_bytes[dimm_num][75] >> 4) & 0xf) >> (dimm_num << 2)) & 0xff;
+	} else if (ctrl_word_num == 14) {
+		val = ((p_dct_stat->spd_data.spd_bytes[dimm_num][76] & 0xf) >> (dimm_num << 2)) & 0xff;
+	} else if (ctrl_word_num == 15) {
+		val = (((p_dct_stat->spd_data.spd_bytes[dimm_num][76] >> 4) & 0xf) >> (dimm_num << 2)) & 0xff;
 	}
 	val &= 0xf;
 
-	printk(BIOS_SPEW, "%s: Preparing to send DCT %d DIMM %d RC%d: %02x\n", __func__, dct, DimmNum >> 1, CtrlWordNum, val);
+	printk(BIOS_SPEW, "%s: Preparing to send DCT %d DIMM %d RC%d: %02x\n", __func__, dct, dimm_num >> 1, ctrl_word_num, val);
 
-	val = MrsChipSel | rc_word_value_to_ctl_bits(val);
-	val |= rc_word_address_to_ctl_bits(CtrlWordNum);
+	val = mrs_chip_sel | rc_word_value_to_ctl_bits(val);
+	val |= rc_word_address_to_ctl_bits(ctrl_word_num);
 
 	return val;
 }
 
-static void mct_SendCtrlWrd(struct MCTStatStruc *p_mct_stat,
+static void mct_send_ctrl_wrd(struct MCTStatStruc *p_mct_stat,
 			struct DCTStatStruc *p_dct_stat, u8 dct, u32 val)
 {
 	u32 dev = p_dct_stat->dev_dct;
@@ -287,7 +287,7 @@ static void mct_SendCtrlWrd(struct MCTStatStruc *p_mct_stat,
 void mct_dram_control_reg_init_d(struct MCTStatStruc *p_mct_stat,
 				struct DCTStatStruc *p_dct_stat, u8 dct)
 {
-	u8 MrsChipSel;
+	u8 mrs_chip_sel;
 	u32 dev = p_dct_stat->dev_dct;
 	u32 val, cw;
 
@@ -302,12 +302,12 @@ void mct_dram_control_reg_init_d(struct MCTStatStruc *p_mct_stat,
 	if (p_dct_stat->ganged_mode & 1)
 		p_dct_stat->cs_present = p_dct_stat->cs_present_dct[0];
 
-	for (MrsChipSel = 0; MrsChipSel < 8; MrsChipSel += 2) {
-		if (p_dct_stat->cs_present & (1 << MrsChipSel)) {
+	for (mrs_chip_sel = 0; mrs_chip_sel < 8; mrs_chip_sel += 2) {
+		if (p_dct_stat->cs_present & (1 << mrs_chip_sel)) {
 			val = Get_NB32_DCT(dev, dct, 0xa8);
 			val &= ~(0xff << 8);
 
-			switch (MrsChipSel) {
+			switch (mrs_chip_sel) {
 				case 0:
 				case 1:
 					val |= 3 << 8;
@@ -330,16 +330,16 @@ void mct_dram_control_reg_init_d(struct MCTStatStruc *p_mct_stat,
 
 			if (is_fam15h()) {
 				for (cw = 0; cw <=15; cw ++) {
-					val = mct_ControlRC(p_mct_stat, p_dct_stat, dct, MrsChipSel << rc_word_chip_select_lower_bit(), cw);
-					mct_SendCtrlWrd(p_mct_stat, p_dct_stat, dct, val);
+					val = mct_ControlRC(p_mct_stat, p_dct_stat, dct, mrs_chip_sel << rc_word_chip_select_lower_bit(), cw);
+					mct_send_ctrl_wrd(p_mct_stat, p_dct_stat, dct, val);
 					if ((cw == 2) || (cw == 8) || (cw == 10))
 						precise_ndelay_fam15(p_mct_stat, 6000);
 				}
 			} else {
 				for (cw = 0; cw <=15; cw ++) {
 					mct_wait(1600);
-					val = mct_ControlRC(p_mct_stat, p_dct_stat, dct, MrsChipSel << rc_word_chip_select_lower_bit(), cw);
-					mct_SendCtrlWrd(p_mct_stat, p_dct_stat, dct, val);
+					val = mct_ControlRC(p_mct_stat, p_dct_stat, dct, mrs_chip_sel << rc_word_chip_select_lower_bit(), cw);
+					mct_send_ctrl_wrd(p_mct_stat, p_dct_stat, dct, val);
 				}
 			}
 		}
@@ -353,8 +353,8 @@ void mct_dram_control_reg_init_d(struct MCTStatStruc *p_mct_stat,
 void freq_chg_ctrl_wrd(struct MCTStatStruc *p_mct_stat,
 			struct DCTStatStruc *p_dct_stat, u8 dct)
 {
-	u32 SaveSpeed = p_dct_stat->dimm_auto_speed;
-	u32 MrsChipSel;
+	u32 save_speed = p_dct_stat->dimm_auto_speed;
+	u32 mrs_chip_sel;
 	u32 dev = p_dct_stat->dev_dct;
 	u32 val;
 	u16 mem_freq;
@@ -365,13 +365,13 @@ void freq_chg_ctrl_wrd(struct MCTStatStruc *p_mct_stat,
 
 	p_dct_stat->dimm_auto_speed = p_dct_stat->target_freq;
 	mem_freq = memclk_to_freq(p_dct_stat->target_freq);
-	for (MrsChipSel = 0; MrsChipSel < 8; MrsChipSel += 2) {
-		if (p_dct_stat->cs_present & (1 << MrsChipSel)) {
+	for (mrs_chip_sel = 0; mrs_chip_sel < 8; mrs_chip_sel += 2) {
+		if (p_dct_stat->cs_present & (1 << mrs_chip_sel)) {
 			/* 2. Program F2x[1, 0]A8[CtrlWordCS]=bit mask for target chip selects. */
 			val = Get_NB32_DCT(dev, dct, 0xa8);
 			val &= ~(0xff << 8);
 
-			switch (MrsChipSel) {
+			switch (mrs_chip_sel) {
 				case 0:
 				case 1:
 					val |= 3 << 8;
@@ -413,9 +413,9 @@ void freq_chg_ctrl_wrd(struct MCTStatStruc *p_mct_stat,
 					break;
 			}
 
-			printk(BIOS_SPEW, "Preparing to send DCT %d DIMM %d RC%d: %02x (F2xA8: %08x)\n", dct, MrsChipSel >> 1, 10, freq_ctl_val, val);
+			printk(BIOS_SPEW, "Preparing to send DCT %d DIMM %d RC%d: %02x (F2xA8: %08x)\n", dct, mrs_chip_sel >> 1, 10, freq_ctl_val, val);
 
-			mct_SendCtrlWrd(p_mct_stat, p_dct_stat, dct, (MrsChipSel << rc_word_chip_select_lower_bit()) | rc_word_address_to_ctl_bits(10) | rc_word_value_to_ctl_bits(freq_ctl_val));
+			mct_send_ctrl_wrd(p_mct_stat, p_dct_stat, dct, (mrs_chip_sel << rc_word_chip_select_lower_bit()) | rc_word_address_to_ctl_bits(10) | rc_word_value_to_ctl_bits(freq_ctl_val));
 
 			if (is_fam15h())
 				precise_ndelay_fam15(p_mct_stat, 6000);
@@ -423,8 +423,8 @@ void freq_chg_ctrl_wrd(struct MCTStatStruc *p_mct_stat,
 				mct_wait(1600);
 
 			/* Resend control word 2 */
-			val = mct_ControlRC(p_mct_stat, p_dct_stat, dct, MrsChipSel << rc_word_chip_select_lower_bit(), 2);
-			mct_SendCtrlWrd(p_mct_stat, p_dct_stat, dct, val);
+			val = mct_ControlRC(p_mct_stat, p_dct_stat, dct, mrs_chip_sel << rc_word_chip_select_lower_bit(), 2);
+			mct_send_ctrl_wrd(p_mct_stat, p_dct_stat, dct, val);
 
 			if (is_fam15h())
 				precise_ndelay_fam15(p_mct_stat, 6000);
@@ -432,8 +432,8 @@ void freq_chg_ctrl_wrd(struct MCTStatStruc *p_mct_stat,
 				mct_wait(1600);
 
 			/* Resend control word 8 */
-			val = mct_ControlRC(p_mct_stat, p_dct_stat, dct, MrsChipSel << rc_word_chip_select_lower_bit(), 8);
-			mct_SendCtrlWrd(p_mct_stat, p_dct_stat, dct, val);
+			val = mct_ControlRC(p_mct_stat, p_dct_stat, dct, mrs_chip_sel << rc_word_chip_select_lower_bit(), 8);
+			mct_send_ctrl_wrd(p_mct_stat, p_dct_stat, dct, val);
 
 			if (is_fam15h())
 				precise_ndelay_fam15(p_mct_stat, 6000);
@@ -441,5 +441,5 @@ void freq_chg_ctrl_wrd(struct MCTStatStruc *p_mct_stat,
 				mct_wait(1600);
 		}
 	}
-	p_dct_stat->dimm_auto_speed = SaveSpeed;
+	p_dct_stat->dimm_auto_speed = save_speed;
 }
