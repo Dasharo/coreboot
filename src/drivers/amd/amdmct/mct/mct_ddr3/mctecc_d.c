a470b320fb99f6913abbd5362c2bf397322dda50
Igor Bagnucki <igor.bagnucki@3mdeb.com>
2022-01-24 13:30:28 +0100

    mct_ddr3/mctecc_d.c: Fix names
    Signed-off-by: Igor Bagnucki <igor.bagnucki@3mdeb.com>

--- src/drivers/amd/amdmct/mct/mct_ddr3/mctecc_d.c
+++ src/drivers/amd/amdmct/mct/mct_ddr3/mctecc_d.c
@@ -20,8 +20,8 @@ static u8 is_dram_ecc_en_d(struct DCTStatStruc *p_dct_stat);
  * Order that items are set:
  *  1. eccen bit in NB
  *  2. Scrub Base
- *  3. Temp Node Base
- *  4. Temp Node Limit
+ *  3. Temp node Base
+ *  4. Temp node Limit
  *  5. Redir bit in NB
  *  6. Scrub CTL
  *
@@ -51,7 +51,7 @@ static u8 is_dram_ecc_en_d(struct DCTStatStruc *p_dct_stat);
  * during setup of scrub Base, we determine how much memory and which node has
  * the largest memory installed.
  *
- * Scrubbing should not ordinarily be enabled on a Node with a chip-select gap
+ * Scrubbing should not ordinarily be enabled on a node with a chip-select gap
  * (aka SW memhole, cs hoisting, etc..).To init ECC memory on this node, the
  * scrubber is used in two steps.  First, the Dram Limit for the node is adjusted
  * down to the bottom of the gap, and that ECC dram is initialized.  Second, the
@@ -60,15 +60,15 @@ static u8 is_dram_ecc_en_d(struct DCTStatStruc *p_dct_stat);
  */
 u8 ecc_init_d(struct MCTStatStruc *p_mct_stat, struct DCTStatStruc *p_dct_stat_a)
 {
-	u8 Node;
-	u8 AllECC;
-	u16 OB_NBECC;
-	u32 curBase;
-	u16 OB_ECCRedir;
-	u32 LDramECC;
-	u32 OF_ScrubCTL;
-	u16 OB_ChipKill;
-	u8 MemClrECC;
+	u8 node;
+	u8 all_ecc;
+	u16 ob_nb_ecc;
+	u32 cur_base;
+	u16 ob_ecc_redir;
+	u32 l_dram_ecc;
+	u32 of_scrub_ctl;
+	u16 ob_chip_kill;
+	u8 mem_clr_ecc;
 
 	u32 dev;
 	u32 reg;
@@ -83,37 +83,37 @@ u8 ecc_init_d(struct MCTStatStruc *p_mct_stat, struct DCTStatStruc *p_dct_stat_a
 
 	/* Construct these booleans, based on setup options, for easy handling
 	later in this procedure */
-	OB_NBECC = mct_get_nv_bits(NV_NBECC);			/* MCA ECC (MCE) enable bit */
+	ob_nb_ecc = mct_get_nv_bits(NV_NBECC);			/* MCA ECC (MCE) enable bit */
 
-	OB_ECCRedir =  mct_get_nv_bits(NV_ECC_REDIR);		/* ECC Redirection */
+	ob_ecc_redir =  mct_get_nv_bits(NV_ECC_REDIR);		/* ECC Redirection */
 
-	OB_ChipKill = mct_get_nv_bits(NV_CHIP_KILL);		/* ECC Chip-kill mode */
-	OF_ScrubCTL = 0;					/* Scrub CTL for Dcache, L2, and dram */
+	ob_chip_kill = mct_get_nv_bits(NV_CHIP_KILL);		/* ECC Chip-kill mode */
+	of_scrub_ctl = 0;					/* Scrub CTL for Dcache, L2, and dram */
 
 	if (!is_fam15h()) {
 		nvbits = mct_get_nv_bits(NV_DC_BK_SCRUB);
 		/* mct_adjust_scrub_d(p_dct_stat_a, &nvbits); */	/* Need not adjust */
-		OF_ScrubCTL |= (u32) nvbits << 16;
+		of_scrub_ctl |= (u32) nvbits << 16;
 
 		nvbits = mct_get_nv_bits(NV_L2_BK_SCRUB);
-		OF_ScrubCTL |= (u32) nvbits << 8;
+		of_scrub_ctl |= (u32) nvbits << 8;
 	}
 
 	nvbits = mct_get_nv_bits(NV_L3_BK_SCRUB);
-	OF_ScrubCTL |= (nvbits & 0x1f) << 24;			/* L3Scrub = NV_L3_BK_SCRUB */
+	of_scrub_ctl |= (nvbits & 0x1f) << 24;			/* L3Scrub = NV_L3_BK_SCRUB */
 
 	nvbits = mct_get_nv_bits(NV_DRAM_BK_SCRUB);
-	OF_ScrubCTL |= nvbits;					/* DramScrub = NV_DRAM_BK_SCRUB */
+	of_scrub_ctl |= nvbits;					/* DramScrub = NV_DRAM_BK_SCRUB */
 
 	/* Prevent lockups on DRAM errors during ECC init */
-	for (Node = 0; Node < MAX_NODES_SUPPORTED; Node++) {
+	for (node = 0; node < MAX_NODES_SUPPORTED; node++) {
 		struct DCTStatStruc *p_dct_stat;
-		p_dct_stat = p_dct_stat_a + Node;
+		p_dct_stat = p_dct_stat_a + node;
 
-		if (node_present_d(Node)) {
+		if (node_present_d(node)) {
 			dword = get_nb32(p_dct_stat->dev_nbmisc, 0x44);
-			sync_flood_on_dram_err[Node] = (dword >> 30) & 0x1;
-			sync_flood_on_any_uc_err[Node] = (dword >> 21) & 0x1;
+			sync_flood_on_dram_err[node] = (dword >> 30) & 0x1;
+			sync_flood_on_any_uc_err[node] = (dword >> 21) & 0x1;
 			dword &= ~(0x1 << 30);
 			dword &= ~(0x1 << 21);
 			set_nb32(p_dct_stat->dev_nbmisc, 0x44, dword);
@@ -131,36 +131,36 @@ u8 ecc_init_d(struct MCTStatStruc *p_mct_stat, struct DCTStatStruc *p_dct_stat_a
 		}
 	}
 
-	AllECC = 1;
-	MemClrECC = 0;
-	for (Node = 0; Node < MAX_NODES_SUPPORTED; Node++) {
+	all_ecc = 1;
+	mem_clr_ecc = 0;
+	for (node = 0; node < MAX_NODES_SUPPORTED; node++) {
 		struct DCTStatStruc *p_dct_stat;
-		p_dct_stat = p_dct_stat_a + Node;
-		LDramECC = 0;
-		if (node_present_d(Node)) {	/*If Node is present */
+		p_dct_stat = p_dct_stat_a + node;
+		l_dram_ecc = 0;
+		if (node_present_d(node)) {	/*If node is present */
 			dev = p_dct_stat->dev_map;
-			reg = 0x40+(Node << 3);	/* Dram Base Node 0 + index */
+			reg = 0x40+(node << 3);	/* Dram Base node 0 + index */
 			val = get_nb32(dev, reg);
 
 			/* WE/RE is checked */
-			if ((val & 3) == 3) {	/* Node has dram populated */
+			if ((val & 3) == 3) {	/* node has dram populated */
 				/* Negate 'all nodes/dimms ECC' flag if non ecc
 				   memory populated */
 				if (p_dct_stat->status & (1 << SB_ECC_DIMMS)) {
-					LDramECC = is_dram_ecc_en_d(p_dct_stat);
+					l_dram_ecc = is_dram_ecc_en_d(p_dct_stat);
 					if (p_dct_stat->err_code != SC_RUNNING_OK) {
 						p_dct_stat->status &=  ~(1 << SB_ECC_DIMMS);
-						if (!OB_NBECC) {
+						if (!ob_nb_ecc) {
 							p_dct_stat->err_status |= (1 << SB_DRAM_ECC_DIS);
 						}
-						AllECC = 0;
-						LDramECC = 0;
+						all_ecc = 0;
+						l_dram_ecc = 0;
 					}
 				} else {
-					AllECC = 0;
+					all_ecc = 0;
 				}
-				if (LDramECC) {	/* if ECC is enabled on this dram */
-					if (OB_NBECC) {
+				if (l_dram_ecc) {	/* if ECC is enabled on this dram */
+					if (ob_nb_ecc) {
 						mct_enable_dat_intlv_d(p_mct_stat, p_dct_stat);
 						val = get_nb32(p_dct_stat->dev_dct, 0x110);
 						val |= 1 << 5;	/* DctDatIntLv = 1 */
@@ -171,45 +171,45 @@ u8 ecc_init_d(struct MCTStatStruc *p_mct_stat, struct DCTStatStruc *p_dct_stat_a
 						val |= 1 << 22;	/* EccEn */
 						set_nb32(dev, reg, val);
 						dct_mem_clr_init_d(p_mct_stat, p_dct_stat);
-						MemClrECC = 1;
-						printk(BIOS_DEBUG, "  ECC enabled on node: %02x\n", Node);
+						mem_clr_ecc = 1;
+						printk(BIOS_DEBUG, "  ECC enabled on node: %02x\n", node);
 					}
 				}	/* this node has ECC enabled dram */
 
-				if (MemClrECC) {
+				if (mem_clr_ecc) {
 					dct_mem_clr_sync_d(p_mct_stat, p_dct_stat);
 				}
 			} else {
-				LDramECC = 0;
-			}	/* Node has Dram */
-		}	/* if Node present */
+				l_dram_ecc = 0;
+			}	/* node has Dram */
+		}	/* if node present */
 	}
 
-	if (AllECC)
+	if (all_ecc)
 		p_mct_stat->g_status |= 1 << GSB_ECCDIMMS;
 	else
 		p_mct_stat->g_status &= ~(1 << GSB_ECCDIMMS);
 
 	/* Program the Dram BKScrub CTL to the proper (user selected) value.*/
 	/* Reset MC4_STS. */
-	for (Node = 0; Node < MAX_NODES_SUPPORTED; Node++) {
+	for (node = 0; node < MAX_NODES_SUPPORTED; node++) {
 		struct DCTStatStruc *p_dct_stat;
-		p_dct_stat = p_dct_stat_a + Node;
-		LDramECC = 0;
-		if (node_present_d(Node)) {	/* If Node is present */
-			reg = 0x40+(Node << 3);	/* Dram Base Node 0 + index */
+		p_dct_stat = p_dct_stat_a + node;
+		l_dram_ecc = 0;
+		if (node_present_d(node)) {	/* If node is present */
+			reg = 0x40+(node << 3);	/* Dram Base node 0 + index */
 			val = get_nb32(p_dct_stat->dev_map, reg);
-			curBase = val & 0xffff0000;
+			cur_base = val & 0xffff0000;
 			/*WE/RE is checked because memory config may have been */
-			if ((val & 3) == 3) {	/* Node has dram populated */
+			if ((val & 3) == 3) {	/* node has dram populated */
 				if (is_dram_ecc_en_d(p_dct_stat)) {	/* if ECC is enabled on this dram */
 					dev = p_dct_stat->dev_nbmisc;
-					val = curBase << 8;
-					if (OB_ECCRedir) {
+					val = cur_base << 8;
+					if (ob_ecc_redir) {
 						val |= (1 << 0);		/* Enable redirection */
 					}
 					set_nb32(dev, 0x5c, val);		/* Dram Scrub Addr Low */
-					val = curBase >> 24;
+					val = cur_base >> 24;
 					set_nb32(dev, 0x60, val);		/* Dram Scrub Addr High */
 
 					/* Set scrub rate controls */
@@ -217,10 +217,10 @@ u8 ecc_init_d(struct MCTStatStruc *p_mct_stat, struct DCTStatStruc *p_dct_stat_a
 						/* Erratum 505 */
 						fam15h_switch_dct(p_dct_stat->dev_map, 0);
 					}
-					set_nb32(dev, 0x58, OF_ScrubCTL);	/* Scrub Control */
+					set_nb32(dev, 0x58, of_scrub_ctl);	/* Scrub Control */
 					if (is_fam15h()) {
 						fam15h_switch_dct(p_dct_stat->dev_map, 1);	/* Erratum 505 */
-						set_nb32(dev, 0x58, OF_ScrubCTL);		/* Scrub Control */
+						set_nb32(dev, 0x58, of_scrub_ctl);		/* Scrub Control */
 						fam15h_switch_dct(p_dct_stat->dev_map, 0);	/* Erratum 505 */
 					}
 
@@ -229,7 +229,7 @@ u8 ecc_init_d(struct MCTStatStruc *p_mct_stat, struct DCTStatStruc *p_dct_stat_a
 						 * divide by 16 when Dcache scrubber or
 						 * L2 scrubber is enabled.
 						 */
-						if ((OF_ScrubCTL & (0x1F << 16)) || (OF_ScrubCTL & (0x1F << 8))) {
+						if ((of_scrub_ctl & (0x1F << 16)) || (of_scrub_ctl & (0x1F << 8))) {
 							val = get_nb32(dev, 0x84);
 							if ((val & 0xE0000000) > 0x80000000) {	/* Get F3x84h[31:29]ClkDivisor for C1 */
 								val &= 0x1FFFFFFF;	/* If ClkDivisor is deeper than divide-by-16 */
@@ -247,22 +247,22 @@ u8 ecc_init_d(struct MCTStatStruc *p_mct_stat, struct DCTStatStruc *p_dct_stat_a
 						pci_write_config32(p_dct_stat->dev_nbmisc, 0xd4, val);
 					}
 				}	/* this node has ECC enabled dram */
-			}	/*Node has Dram */
-		}	/*if Node present */
+			}	/*node has Dram */
+		}	/*if node present */
 	}
 
 	/* Restore previous MCA error handling settings */
-	for (Node = 0; Node < MAX_NODES_SUPPORTED; Node++) {
+	for (node = 0; node < MAX_NODES_SUPPORTED; node++) {
 		struct DCTStatStruc *p_dct_stat;
-		p_dct_stat = p_dct_stat_a + Node;
+		p_dct_stat = p_dct_stat_a + node;
 
-		if (node_present_d(Node)) {
+		if (node_present_d(node)) {
 			dev = p_dct_stat->dev_map;
-			reg = 0x40 + (Node << 3);	/* Dram Base Node 0 + index */
+			reg = 0x40 + (node << 3);	/* Dram Base node 0 + index */
 			val = get_nb32(dev, reg);
 
 			/* WE/RE is checked */
-			if ((val & 0x3) == 0x3) {	/* Node has dram populated */
+			if ((val & 0x3) == 0x3) {	/* node has dram populated */
 				u32 mc4_status_high = pci_read_config32(p_dct_stat->dev_nbmisc, 0x4c);
 				u32 mc4_status_low = pci_read_config32(p_dct_stat->dev_nbmisc, 0x48);
 				if ((mc4_status_high & (0x1 << 31)) && (mc4_status_high != 0xffffffff)) {
@@ -276,8 +276,8 @@ u8 ecc_init_d(struct MCTStatStruc *p_mct_stat, struct DCTStatStruc *p_dct_stat_a
 
 				/* Restore previous MCA error handling settings */
 				dword = get_nb32(p_dct_stat->dev_nbmisc, 0x44);
-				dword |= (sync_flood_on_dram_err[Node] & 0x1) << 30;
-				dword |= (sync_flood_on_any_uc_err[Node] & 0x1) << 21;
+				dword |= (sync_flood_on_dram_err[node] & 0x1) << 30;
+				dword |= (sync_flood_on_any_uc_err[node] & 0x1) << 21;
 				set_nb32(p_dct_stat->dev_nbmisc, 0x44, dword);
 			}
 		}
@@ -287,36 +287,36 @@ u8 ecc_init_d(struct MCTStatStruc *p_mct_stat, struct DCTStatStruc *p_dct_stat_a
 		set_sync_on_un_ecc_en_d(p_mct_stat, p_dct_stat_a);
 
 	mct_hook_after_ecc();
-	for (Node = 0; Node < MAX_NODES_SUPPORTED; Node++) {
+	for (node = 0; node < MAX_NODES_SUPPORTED; node++) {
 		struct DCTStatStruc *p_dct_stat;
-		p_dct_stat = p_dct_stat_a + Node;
-		if (node_present_d(Node)) {
-			printk(BIOS_DEBUG, "ECCInit: Node %02x\n", Node);
+		p_dct_stat = p_dct_stat_a + node;
+		if (node_present_d(node)) {
+			printk(BIOS_DEBUG, "ECCInit: node %02x\n", node);
 			printk(BIOS_DEBUG, "ECCInit: status %x\n", p_dct_stat->status);
 			printk(BIOS_DEBUG, "ECCInit: err_status %x\n", p_dct_stat->err_status);
 			printk(BIOS_DEBUG, "ECCInit: err_code %x\n", p_dct_stat->err_code);
 			printk(BIOS_DEBUG, "ECCInit: Done\n");
 		}
 	}
-	return MemClrECC;
+	return mem_clr_ecc;
 }
 
 static void set_sync_on_un_ecc_en_d(struct MCTStatStruc *p_mct_stat,
 				struct DCTStatStruc *p_dct_stat_a)
 {
-	u32 Node;
+	u32 node;
 	u32 reg;
 	u32 dev;
 	u32 val;
 
-	for (Node = 0; Node < MAX_NODES_SUPPORTED; Node++) {
+	for (node = 0; node < MAX_NODES_SUPPORTED; node++) {
 		struct DCTStatStruc *p_dct_stat;
-		p_dct_stat = p_dct_stat_a + Node;
-		if (node_present_d(Node)) {	/* If Node is present*/
-			reg = 0x40+(Node << 3);	/* Dram Base Node 0 + index*/
+		p_dct_stat = p_dct_stat_a + node;
+		if (node_present_d(node)) {	/* If node is present*/
+			reg = 0x40+(node << 3);	/* Dram Base node 0 + index*/
 			val = get_nb32(p_dct_stat->dev_map, reg);
 			/*WE/RE is checked because memory config may have been*/
-			if ((val & 3) == 3) {	/* Node has dram populated*/
+			if ((val & 3) == 3) {	/* node has dram populated*/
 				if (is_dram_ecc_en_d(p_dct_stat)) {
 					/*if ECC is enabled on this dram*/
 					dev = p_dct_stat->dev_nbmisc;
@@ -325,8 +325,8 @@ static void set_sync_on_un_ecc_en_d(struct MCTStatStruc *p_mct_stat,
 					val |= (1 << SYNC_ON_UC_ECC_EN);
 					set_nb32(dev, reg, val);
 				}
-			}	/* Node has Dram*/
-		}	/* if Node present*/
+			}	/* node has Dram*/
+		}	/* if node present*/
 	}
 }
 
@@ -337,7 +337,7 @@ static u8 is_dram_ecc_en_d(struct DCTStatStruc *p_dct_stat)
 	u8 i;
 	u32 dev = p_dct_stat->dev_dct;
 	u8 ch_end;
-	u8 isDimmECCEn = 0;
+	u8 is_dimm_ecc_en = 0;
 
 	if (p_dct_stat->ganged_mode) {
 		ch_end = 1;
@@ -350,10 +350,10 @@ static u8 is_dram_ecc_en_d(struct DCTStatStruc *p_dct_stat)
 			val = Get_NB32_DCT(dev, i, reg);
 			if (val & (1 << DIMM_EC_EN)) {
 				/* set local flag 'dram ecc capable' */
-				isDimmECCEn = 1;
+				is_dimm_ecc_en = 1;
 				break;
 			}
 		}
 	}
-	return isDimmECCEn;
+	return is_dimm_ecc_en;
 }
