6607fd5427c1f768df6bf7452379ca19761866fc
Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
2022-02-06 01:24:54 +0200

    soc/power9/mcbist.c: update for second CPU
    Change-Id: I3580a3a853a5046d6b630e50958bc7783e5fbf02
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>

--- src/soc/ibm/power9/mcbist.h
+++ src/soc/ibm/power9/mcbist.h
@@ -3,10 +3,12 @@
 #ifndef __SOC_IBM_POWER9_MCBIST_H
 #define __SOC_IBM_POWER9_MCBIST_H
 
-void add_scrub(int mcs_i, int port_dimm);
-void add_fixed_pattern_write(int mcs_i, int port_dimm);
+#include <stdint.h>
 
-void mcbist_execute(int mcs_i);
-int mcbist_is_done(int mcs_i);
+void add_scrub(uint8_t chip, int mcs_i, int port_dimm);
+void add_fixed_pattern_write(uint8_t chip, int mcs_i, int port_dimm);
+
+void mcbist_execute(uint8_t chip, int mcs_i);
+int mcbist_is_done(uint8_t chip, int mcs_i);
 
 #endif /* __SOC_IBM_POWER9_MCBIST_H */
