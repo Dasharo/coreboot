138939b021336edc5cce580e4c244ea55a8a22f7
Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
2022-02-15 23:40:50 +0200

    soc/power9/istep_18_11.c: update for second CPU
    Change-Id: I6612e72b1a44e9bd50d51a10ee8632a016a89588
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>

--- src/include/cpu/power/istep_18.h
+++ src/include/cpu/power/istep_18.h
@@ -3,7 +3,9 @@
 #ifndef CPU_PPC64_ISTEP18_H
 #define CPU_PPC64_ISTEP18_H
 
-void istep_18_11(void);
+#include <stdint.h>
+
+void istep_18_11(uint8_t chips);
 void istep_18_12(void);
 
 #endif /* CPU_PPC64_ISTEP18_H */
