e9b127a1a867928f5f596c2b9a6e9a6ef9e648cb
Michał Kopeć <michal.kopec@3mdeb.com>
2022-03-04 10:17:31 +0100

    mb/clevo/ns50mu: add FTCS1000 touchpad and probe touchpads instead of enabling them all
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>

--- src/mainboard/clevo/tgl-u/mainboard.c
+++ src/mainboard/clevo/tgl-u/mainboard.c
@@ -1,8 +1,10 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <bootstate.h>
 #include <baseboard/gpio.h>
 #include <baseboard/variants.h>
 #include <device/device.h>
+#include <device/i2c_simple.h>
 #include <intelblocks/lpc_lib.h>
 #include <soc/gpio.h>
 #include <smbios.h>
@@ -29,6 +31,24 @@ static void mainboard_init(void *chip_info)
 	/* Configure MMIO window before FSP-S locks the DMI registers */
 	lpc_open_mmio_window(CONFIG_EC_CLEVO_IT5570_RAM_BASE, 0x10000);
 }
+
+static void mainboard_prune_touchpads(void *unused)
+{
+	struct device *child, *i2c_dev = pcidev_on_root(0x15, 0);
+	uint8_t data;
+
+	/*
+	 * NS5x comes with different touchpads from various vendors. Windows
+	 * gets confused when multiple touchpads are defined on the same I2C bus
+	 * in devicetree, so detect which touchpad variant is actually attached
+	 * to the mainboard and disable the other touchpads.
+	 */
+	for (child = i2c_dev->link_list->children; child; child = child->sibling)
+		if (child->path.i2c.device)
+			child->enabled = !(i2c_readb(0, child->path.i2c.device, 0, &data));
+}
+
+BOOT_STATE_INIT_ENTRY(BS_POST_DEVICE, BS_ON_EXIT, mainboard_prune_touchpads, NULL);
 
 struct chip_operations mainboard_ops = {
 	.init = mainboard_init,
