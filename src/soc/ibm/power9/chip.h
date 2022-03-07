ebc3c0792555eb1ff9b8ef6526630efd3d591e1b
Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
2022-03-08 01:23:54 +0200

    soc/power9/chip.c: allocate only necessary amount of HOMERs
    Rather than copying what Hostboot does.
    
    Change-Id: I60168789fd2e1664aa746d4cf849338a37d2e036
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>

--- src/soc/ibm/power9/chip.h
+++ src/soc/ibm/power9/chip.h
@@ -6,6 +6,6 @@
 struct soc_ibm_power9_config {
 };
 
-void build_homer_image(void *homer_bar, uint64_t nominal_freq[]);
+void build_homer_image(void *homer_bar, void *common_occ_area, uint64_t nominal_freq[]);
 
 #endif /* __SOC_CAVIUM_CN81XX_CHIP_H */
