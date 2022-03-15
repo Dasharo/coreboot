77a4cce97f62da6494a22d89f5d37f76ba4b81c5
Michał Kopeć <michal@nozomi.space>
2022-03-16 00:16:39 +0100

    superio/ite/it8625e: add initial IT8625E support

---
+++ src/superio/ite/it8625e/it8625e.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#ifndef SUPERIO_ITE_IT8625E_H
+#define SUPERIO_ITE_IT8625E_H
+
+/* Datasheet not available. The list of LDNs might not be complete. */
+
+#define IT8625E_SP1  0x01 /* Com1 */
+#define IT8625E_SP2  0x02 /* Com2 */
+#define IT8625E_PP   0x03 /* Parallel port */
+#define IT8625E_EC   0x04 /* Environment controller */
+#define IT8625E_KBCK 0x05 /* PS/2 keyboard */
+#define IT8625E_KBCM 0x06 /* PS/2 mouse */
+#define IT8625E_GPIO 0x07 /* GPIO */
+#define IT8625E_CIR  0x0a /* Consumer IR */
+
+#endif /* SUPERIO_ITE_IT8625E_H */
