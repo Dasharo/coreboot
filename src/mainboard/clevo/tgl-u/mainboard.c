f81d97ba3309c61c14541ad430539d17fe55f9c8
Michał Kopeć <michal.kopec@3mdeb.com>
2022-03-31 14:12:53 +0200

    mb/clevo/tgl-u/mainboard.c: disable AER for SSD RP
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>

--- src/mainboard/clevo/tgl-u/mainboard.c
+++ src/mainboard/clevo/tgl-u/mainboard.c
@@ -7,8 +7,15 @@
 #include <device/i2c_simple.h>
 #include <intelblocks/lpc_lib.h>
 #include <soc/gpio.h>
+#include <soc/ramstage.h>
 #include <smbios.h>
 #include <string.h>
+
+void mainboard_silicon_init_params(FSP_S_CONFIG *params)
+{
+	/* Disable AER for the SSD slot to support SSDs with buggy firmware */
+	params->CpuPcieRpAdvancedErrorReporting[0] = 0;
+}
 
 const char *smbios_system_sku(void)
 {
