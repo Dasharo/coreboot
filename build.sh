999bae54b44649325b256a69eb710112117692a1
Michał Żygowski <michal.zygowski@3mdeb.com>
2022-04-12 12:59:33 +0200

    build.sh: Append board specific options
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+docker run --rm -it -u 1000 -v $PWD:/home/coreboot/coreboot \
+        -w /home/coreboot/coreboot coreboot/coreboot-sdk:2021-09-23_b0d87f753c \
+        /bin/bash -c "make distclean"
+
+git reset --hard HEAD
+git clean -df
+
+git fetch https://review.coreboot.org/coreboot refs/changes/78/63578/1 && \
+        git format-patch -20 --stdout FETCH_HEAD |git apply
+
+cp configs/config.msi_ms7d25 .config
+
+echo "CONFIG_USE_ADLS_IOT_FSP=y" >> .config
+echo "CONFIG_TIANOCORE_BOOTSPLASH_FILE=\"bootsplash.bmp\""  >> .config
+echo "CONFIG_POWER_STATE_OFF_AFTER_FAILURE=y"  >> .config
+
 git submodule update --init --checkout
 
 if [ ! -d 3rdparty/fsp/AlderLakeFspBinPkg ]; then
@@ -12,18 +28,6 @@ if [ ! -f adl-s_microcode.bin ]; then
         echo "Extract it from vendor fw first and save as adl-s_microcode.bin"
         exit 1
 fi
-
-docker run --rm -it -u 1000 -v $PWD:/home/coreboot/coreboot \
-        -w /home/coreboot/coreboot coreboot/coreboot-sdk:2021-09-23_b0d87f753c \
-        /bin/bash -c "make distclean"
-
-git reset --hard HEAD
-git clean -df
-
-git fetch https://review.coreboot.org/coreboot refs/changes/07/63507/5 && \
-        git format-patch -19 --stdout FETCH_HEAD |git apply
-
-cp configs/config.msi_ms7d25 .config
 
 echo "Building Dasharo compatible with MSI PRO Z690-A WiFi DDR4"
 
