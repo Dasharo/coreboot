e3db20701839995b935121d5e702b907999a3fe1
Michał Żygowski <michal.zygowski@3mdeb.com>
2022-05-27 10:32:05 +0200

    dasharo/sign.sh: udpate key to v1.x
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- dasharo/sign.sh
+++ dasharo/sign.sh
@@ -3,5 +3,5 @@
 file=$1
 
 sha256sum $file > $file.sha256
-gpg --default-key 30FF3BD32AF61642 --armor --output $file.sha256.sig --detach-sig $file.sha256
+gpg --default-key 5DC481E1F371151E --armor --output $file.sha256.sig --detach-sig $file.sha256
 gpg --verify $file.sha256.sig $file.sha256
