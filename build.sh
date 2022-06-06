596ee3861a389f65e58d1ebb52f96e4142d25a49
Michał Kopeć <michal.kopec@3mdeb.com>
2022-06-06 14:51:36 +0200

    build.sh: fix board name
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>

--- build.sh
+++ build.sh
@@ -26,7 +26,7 @@ usage() {
 # Novacustom Open Source Firmware Release 0.x Signing Key
 GPG_FINGERPRINT="FECB8B01334874A0"
 
-BOARD="novacustom_ns5x"
+BOARD="novacustom_nv4x"
 DEFCONFIG="configs/config.${BOARD}"
 FW_VERSION=$(cat ${DEFCONFIG} | grep CONFIG_LOCALVERSION | cut -d '=' -f 2 | tr -d '"')
 FW_FILE="dasharo_${BOARD}_${FW_VERSION}.rom"
