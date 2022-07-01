0dfab70af3233dbb9257ad06bd0ca59946df744c
Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
2022-07-02 00:57:00 +0300

    soc/power9/istep_8_9.c: rewrite to support 1-cpu configuration
    1 CPU configuration also needs XBus properly setup in order to know that
    it shouldn't try to talk to second CPU (e.g., for cache coherency).
    
    Change-Id: I80a4fed902ed6b76c847be3d8c4632fd3da92f03
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>

--- src/soc/ibm/power9/istep_8_9.c
+++ src/soc/ibm/power9/istep_8_9.c
@@ -3,6 +3,7 @@
 #include <cpu/power/istep_8.h>
 
 #include <console/console.h>
+#include <cpu/power/istep_18.h>
 #include <cpu/power/powerbus.h>
 #include <cpu/power/proc.h>
 #include <cpu/power/scom.h>
