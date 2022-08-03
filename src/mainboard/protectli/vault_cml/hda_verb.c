d662831d1df13e79d8875ed04968bfb7a08ebfac
Michał Kopeć <michal.kopec@3mdeb.com>
2022-08-03 16:45:03 +0200

    vp4: fix hda verbs
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>

--- src/mainboard/protectli/vault_cml/hda_verb.c
+++ src/mainboard/protectli/vault_cml/hda_verb.c
@@ -3,21 +3,24 @@
 #include <device/azalia_device.h>
 
 const u32 cim_verb_data[] = {
-	0x10ec0662,	/* Codec Vendor / Device ID: Realtek */
-	0x10ec0662,	/* Subsystem ID */
-	12,		/* Number of 4 dword sets */
+	0x10ec0897,	/* Codec Vendor / Device ID: Realtek */
+	0x10ec0000,	/* Subsystem ID */
+	15,		/* Number of 4 dword sets */
 	AZALIA_SUBVENDOR(0, 0x10ec0662),
+	AZALIA_PIN_CFG(0, 0x11, 0x40000000),
 	AZALIA_PIN_CFG(0, 0x12, 0x411111f0),
-	AZALIA_PIN_CFG(0, 0x14, 0x01014110),
-	AZALIA_PIN_CFG(0, 0x15, 0x40030000),
+	AZALIA_PIN_CFG(0, 0x14, 0x04214110),
+	AZALIA_PIN_CFG(0, 0x15, 0x411111f0),
 	AZALIA_PIN_CFG(0, 0x16, 0x411111f0),
-	AZALIA_PIN_CFG(0, 0x18, 0x411111f0),
-	AZALIA_PIN_CFG(0, 0x19, 0x02a19120),
+	AZALIA_PIN_CFG(0, 0x17, 0x411111f0),
+	AZALIA_PIN_CFG(0, 0x18, 0x04a19120),
+	AZALIA_PIN_CFG(0, 0x19, 0x411111f0),
 	AZALIA_PIN_CFG(0, 0x1a, 0x411111f0),
 	AZALIA_PIN_CFG(0, 0x1b, 0x411111f0),
 	AZALIA_PIN_CFG(0, 0x1c, 0x411111f0),
-	AZALIA_PIN_CFG(0, 0x1d, 0x40a24101),
+	AZALIA_PIN_CFG(0, 0x1d, 0x40231105),
 	AZALIA_PIN_CFG(0, 0x1e, 0x411111f0),
+	AZALIA_PIN_CFG(0, 0x1f, 0x411111f0),
 
 	0x8086280b,	/* Codec Vendor / Device ID: Intel */
 	0x80860101,	/* Subsystem ID */
