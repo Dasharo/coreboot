7893b5d61bc06a14efc6db7451973b4258f8d7a5
Michał Kopeć <michal.kopec@3mdeb.com>
2022-08-18 17:29:50 +0200

    Load UEFI Logo into CBMEM
    Change-Id: I9a8f8445877df1823b8fb0b184bdb3fd3984f2c9
    Upstream-Status: Inappropriate [Dasharo downstream]
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/commonlib/include/commonlib/coreboot_tables.h
+++ src/commonlib/include/commonlib/coreboot_tables.h
@@ -91,6 +91,7 @@ enum {
 	LB_TAG_CAPSULE			= 0x0046,
 	LB_TAG_CFR_ROOT			= 0x0047,
 	LB_TAG_ROOT_BRIDGE_INFO		= 0x0048,
+	LB_TAG_LOGO			= 0x00a0,
 	/* The following options are CMOS-related */
 	LB_TAG_CMOS_OPTION_TABLE	= 0x00c8,
 	LB_TAG_OPTION			= 0x00c9,
@@ -642,5 +643,13 @@ struct lb_boot_mode {
 
 	enum boot_mode_t boot_mode;
 };
+
+/*
+ * Bootlogo header for TianoCore boot logo
+ * * size   Contains the size of the BMP file
+ */
+struct bootlogo_header {
+	uint64_t size;
+} __packed;
 
 #endif
