7097aa7e1c586948a31198c801dede537e5b7dc8
Krystian Hebel <krystian.hebel@3mdeb.com>
2022-08-31 23:44:36 +0200

    cpu/amd/pi/skinit: load and start DRTM payload
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>

--- src/cpu/amd/pi/skinit.c
+++ src/cpu/amd/pi/skinit.c
@@ -1,8 +1,27 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <cbfs.h>
+#include <cpu/x86/lapic.h>
 #include <program_loading.h>
+#include <stdlib.h>
+#include <security/tpm/tis.h>
 
 void platform_prog_run(struct prog *prog)
 {
-	/* do nothing */
+	void *skl = NULL;
+
+	/*
+	 * APs have to be in wait-for-SIPI state for at least 1000 cycles before
+	 * SKINIT. Send INIT now and assume that loading SKL from CBFS is long
+	 * enough.
+	 */
+	lapic_send_ipi_others(LAPIC_INT_LEVELTRIG | LAPIC_INT_ASSERT | LAPIC_MT_INIT);
+
+	skl = memalign(64*KiB, 64*KiB);
+
+	cbfs_load(CONFIG_CBFS_PREFIX "/drtm_payload", skl, 64*KiB);
+
+	/* TODO: fill SKL input data */
+
+	asm volatile ("skinit" :: "a"(skl));
 }
