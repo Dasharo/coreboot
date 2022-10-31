932ef9c5190890fadf2db5ae0ef305b7af66cef3
Stojek139808 <108516646+Stojek139808@users.noreply.github.com>
2022-10-31 12:41:57 +0100

    Apply suggestions from code review
    Co-authored-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/mainboard/protectli/vault_ehl/romstage.c
+++ src/mainboard/protectli/vault_ehl/romstage.c
@@ -1,17 +1,15 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
-#include <assert.h>
 #include <console/console.h>
 #include <fsp/api.h>
 #include <soc/romstage.h>
 #include <soc/meminit.h>
-#include "gpio.h"
 
 static const struct mb_cfg ddr4_mem_config = {
-	.UserBd = BOARD_TYPE_DESKTOP, /* FIXME */
+	.UserBd = BOARD_TYPE_DESKTOP,
 	.dq_pins_interleaved = 0x1,
-	.vref_ca_config = 0x0,
-	.ect = 0x0,
+	.vref_ca_config = 0x2,
+	.ect = 0x1,
 };
 
 static const struct spd_info module_spd_info = {
@@ -24,5 +22,4 @@ void mainboard_memory_init_params(FSPM_UPD *memupd)
 	FSP_M_CONFIG *mem_cfg = &memupd->FspmConfig;
 
 	memcfg_init(mem_cfg, &ddr4_mem_config, &module_spd_info, false);
-	gpio_configure_pads(gpio_table, ARRAY_SIZE(gpio_table));
 }
