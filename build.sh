fa006eecb280ff9c00c5e1c7782b206299b97575
Michał Żygowski <michal.zygowski@3mdeb.com>
2022-11-07 11:07:00 +0100

    build.sh: remove blob extraction
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- build.sh
+++ build.sh
@@ -13,30 +13,6 @@ if [ $# -le 0 ]; then
     usage
 fi
 
-function extract_lan_rom()
-{
-	if which UEFIExtract &> /dev/null; then
-		echo "Downloading and extracting original firmware"
-		wget https://github.com/protectli-root/protectli-firmware-updater/raw/289a8c60881707db934ee707a7e3ddccfa185b94/images/vp4630_YW6L2314.bin 2> /dev/null
-		if [[ -f vp4630_YW6L2314.bin ]]; then
-			echo "Extracting LAN ROM from vendor firmware"
-			UEFIExtract vp4630_YW6L2314.bin AA5618A2-5EEB-434C-9E58-4123C039FE69 \
-				-o ./lanrom -m body > /dev/null && \
-			cp lanrom/body.bin LanRom.efi && \
-			rm -rf lanrom vp4630_YW6L2314.bin && \
-			echo "LAN ROM extracted successfully" && \
-			return 0
-		fi
-		echo "Failed to extract LAN ROM. Network boot will not work."
-		return 1
-	else
-		echo "UEFIExtract not found, but it's required to extract LAN ROM!"
-		echo "Install it from: https://github.com/LongSoft/UEFITool/releases/download/A59/UEFIExtract_NE_A59_linux_x86_64.zip"
-		echo "Failed to extract LAN ROM. Network boot will not work."
-		return 1
-	fi
-}
-
 function buildVP46xxImage {
 
 	if [ ! -d 3rdparty/blobs/mainboard ]; then
@@ -61,12 +37,7 @@ function buildVP46xxImage {
 		/bin/bash -c "make distclean"
 
 	cp configs/config.protectli_cml_$1 .config
-
-	extract_lan_rom
-
-	if [ $? -eq 0 ]; then
-		echo "CONFIG_EDK2_LAN_ROM_DRIVER=\"LanRom.efi\"" >> .config
-	fi
+	echo "CONFIG_EDK2_LAN_ROM_DRIVER=\"3rdparty/blobs/mainboard/protectli/vault_cml/E0104X7.EFI\"" >> .config
 
 	echo "Building Dasharo for Protectli $2 (version $version)"
 
