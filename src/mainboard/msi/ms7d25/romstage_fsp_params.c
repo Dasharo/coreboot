e51c547abfaa5b19c1819fd1139a7e06d5859fef
Michał Żygowski <michal.zygowski@3mdeb.com>
2023-01-11 16:20:52 +0100

    src/mainboard/msi/ms7d25/romstage_fsp_params.c: remove OC options as they cause slight performance degradation
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/mainboard/msi/ms7d25/romstage_fsp_params.c
+++ src/mainboard/msi/ms7d25/romstage_fsp_params.c
@@ -87,9 +87,6 @@ void mainboard_memory_init_params(FSPM_UPD *memupd)
 	memupd->FspmConfig.MmioSize = 0xb00; /* 2.75GB in MB */
 
 	memupd->FspmConfig.OcLock = 0;
-	memupd->FspmConfig.OcSupport = 1;
-	memupd->FspmConfig.SaOcSupport = 1;
-	memupd->FspmConfig.RingDownBin = 0;
 
 	if (CONFIG(BOARD_MSI_Z690_A_PRO_WIFI_DDR4))
 		memcfg_init(memupd, &ddr4_mem_config, &dimm_module_spd_info, false);
