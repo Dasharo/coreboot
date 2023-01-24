3c055ceec2126142059b06867b5a0e9fcf31c5f8
Michał Żygowski <michal.zygowski@3mdeb.com>
2023-01-24 16:55:00 +0100

    src/mainboard/protectli/vault_ehl: mirror vendor SIO configuration
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/mainboard/protectli/vault_ehl/bootblock.c
+++ src/mainboard/protectli/vault_ehl/bootblock.c
@@ -9,9 +9,12 @@
 #define UART_DEV PNP_DEV(0x2e, IT8613E_SP1)
 
 void bootblock_mainboard_early_init(void)
-{	
+{
 	ite_reg_write(GPIO_DEV, 0x29, 0xc1);
+	ite_reg_write(GPIO_DEV, 0x2a, 0x00); /* Disable FAN_CTL4 */
 	ite_reg_write(GPIO_DEV, 0x2c, 0x41); /* disable k8 power seq */
+	ite_reg_write(GPIO_DEV, 0x2d, 0x02); /* PCICLK=25MHz */
+	ite_kill_watchdog(GPIO_DEV);
 	ite_enable_serial(UART_DEV, CONFIG_TTYS0_BASE);
 }
 
