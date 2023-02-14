600e2f9c77f267a3a04a1ebbb3e1b694e82c9488
Michał Kopeć <michal.kopec@3mdeb.com>
2023-02-14 15:36:39 +0100

    soc/intel/tigerlake/lockdown.c: implement native PMC lockdown
    This patch allows skipping FSP notify after PCI enumeration. Based on patches
    done for ADL in https://review.coreboot.org/q/topic:DROP_FSP_NOTIFY1_API
    
    Change-Id: I6001f486b6fc88bbcab8f90592951ffeae454b64
    Upstream-Status: Pending
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/soc/intel/tigerlake/lockdown.c
+++ src/soc/intel/tigerlake/lockdown.c
@@ -8,9 +8,16 @@
 
 #include <device/mmio.h>
 #include <intelblocks/cfg.h>
+#include <intelblocks/pcr.h>
+#include <intelblocks/pmclib.h>
 #include <intelpch/lockdown.h>
+#include <soc/pcr_ids.h>
 #include <soc/pm.h>
 #include <stdint.h>
+
+/* PCR PSTH Control Register */
+#define PCR_PSTH_CTRLREG		0x1d00
+#define   PSTH_CTRLREG_IOSFPTCGE	(1 << 2)
 
 static void pmc_lock_pmsync(void)
 {
@@ -50,6 +57,8 @@ static void pmc_lock_smi(void)
 
 static void pmc_lockdown_cfg(int chipset_lockdown)
 {
+	uint8_t *pmcbase = pmc_mmio_regs();
+
 	/* PMSYNC */
 	pmc_lock_pmsync();
 	/* Lock down ABASE and sleep stretching policy */
@@ -57,10 +66,31 @@ static void pmc_lockdown_cfg(int chipset_lockdown)
 
 	if (chipset_lockdown == CHIPSET_LOCKDOWN_COREBOOT)
 		pmc_lock_smi();
+
+	if (!CONFIG(USE_FSP_NOTIFY_PHASE_POST_PCI_ENUM)) {
+		setbits32(pmcbase + ST_PG_FDIS1, ST_FDIS_LOCK);
+		setbits32(pmcbase + SSML, SSML_SSL_EN);
+		setbits32(pmcbase + PM_CFG, PM_CFG_DBG_MODE_LOCK |
+					 PM_CFG_XRAM_READ_DISABLE);
+	}
+
+	/* Send PMC IPC to inform about PCI enumeration done */
+	pmc_send_bios_reset_pci_enum_done();
+}
+
+static void pch_lockdown_cfg(void)
+{
+	if (CONFIG(USE_FSP_NOTIFY_PHASE_POST_PCI_ENUM))
+		return;
+
+	/* Enable IOSF Primary Trunk Clock Gating */
+	pcr_rmw32(PID_PSTH, PCR_PSTH_CTRLREG, ~0, PSTH_CTRLREG_IOSFPTCGE);
 }
 
 void soc_lockdown_config(int chipset_lockdown)
 {
 	/* PMC lock down configuration */
 	pmc_lockdown_cfg(chipset_lockdown);
+	/* PCH lock down configuration */
+	pch_lockdown_cfg();
 }
