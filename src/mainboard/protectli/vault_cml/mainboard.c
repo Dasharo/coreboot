af6bbae791fb7d1a01b2651db9760c070339ac43
Michał Żygowski <michal.zygowski@3mdeb.com>
2023-02-22 13:37:12 +0100

    src/mainboard/protectli/vault_cml/mainboard.c: Write DBG2 and DBGP ACPI tables
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>
    Change-Id: Ia9368d21255ece5641e90f35c40b1815ca4257e0

--- src/mainboard/protectli/vault_cml/mainboard.c
+++ src/mainboard/protectli/vault_cml/mainboard.c
@@ -1,6 +1,7 @@
 
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
+#include <acpi/acpi.h>
 #include <arch/cpu.h>
 #include <device/device.h>
 #include <pc80/i8254.h>
@@ -50,6 +51,57 @@ static void mainboard_final(void *unused)
 		beep(1500, 100);
 }
 
+static unsigned long mainboard_write_acpi_tables(const struct device *device,
+					  unsigned long start,
+					  acpi_rsdp_t *rsdp)
+{
+	unsigned long current;
+	acpi_dbg2_header_t *dbg2;
+	acpi_dbgp_t *dbgp;
+	acpi_addr_t address;
+
+	printk(BIOS_DEBUG, "ACPI:     * DBG2\n");
+	current = acpi_align_current(start);
+	dbg2 = (acpi_dbg2_header_t *)current;
+
+	memset(&address, 0, sizeof(address));
+	address.space_id = ACPI_ADDRESS_SPACE_IO;
+	address.addrl = (uint32_t)0x3f8;
+
+	acpi_create_dbg2(dbg2,
+			 ACPI_DBG2_PORT_SERIAL,
+			 ACPI_DBG2_PORT_SERIAL_16550_DBGP,
+			 &address, 8, NULL);
+
+	if (dbg2->header.length) {
+		current += dbg2->header.length;
+		current = acpi_align_current(current);
+		acpi_add_table(rsdp, dbg2);
+	}
+
+	printk(BIOS_DEBUG, "ACPI:     * DBGP\n");
+	current = acpi_align_current(current);
+	dbgp = (acpi_dbgp_t *)current;
+
+	acpi_create_dbgp(dbgp,
+			 ACPI_DBGP_PORT_SERIAL_16550_DBGP,
+			 &address);
+
+	if (dbgp->header.length) {
+		current += dbgp->header.length;
+		current = acpi_align_current(current);
+		acpi_add_table(rsdp, dbgp);
+	}
+
+	return current;
+}
+
+static void mainboard_enable(struct device *dev)
+{
+	dev->ops->write_acpi_tables = mainboard_write_acpi_tables;
+}
+
 struct chip_operations mainboard_ops = {
+	.enable_dev = mainboard_enable,
 	.final = mainboard_final,
 };
