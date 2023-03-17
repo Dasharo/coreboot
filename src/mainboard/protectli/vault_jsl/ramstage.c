536968377d67041c067a4aa0d33415f338cbed78
Kacper Stojek <kacper.stojek@3mdeb.com>
2023-03-17 13:55:27 +0100

    src/mainboard/protectli/vault_jsl: Add basic file structure
    Signed-off-by: Kacper Stojek <kacper.stojek@3mdeb.com>

---
+++ src/mainboard/protectli/vault_jsl/ramstage.c
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include <soc/gpio.h>
+#include <soc/ramstage.h>
+
+void mainboard_silicon_init_params(FSP_SIL_UPD *params)
+{
+}
---
+++ src/mainboard/protectli/vault_jsl/romstage.c
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include <fsp/api.h>
+#include <soc/romstage.h>
+
+void mainboard_memory_init_params(FSPM_UPD *mupd)
+{
+}
