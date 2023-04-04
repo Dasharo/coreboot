edb3fbea570af289f2370a6bee4e4170a0d3226c
Michał Żygowski <michal.zygowski@3mdeb.com>
2023-04-04 13:45:30 +0200

    src/soc/intel/cannonlake/acpi.c: use Intel recommended C-states
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/soc/intel/cannonlake/acpi.c
+++ src/soc/intel/cannonlake/acpi.c
@@ -100,7 +100,7 @@ static const acpi_cstate_t cstate_map[NUM_C_STATES] = {
 
 static int cstate_set_non_s0ix[] = {
 	C_STATE_C1E,
-	C_STATE_C6_LONG_LAT,
+//	C_STATE_C6_LONG_LAT,
 	C_STATE_C7S_LONG_LAT
 };
 
