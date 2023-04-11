70feab7b7d64d7996945bb29e5821d6b2e0acc54
Michał Żygowski <michal.zygowski@3mdeb.com>
2023-04-11 16:26:50 +0200

    mb/protectli/vault_ehl/mainboard.c: clear recovery request
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>
    Change-Id: I9fbb4dd3a8fe0c65ae66fc39a89657ead6bd16d1

--- src/mainboard/protectli/vault_ehl/mainboard.c
+++ src/mainboard/protectli/vault_ehl/mainboard.c
@@ -3,13 +3,16 @@
 #include <device/device.h>
 #include <fsp/api.h>
 #include <pc80/i8254.h>
+#include <security/vboot/vboot_common.h>
 #include <soc/ramstage.h>
 
 static void mainboard_final(void *chip_info)
 {
+	if (CONFIG(VBOOT))
+		vboot_clear_recovery_request();
+
 	beep(1500, 200);
 }
-
 
 void mainboard_silicon_init_params(FSP_S_CONFIG *silconfig)
 {
