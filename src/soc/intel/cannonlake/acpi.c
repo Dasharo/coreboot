ec7d3b9b43dab8b9a2e7e1fcf9db81968a83f922
Michał Żygowski <michal.zygowski@3mdeb.com>
2023-04-13 14:01:40 +0200

    src/soc/intel/cannonlake/acpi.c: disable non_s0ix C-states on VP4670
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>
    Change-Id: I2f3e4a13228c247ca1894b5d8fd083d061f0bfc3

--- src/soc/intel/cannonlake/acpi.c
+++ src/soc/intel/cannonlake/acpi.c
@@ -100,8 +100,10 @@ static const acpi_cstate_t cstate_map[NUM_C_STATES] = {
 
 static int cstate_set_non_s0ix[] = {
 	C_STATE_C1E,
-//	C_STATE_C6_LONG_LAT,
+#if !CONFIG(BOARD_PROTECTLI_VP4670)
+	C_STATE_C6_LONG_LAT,
 	C_STATE_C7S_LONG_LAT
+#endif
 };
 
 static int cstate_set_s0ix[] = {
