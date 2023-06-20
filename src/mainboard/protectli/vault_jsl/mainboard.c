2afd37bd76edcc94dd9c1233ef02d5ba1a931259
Michał Żygowski <michal.zygowski@3mdeb.com>
2023-06-20 17:51:40 +0200

    src/mainboard/protectli/vault_jsl: update USB configuration
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/mainboard/protectli/vault_jsl/mainboard.c
+++ src/mainboard/protectli/vault_jsl/mainboard.c
@@ -3,11 +3,17 @@
 #include <arch/mmio.h>
 #include <device/device.h>
 #include <soc/iomap.h>
+#include <soc/ramstage.h>
+
+void mainboard_silicon_init_params(FSP_S_CONFIG *params)
+{
+	params->UsbPdoProgramming = 0;
+}
 
 static void mainboard_enable(struct device *dev)
 {
 }
 
 struct chip_operations mainboard_ops = {
-    .enable_dev = mainboard_enable,
+	.enable_dev = mainboard_enable,
 };
--- src/mainboard/protectli/vault_jsl/ramstage.c
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-
-#include <soc/gpio.h>
-#include <soc/ramstage.h>
-
-void mainboard_silicon_init_params(FSP_SIL_UPD *params)
-{
-}
