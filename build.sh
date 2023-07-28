f36c0b3b509cf809c98056c09e2e962a30d020fe
Michał Żygowski <michal.zygowski@3mdeb.com>
2023-07-28 13:50:48 +0200

    build.sh: add support for MSI Z790-P builds
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- build.sh
+++ build.sh
@@ -4,8 +4,10 @@ function usage {
     echo -e "Usage:"
     echo -e "$0 CMD"
     echo -e "Available CMDs:"
-    echo -e "\tddr4  - build Dasharo image compatible with MSI PRO Z690-A (WIFI) DDR4"
-    echo -e "\tddr5 - build Dasharo image compatible with MSI PRO Z690-A (WIFI)"
+    echo -e "\tz690a_ddr4  - build Dasharo image compatible with MSI PRO Z690-A (WIFI) DDR4"
+    echo -e "\tz690a_ddr5 - build Dasharo image compatible with MSI PRO Z690-A (WIFI)"
+    echo -e "\tz790p_ddr4  - build Dasharo image compatible with MSI PRO Z790-P (WIFI) DDR4"
+    echo -e "\tz790p_ddr5 - build Dasharo image compatible with MSI PRO Z790-P (WIFI)"
     exit 1
 }
 
@@ -38,9 +40,9 @@ function extract_lan_rom()
 	fi
 }
 
-function build_image {
+function build_image_z690 {
 
-	version=$(git describe --abbrev=1 --tags --always --dirty)
+	version=$(git describe --abbrev=1 --tags --always --dirty --match "msi_ms7d25*")
 	version=${version//msi_ms7d25_/}
 
 	docker run --rm -t -u $UID -v $PWD:/home/coreboot/coreboot \
@@ -50,7 +52,7 @@ function build_image {
 
 	cp configs/config.msi_ms7d25_$1 .config
 
-	extract_lan_rom
+	#extract_lan_rom
 
 	if [ $? -eq 0 ]; then
 		echo "CONFIG_EDK2_LAN_ROM_DRIVER=\"LanRom.efi\"" >> .config
@@ -75,14 +77,57 @@ function build_image {
 	fi
 }
 
+function build_image_z790 {
+
+	version=$(git describe --abbrev=1 --tags --always --dirty --match "msi_ms7e06*")
+	version=${version//msi_ms7e06_/}
+
+	docker run --rm -t -u $UID -v $PWD:/home/coreboot/coreboot \
+		-v $HOME/.ssh:/home/coreboot/.ssh \
+		-w /home/coreboot/coreboot coreboot/coreboot-sdk:2021-09-23_b0d87f753c \
+		/bin/bash -c "make distclean"
+
+	cp configs/config.msi_ms7e06_$1 .config
+
+	#extract_lan_rom
+
+	if [ $? -eq 0 ]; then
+		echo "CONFIG_EDK2_LAN_ROM_DRIVER=\"LanRom.efi\"" >> .config
+	fi
+
+	git submodule update --init --checkout
+
+	echo "Building Dasharo compatible with MSI PRO Z790-P (WIFI) $2(version $version)"
+
+	docker run --rm -t -u $UID -v $PWD:/home/coreboot/coreboot \
+		-v $HOME/.ssh:/home/coreboot/.ssh \
+		-w /home/coreboot/coreboot coreboot/coreboot-sdk:2021-09-23_b0d87f753c \
+		/bin/bash -c "make olddefconfig && make -j$(nproc)"
+
+	cp build/coreboot.rom msi_ms7e06_${version}_$1.rom
+	if [ $? -eq 0 ]; then
+		echo "Result binary placed in $PWD/msi_ms7e06_${version}_$1.rom" 
+		sha256sum msi_ms7e06_${version}_$1.rom > msi_ms7e06_${version}_$1.rom.sha256
+	else
+		echo "Build failed!"
+		exit 1
+	fi
+}
+
 CMD="$1"
 
 case "$CMD" in
-    "ddr4")
-        build_image $CMD "DDR4 "
+    "ddr4" | "z690a_ddr4")
+        build_image_z690 ddr4 "DDR4 "
         ;;
-    "ddr5")
-        build_image $CMD "DDR5 "
+    "ddr5" | "z690a_ddr5")
+        build_image_z690 ddr5 "DDR5 "
+        ;;
+    "z790p_ddr4")
+        build_image_z790 ddr4 "DDR4 "
+        ;;
+    "z790p_ddr5")
+        build_image_z790 ddr5 "DDR5 "
         ;;
     *)
         echo "Invalid command: \"$CMD\""
