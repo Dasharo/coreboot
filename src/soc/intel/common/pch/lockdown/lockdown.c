069757298da630d2944c22f0fbc5c24c60ed362b
Krystian Hebel <krystian.hebel@3mdeb.com>
2023-08-14 14:21:35 +0200

    soc/intel/common/pch/lockdown: disable locking if in Firmware Update Mode
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>

--- src/soc/intel/common/pch/lockdown/lockdown.c
+++ src/soc/intel/common/pch/lockdown/lockdown.c
@@ -50,6 +50,12 @@ bool is_smm_bwp_permitted(void)
 	if (smmstore_lookup_region(&rdev))
 		return false;
 
+    /* Disable locking if in Firmware Update Mode */
+	size = sizeof(var);
+	ret = efi_fv_get_option(&rdev, &dasharo_system_features_guid, "FirmwareUpdateMode", &var, &size);
+	if (ret == CB_SUCCESS && var == TRUE)
+		return false;
+
 	size = sizeof(var);
 	ret = efi_fv_get_option(&rdev, &dasharo_system_features_guid, "SmmBwp", &var, &size);
 	if (ret != CB_SUCCESS)
