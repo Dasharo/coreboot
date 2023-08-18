1674cb61d077939ca7976efd2e1268c59fe87613
Michał Żygowski <michal.zygowski@3mdeb.com>
2023-08-18 11:51:24 +0200

    src/mainboard/protectli/vault_jsl/mainboard.c: enable additional PCIe RP capabilities
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/mainboard/protectli/vault_jsl/mainboard.c
+++ src/mainboard/protectli/vault_jsl/mainboard.c
@@ -8,6 +8,40 @@
 void mainboard_silicon_init_params(FSP_S_CONFIG *params)
 {
 	params->UsbPdoProgramming = 0;
+
+	params->PcieRpAcsEnabled[0] = 1;
+	params->PcieRpAcsEnabled[1] = 1;
+	params->PcieRpAcsEnabled[3] = 1;
+	params->PcieRpAcsEnabled[4] = 1;
+	params->PcieRpAcsEnabled[5] = 1;
+	params->PcieRpAcsEnabled[6] = 1;
+
+	params->PcieRpAdvancedErrorReporting[0] = 1;
+	params->PcieRpAdvancedErrorReporting[1] = 1;
+	params->PcieRpAdvancedErrorReporting[3] = 1;
+	params->PcieRpAdvancedErrorReporting[4] = 1;
+	params->PcieRpAdvancedErrorReporting[5] = 1;
+	params->PcieRpAdvancedErrorReporting[6] = 1;
+
+	params->PcieRpLtrEnable[0] = 1;
+	params->PcieRpLtrEnable[1] = 1;
+	params->PcieRpLtrEnable[3] = 1;
+	params->PcieRpLtrEnable[4] = 1;
+	params->PcieRpLtrEnable[5] = 1;
+	params->PcieRpLtrEnable[6] = 1;
+
+	/* Set Max Paylaod to 256B */
+	params->PcieRpMaxPayload[0] = 1;
+	params->PcieRpMaxPayload[1] = 1;
+	params->PcieRpMaxPayload[3] = 1;
+	params->PcieRpMaxPayload[4] = 1;
+	params->PcieRpMaxPayload[5] = 1;
+	params->PcieRpMaxPayload[6] = 1;
+
+	params->PcieRpSlotImplemented[3] = 1;
+	params->PcieRpSlotImplemented[4] = 1;
+
+	params->PcieRpDetectTimeoutMs[1] = 200;
 }
 
 static void mainboard_enable(struct device *dev)
