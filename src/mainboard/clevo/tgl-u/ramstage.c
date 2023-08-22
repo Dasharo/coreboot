098fc30e13ff56b172e0706f94ebd78e87997cb8
Mixss <michal.ziemiec@3mdeb.com>
2023-08-22 15:30:16 +0200

    Send command to EC after read Camera enablement efi var

--- src/mainboard/clevo/tgl-u/ramstage.c
+++ src/mainboard/clevo/tgl-u/ramstage.c
@@ -32,6 +32,10 @@ struct smfi_cmd_set_fan_curve {
 	struct fan_curve curve;
 } __packed;
 
+struct smfi_cmd_camera_enablement_set {
+    bool enable;
+} __packed;
+
 struct fan_curve fan_curve_silent = {
 	{ .temp = 0,   .duty = 20  },
 	{ .temp = 65,  .duty = 25  },
@@ -50,6 +54,11 @@ struct fan_curve fan_curve_performance = {
 #define FAN_CURVE_OPTION_PERFORMANCE 1
 
 #define FAN_CURVE_OPTION_DEFAULT FAN_CURVE_OPTION_SILENT
+
+#define CAMERA_ENABLED_OPTION 1
+#define CAMERA_DISABLED_OPTION 0
+
+#define CAMERA_ENABLEMENT_DEFAULT CAMERA_ENABLED_OPTION
 
 const char *smbios_system_sku(void)
 {
@@ -112,11 +121,46 @@ efi_err:
 	}
 }
 
+static void set_camera_enablement(void)
+{
+    struct smfi_cmd_camera_enablement_set cmd;
+    bool enabled = CAMERA_ENABLEMENT_DEFAULT;
+
+#if CONFIG(DRIVERS_EFI_VARIABLE_STORE)
+    struct region_device rdev;
+    enum cb_err ret;
+    uint32_t size;
+
+    const EFI_GUID dasharo_system_features_guid = {
+		0xd15b327e, 0xff2d, 0x4fc1, { 0xab, 0xf6, 0xc1, 0x2b, 0xd0, 0x8c, 0x13, 0x59 }
+	};
+
+    if (smmstore_lookup_region(&rdev))
+		goto efi_err;
+
+    size = sizeof(enabled);
+    ret = efi_fv_get_option(&rdev, &dasharo_system_features_guid, "EnableCamera", &enabled, &size);
+
+    if (ret != CB_SUCCESS) {
+		printk(BIOS_DEBUG, "camera: failed to read camera enablement status from EFI vars, using board default\n");
+		goto efi_err;
+    }
+
+efi_err:
+#endif
+
+    cmd.enable = enabled;
+    system76_ec_smfi_cmd(CMD_CAMERA_ENABLEMENT_SET, sizeof(cmd) / sizeof(uint8_t), (uint8_t *)&cmd);
+
+}
+
 static void init_mainboard(void *chip_info)
 {
 	variant_configure_gpios();
 
 	set_fan_curve();
+
+    set_camera_enablement();
 }
 
 #if CONFIG(GENERATE_SMBIOS_TABLES)
