f442e2129514fd6d0674e959d877af631c2fd0f6
Michał Żygowski <michal.zygowski@3mdeb.com>
2023-09-13 16:13:48 +0200

    src/vendorcode/dasharo: add Dasharo features
    Change-Id: Ibb370725a11c49f15612c466e80e7f7923078a08
    Upstream-Status: Partially Submitted [chain starting with CB:78809],
                     Inappropriate [the rest is Dasharo downstream]
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>
    Signed-off-by: Filip Gołaś <filip.golas@3mdeb.com>

--- src/vendorcode/dasharo/include/dasharo/options.h
+++ src/vendorcode/dasharo/include/dasharo/options.h
@@ -5,6 +5,17 @@
 
 #include <types.h>
 
+enum cse_disable_mode {
+	ME_MODE_ENABLE = 0,
+	ME_MODE_DISABLE_HECI = 1,
+	ME_MODE_DISABLE_HAP = 2,
+};
+
+struct watchdog_config {
+	bool wdt_enable;
+	uint16_t wdt_timeout;
+} __packed;
+
 /* Looks up "power_on_after_fail" option and Dasharo/"PowerFailureState"
  * variable.
  *
@@ -32,5 +43,51 @@ bool dasharo_resizeable_bars_enabled(void);
  *  - false - bootmedia lock is disabled or not supported
  */
 bool is_vboot_locking_permitted(void);
+
+/* Looks Dasharo/"IommuConfig" variable if early DMA protection lock was
+ * enabled at compile time.
+ *
+ * Result:
+ *  - true  - DMA protection is supported and enabled
+ *  - false - DMA protection is disabled or not supported
+ */
+bool dma_protection_enabled(void);
+
+/* Looks Dasharo/"MeMode" variable if ME should be enabeld or disabled.
+ *
+ * Result:
+ *  - 0 - ME enabled
+ *  - 1 - ME soft disabled
+ *  - 2 - ME HAP disabled
+ */
+uint8_t cse_get_me_disable_mode(void);
+
+/* Looks Dasharo/"SmmBwp" variable if SMM BIOS Write Protection was
+ * enabled at compile time.
+ *
+ * Result:
+ *  - true  - DMA protection is supported and enabled
+ *  - false - DMA protection is disabled or not supported
+ */
+bool is_smm_bwp_permitted(void);
+
+/* Looks Dasharo/"WatchdogCOnfig" variable if OC Watchdog was
+ * enabled at compile time.
+ *
+ * Result:
+ *  - true  - DMA protection is supported and enabled
+ *  - false - DMA protection is disabled or not supported
+ */
+void get_watchdog_config(struct watchdog_config *wdt_cfg);
+
+/* Looks Dasharo/"WatchdogCOnfig" variable if PS/2 controller should be
+ * enabled. The function should be called from mainboard code to disable
+ * appropriate PNP device in devicetree.
+ *
+ * Result:
+ *  - true  - PS/2 controller enabled
+ *  - false - PS/2 controller disabled
+ */
+bool get_ps2_option(void);
 
 #endif /* DASHARO_OPTIONS_H */
