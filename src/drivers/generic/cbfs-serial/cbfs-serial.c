9e5b5b1af7b47a5df08c39363f8268c24d2978d6
Michał Żygowski <michal.zygowski@3mdeb.com>
2023-09-28 16:12:25 +0200

    drivers/generic/cbfs-serial,cbfs-uuid: fallback to EFI variable if CBFS file not found
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/drivers/generic/cbfs-serial/cbfs-serial.c
+++ src/drivers/generic/cbfs-serial/cbfs-serial.c
@@ -2,10 +2,40 @@
 
 #include <cbfs.h>
 #include <device/device.h>
+#include <drivers/efi/efivars.h>
+#include <smmstore.h>
 #include <smbios.h>
 #include <string.h>
 
+#include <Uefi/UefiBaseType.h>
+
 #define MAX_SERIAL_LENGTH 0x100
+
+static bool get_serial_number_from_efivar(char *serial_number)
+{
+#if CONFIG(DRIVERS_EFI_VARIABLE_STORE)
+	struct region_device rdev;
+	enum cb_err ret;
+	uint32_t size;
+	const EFI_GUID dasharo_system_features_guid = {
+		0xd15b327e, 0xff2d, 0x4fc1, { 0xab, 0xf6, 0xc1, 0x2b, 0xd0, 0x8c, 0x13, 0x59 }
+	};
+	if (smmstore_lookup_region(&rdev))
+		return false;
+
+	size = MAX_SERIAL_LENGTH;
+	ret = efi_fv_get_option(&rdev, &dasharo_system_features_guid, "Type2SN", serial_number, &size);
+	if (ret != CB_SUCCESS || size > MAX_SERIAL_LENGTH)
+		return false;
+
+	serial_number[size] = '\0';
+
+	return true;
+#else
+	/* In absence of EFI variables, follow compilation options */
+	return false;
+#endif
+}
 
 const char *smbios_mainboard_serial_number(void)
 {
@@ -19,6 +49,9 @@ const char *smbios_mainboard_serial_number(void)
 		serial_number[serial_len] = '\0';
 		return serial_number;
 	}
+
+	if (get_serial_number_from_efivar(serial_number))
+		return serial_number;
 
 	strncpy(serial_number, CONFIG_MAINBOARD_SERIAL_NUMBER,
 			MAX_SERIAL_LENGTH);
--- src/drivers/generic/cbfs-uuid/cbfs-uuid.c
+++ src/drivers/generic/cbfs-uuid/cbfs-uuid.c
@@ -1,10 +1,41 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <lib.h>
 #include <cbfs.h>
+#include <console/console.h>
 #include <device/device.h>
+#include <drivers/efi/efivars.h>
+#include <smmstore.h>
 #include <smbios.h>
 #include <string.h>
 #include <uuid.h>
+
+#include <Uefi/UefiBaseType.h>
+
+
+static bool get_uuid_from_efivar(uint8_t *uuid)
+{
+#if CONFIG(DRIVERS_EFI_VARIABLE_STORE)
+	struct region_device rdev;
+	enum cb_err ret;
+	uint32_t size;
+	const EFI_GUID dasharo_system_features_guid = {
+		0xd15b327e, 0xff2d, 0x4fc1, { 0xab, 0xf6, 0xc1, 0x2b, 0xd0, 0x8c, 0x13, 0x59 }
+	};
+	if (smmstore_lookup_region(&rdev))
+		return false;
+
+	size = UUID_LEN;
+	ret = efi_fv_get_option(&rdev, &dasharo_system_features_guid, "Type1UUID", uuid, &size);
+	if (ret != CB_SUCCESS || size != UUID_LEN)
+		return false;
+
+	return true;
+#else
+	/* In absence of EFI variables, follow compilation options */
+	return false;
+#endif
+}
 
 void smbios_system_set_uuid(u8 *uuid)
 {
@@ -17,7 +48,12 @@ void smbios_system_set_uuid(u8 *uuid)
 	if (uuid_len >= UUID_STRLEN && uuid_len <= UUID_STRLEN + 3) {
 		/* Cut off any trailing whitespace like CR or LF */
 		uuid_str[UUID_STRLEN] = '\0';
-		if (!parse_uuid(system_uuid, uuid_str))
+		if (!parse_uuid(system_uuid, uuid_str)) {
 			memcpy(uuid, system_uuid, UUID_LEN);
+			return;
+		}
 	}
+
+	if (get_uuid_from_efivar(system_uuid))
+		memcpy(uuid, system_uuid, UUID_LEN);
 }
