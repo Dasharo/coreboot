6ebd72f790987eb044ed58107d59d00fcc243dd6
Michał Żygowski <michal.zygowski@3mdeb.com>
2023-11-20 12:50:09 +0100

    mb/protectli/vault_jsl: Add board support
    Using new SPD for V1210 which fixes MemoryInit on the newest hardware
    revision.
    
    Change-Id: I652fdc6c2e4258f7c4660606001fa2e3a8f5b203
    Upstream-Status: Pending
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>

---
+++ src/mainboard/protectli/vault_jsl/Makefile.mk
@@ -0,0 +1,12 @@
+
+# SPDX-License-Identifier: GPL-2.0-only
+
+bootblock-y += bootblock.c
+
+romstage-y += romstage.c
+
+ramstage-y += mainboard.c
+
+#SPD_SOURCES = MT53E512M32D4NQ-053 # 2GBx4 (pre-production V1210)
+SPD_SOURCES = MT53D512M64D4RQ-046 # 2GBx4 (production V1210)
+SPD_SOURCES += MT53E1G32D2NP-046 # 4GBx4
