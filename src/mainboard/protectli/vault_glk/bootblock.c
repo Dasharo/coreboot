3c1fc24369886c15f9da2cac094736cdb920b325
Michał Żygowski <michal.zygowski@3mdeb.com>
2023-12-21 11:06:05 +0100

    src/mainboard/protectli/vault_glk/bootblock.c: Cover SIO differences
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/mainboard/protectli/vault_glk/bootblock.c
+++ src/mainboard/protectli/vault_glk/bootblock.c
@@ -10,7 +10,13 @@
 
 void bootblock_mainboard_early_init(void)
 {
+	ite_reg_write(GPIO_DEV, 0x25, 0x05); /* PCIRST1# and PCIRST3# as GPIO */
 	ite_reg_write(GPIO_DEV, 0x2c, 0x41); /* disable k8 power seq */
 	ite_reg_write(GPIO_DEV, 0x2d, 0x02); /* PCICLK 25MHz */
+
+	ite_reg_write(GPIO_DEV, 0xc0, 0x05); /* PCIRST3# and 5VSB_CTRL GPIO Simple I/O */
+	ite_reg_write(GPIO_DEV, 0xc1, 0x02); /* GP22 GPIO Simple I/O */
+	ite_reg_write(GPIO_DEV, 0xc9, 0x02); /* GP22 GPIO output */
+
 	ite_enable_serial(UART_DEV, CONFIG_TTYS0_BASE);
 }
