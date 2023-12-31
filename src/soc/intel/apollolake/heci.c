b82670275b4c4ebd4583e7c8ce89381b1f734977
Michał Żygowski <michal.zygowski@3mdeb.com>
2023-12-31 16:13:54 +0100

    soc/intel: Add function to report ME FWSTS in SMBIOS
    Upstream-Status: Pending
    Change-Id: I2218725ecf7a8787a70816660de237bed77b2b8f
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/soc/intel/apollolake/heci.c
+++ src/soc/intel/apollolake/heci.c
@@ -5,6 +5,7 @@
 #include <device/pci_ops.h>
 #include <soc/heci.h>
 #include <soc/pci_devs.h>
+#include <intelblocks/cse.h>
 
 uint32_t heci_fw_sts(void)
 {
@@ -19,4 +20,18 @@ bool heci_cse_normal(void)
 bool heci_cse_done(void)
 {
 	return (!!(heci_fw_sts() & MASK_SEC_FIRMWARE_COMPLETE));
+}
+
+unsigned int soc_get_heci_dev(unsigned int heci_idx)
+{
+	if (heci_idx > 2)
+		return 0;
+
+	static const unsigned int heci_devs[] = {
+		PCH_DEVFN_CSE,
+		PCH_DEVFN_CSE_2,
+		PCH_DEVFN_CSE_3,
+	};
+
+	return heci_devs[heci_idx];
 }
