aeba5d16a4395fdd237b2ef26956223487d39867
Michał Żygowski <michal.zygowski@3mdeb.com>
2024-01-23 10:57:16 +0100

    src/mainboard/protectli/vault_glk: Drop custom beep implementation
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/mainboard/protectli/vault_glk/mainboard.c
+++ src/mainboard/protectli/vault_glk/mainboard.c
@@ -3,36 +3,14 @@
 #include <arch/io.h>
 #include <device/device.h>
 #include <fsp/api.h>
+#include <pc80/i8254.h>
 #include <soc/ramstage.h>
-
-static void beep(unsigned int frequency)
-{
-
-	unsigned int count = 1193180 / frequency;
-
-	// Switch on the speaker
-	outb(inb(0x61)|3, 0x61);
-
-	// Set command for counter 2, 2 byte write
-	outb(0xb6, 0x43);
-
-	// Select desired Hz
-	outb(count & 0xff, 0x42);
-	outb((count >> 8) & 0xff, 0x42);
-
-	// Block for 100 milioseconds
-	int i;
-	for (i = 0; i < 10000; i++) inb(0x61);
-
-	// Switch off the speaker
-	outb(inb(0x61) & 0xfc, 0x61);
-}
 
 static void mainboard_final(void *chip_info)
 {
-	beep(1500);
+	if (CONFIG(BEEP_ON_BOOT))
+		beep(1500, 100);
 }
-
 
 void mainboard_silicon_init_params(FSP_S_CONFIG *silconfig)
 {
