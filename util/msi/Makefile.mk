c6dc33a188a9356515091bcfe8cd6a63db204701
Michał Żygowski <michal.zygowski@3mdeb.com>
2024-02-12 11:43:50 +0100

    util/msi: Allow to include ROMHOLE as CBFS file
    Change-Id: I3871236dccf128fb9038e98977629613d28b7121
    Upstream-Status: Pending
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- util/msi/Makefile.mk
+++ util/msi/Makefile.mk
@@ -9,6 +9,23 @@ $(MSIROMHOLETOOL): $(dir)/romholetool/romholetool.c
 
 ifeq ($(CONFIG_VENDOR_MSI)$(CONFIG_BOARD_HAS_MSI_ROMHOLE),yy)
 
+ifeq ($(CONFIG_MSI_ROMHOLE_IN_CBFS),y)
+
+$(obj)/mainboard/$(MAINBOARDDIR)/romhole.bin: $(MSIROMHOLETOOL)
+	printf "    TOOL       Creating MSI ROMHOLE blob\n"
+	$(MSIROMHOLETOOL) -a $(CONFIG_MSI_ROMHOLE_ADDRESS_IN_CBFS) \
+			  -s $(CONFIG_MSI_ROMHOLE_SIZE_IN_CBFS) -o $@
+
+
+cbfs-files-y += msi_romhole.bin
+
+msi_romhole.bin-file := $(obj)/mainboard/$(MAINBOARDDIR)/romhole.bin
+msi_romhole.bin-type := raw
+msi_romhole.bin-compression := none
+msi_romhole.bin-position := $(CONFIG_MSI_ROMHOLE_ADDRESS_IN_CBFS)
+
+else
+
 $(obj)/mainboard/$(MAINBOARDDIR)/romhole.bin: $(MSIROMHOLETOOL) $(obj)/fmap_config.h
 	printf "    TOOL       Creating MSI ROMHOLE blob\n"
 	$(MSIROMHOLETOOL) -l $(obj)/fmap_config.h -o $@
@@ -16,5 +33,7 @@ $(obj)/mainboard/$(MAINBOARDDIR)/romhole.bin: $(MSIROMHOLETOOL) $(obj)/fmap_conf
 build_complete:: $(obj)/mainboard/$(MAINBOARDDIR)/romhole.bin
 	@printf "    WRITE      MSI ROMHOLE\n"
 	$(CBFSTOOL) $(obj)/coreboot.rom write -u -i 255 -r ROMHOLE -f $<
+
+endif
 
 endif
