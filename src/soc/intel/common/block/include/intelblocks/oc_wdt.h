70a4f62f560b96275400635aeea04842073a3b20
Michał Kopeć <michal.kopec@3mdeb.com>
2024-03-14 17:09:35 +0100

    soc/intel/cmn/blk/oc_wdt: implement ACPI WDAT
    Bootstatus is stored in scratch register bits.
    
    32-bit accesses in oc_wdt/acpi.c: are needed at least on alderlake for
    correct operation.
    
    Upstream-Status: Pending
    Change-Id: I5b936f26db575abf38170bd4b5ce6e126d85dfd3
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/soc/intel/common/block/include/intelblocks/oc_wdt.h
+++ src/soc/intel/common/block/include/intelblocks/oc_wdt.h
@@ -5,6 +5,19 @@
 
 #include <stdbool.h>
 
+/* OC WDT configuration */
+#define PCH_OC_WDT_CTL				(ACPI_BASE_ADDRESS + 0x54)
+#define   PCH_OC_WDT_CTL_RLD			BIT(31)
+#define   PCH_OC_WDT_CTL_ICCSURV_STS		BIT(25)
+#define   PCH_OC_WDT_CTL_NO_ICCSURV_STS		BIT(24)
+#define   PCH_OC_WDT_CTL_SCRATCH_OFFSET		0x10
+#define   PCH_OC_WDT_CTL_SCRATCH_MASK		(0xFF << PCH_OC_WDT_CTL_SCRATCH_OFFSET)
+#define   PCH_OC_WDT_CTL_FORCE_ALL		BIT(15)
+#define   PCH_OC_WDT_CTL_EN			BIT(14)
+#define   PCH_OC_WDT_CTL_ICCSURV		BIT(13)
+#define   PCH_OC_WDT_CTL_LCK			BIT(12)
+#define   PCH_OC_WDT_CTL_TOV_MASK		0x3FF
+
 /* Starts and reloads the OC watchdog if enabled in Kconfig */
 void setup_oc_wdt(void);
 
--- src/soc/intel/common/block/oc_wdt/oc_wdt.c
+++ src/soc/intel/common/block/oc_wdt/oc_wdt.c
@@ -7,17 +7,6 @@
 #include <soc/iomap.h>
 #include <types.h>
 
-/* OC WDT configuration */
-#define PCH_OC_WDT_CTL				(ACPI_BASE_ADDRESS + 0x54)
-#define   PCH_OC_WDT_CTL_RLD			BIT(31)
-#define   PCH_OC_WDT_CTL_ICCSURV_STS		BIT(25)
-#define   PCH_OC_WDT_CTL_NO_ICCSURV_STS		BIT(24)
-#define   PCH_OC_WDT_CTL_FORCE_ALL		BIT(15)
-#define   PCH_OC_WDT_CTL_EN			BIT(14)
-#define   PCH_OC_WDT_CTL_ICCSURV		BIT(13)
-#define   PCH_OC_WDT_CTL_LCK			BIT(12)
-#define   PCH_OC_WDT_CTL_TOV_MASK		0x3FF
-
 static struct watchdog_config wdt_config;
 /*
  * Starts and reloads the OC watchdog with given timeout.
@@ -27,6 +16,7 @@ static struct watchdog_config wdt_config;
 static void oc_wdt_start(unsigned int timeout)
 {
 	uint32_t oc_wdt_ctrl;
+	bool bootstatus;
 
 	if ((timeout < 70) || (timeout > (PCH_OC_WDT_CTL_TOV_MASK + 1))) {
 		timeout = CONFIG_SOC_INTEL_COMMON_OC_WDT_TIMEOUT_SECONDS;
@@ -34,11 +24,19 @@ static void oc_wdt_start(unsigned int timeout)
 				     " using config default: %ds\n", timeout);
 	}
 
-	printk(BIOS_SPEW, "OC Watchdog: start and relaod timer (timeout %ds)\n", timeout);
+	printk(BIOS_SPEW, "OC Watchdog: start and reload timer (timeout %ds)\n", timeout);
 
 	oc_wdt_ctrl = inl(PCH_OC_WDT_CTL);
-	oc_wdt_ctrl |= (PCH_OC_WDT_CTL_EN | PCH_OC_WDT_CTL_FORCE_ALL | PCH_OC_WDT_CTL_ICCSURV);
 
+	bootstatus = !!(oc_wdt_ctrl & (PCH_OC_WDT_CTL_ICCSURV_STS | PCH_OC_WDT_CTL_NO_ICCSURV_STS));
+	if (bootstatus)
+		printk(BIOS_ALERT, "OC Watchdog: Last reset caused by timeout!\n");
+
+	/* Store bootstatus in scratch register to be consumed by WDAT */
+	oc_wdt_ctrl &= ~PCH_OC_WDT_CTL_SCRATCH_MASK;
+	oc_wdt_ctrl |= bootstatus << PCH_OC_WDT_CTL_SCRATCH_OFFSET;
+
+	oc_wdt_ctrl |= (PCH_OC_WDT_CTL_EN | PCH_OC_WDT_CTL_FORCE_ALL | PCH_OC_WDT_CTL_ICCSURV);
 
 	oc_wdt_ctrl &= ~PCH_OC_WDT_CTL_TOV_MASK;
 	oc_wdt_ctrl |= (timeout - 1);
