7013e9213604ebd80ba231266753cb331292c4d9
Michał Kopeć <michal.kopec@3mdeb.com>
2024-04-11 18:25:13 +0200

    ec/system76/ec: compress EC update
    Full decompressed EC (128K) doesn't fit into the default CBFS cache size
    (16K). So bump the size to twice of what we need here.
    
    Change-Id: I21e01d0dfb0e2221d5d5f4bf0656e27561df0e06
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>

--- src/ec/system76/ec/Makefile.mk
+++ src/ec/system76/ec/Makefile.mk
@@ -8,6 +8,7 @@ smm-$(CONFIG_DEBUG_SMI) += system76_ec.c
 
 cbfs-files-$(CONFIG_EC_SYSTEM76_EC_UPDATE) += ec.rom
 ec.rom-file :=$(call strip_quotes,$(CONFIG_EC_SYSTEM76_EC_UPDATE_FILE))
+ec.rom-compression := $(CBFS_COMPRESS_FLAG)
 ec.rom-type := raw
 
 endif
