abcb5f5dbaa1c17c2697783b597400393297a346
Michał Kopeć <michal.kopec@3mdeb.com>
2024-04-15 13:00:48 +0200

    oc_wdt/acpi.c: switch back to 32bit accesses
    needed at least on alderlake for correct operation
    
    Change-Id: Idcf5ce66248c0486bc2ea7242c75c6b824c82b75
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>

--- src/soc/intel/common/block/oc_wdt/acpi.c
+++ src/soc/intel/common/block/oc_wdt/acpi.c
@@ -4,9 +4,9 @@
 #include <console/console.h>
 #include <intelblocks/oc_wdt.h>
 
-#define OC_WDT_MIN_VALUE	0
-#define OC_WDT_MAX_VALUE	1023
-#define OC_WDT_PERIOD_MS	1000
+#define OC_WDT_MIN_VALUE 0
+#define OC_WDT_MAX_VALUE 1023
+#define OC_WDT_PERIOD_MS 1000
 
 static bool fill_wdat_timeout_entry(acpi_wdat_entry_t *entry)
 {
@@ -14,42 +14,43 @@ static bool fill_wdat_timeout_entry(acpi_wdat_entry_t *entry)
 
 	entry->action = ACPI_WDAT_SET_COUNTDOWN;
 	entry->instruction = ACPI_WDAT_WRITE_COUNTDOWN | ACPI_WDAT_PRESERVE_REGISTER;
-	entry->mask = PCH_OC_WDT_CTL_TOV_MASK;
+	entry->mask = PCH_OC_WDT_CTL_TOV_MASK | PCH_OC_WDT_CTL_ICCSURV_STS
+		      | PCH_OC_WDT_CTL_NO_ICCSURV_STS;
 	entry->register_region.space_id = ACPI_ADDRESS_SPACE_IO;
 	entry->register_region.addrl = PCH_OC_WDT_CTL;
-	entry->register_region.access_size = ACPI_WDAT_ACCESS_SIZE_WORD;
+	entry->register_region.access_size = ACPI_WDAT_ACCESS_SIZE_DWORD;
 
 	return true;
 }
 
 static bool fill_wdat_boot_status_entry(acpi_wdat_entry_t *entry, uint8_t action,
-					uint8_t instruction, uint32_t value)
+					uint8_t instruction, uint32_t value, uint32_t mask)
 {
 	memset((void *)entry, 0, sizeof(acpi_wdat_entry_t));
 
 	entry->action = action;
 	entry->instruction = instruction;
 	entry->value = value;
-	entry->mask = PCH_OC_WDT_CTL_SCRATCH_MASK >> PCH_OC_WDT_CTL_SCRATCH_OFFSET;
+	entry->mask = mask;
 	entry->register_region.space_id = ACPI_ADDRESS_SPACE_IO;
-	entry->register_region.addrl = PCH_OC_WDT_CTL + 2;
-	entry->register_region.access_size = ACPI_WDAT_ACCESS_SIZE_BYTE;
+	entry->register_region.addrl = PCH_OC_WDT_CTL;
+	entry->register_region.access_size = ACPI_WDAT_ACCESS_SIZE_DWORD;
 
 	return true;
 }
 
 static bool fill_wdat_run_state_entry(acpi_wdat_entry_t *entry, uint8_t action,
-				       uint8_t instruction, uint32_t value)
+				      uint8_t instruction, uint32_t value, uint32_t mask)
 {
 	memset((void *)entry, 0, sizeof(acpi_wdat_entry_t));
 
 	entry->action = action;
 	entry->instruction = instruction;
 	entry->value = value;
-	entry->mask = PCH_OC_WDT_CTL_EN;
+	entry->mask = mask;
 	entry->register_region.space_id = ACPI_ADDRESS_SPACE_IO;
 	entry->register_region.addrl = PCH_OC_WDT_CTL;
-	entry->register_region.access_size = ACPI_WDAT_ACCESS_SIZE_WORD;
+	entry->register_region.access_size = ACPI_WDAT_ACCESS_SIZE_DWORD;
 
 	return true;
 }
@@ -59,12 +60,13 @@ static bool fill_wdat_ping_entry(acpi_wdat_entry_t *entry)
 	memset((void *)entry, 0, sizeof(acpi_wdat_entry_t));
 
 	entry->action = ACPI_WDAT_RESET;
-	entry->instruction = ACPI_WDAT_WRITE_VALUE;
-	entry->value = PCH_OC_WDT_CTL_RLD >> 24;
-	entry->mask = PCH_OC_WDT_CTL_RLD >> 24;
+	entry->instruction = ACPI_WDAT_WRITE_VALUE | ACPI_WDAT_PRESERVE_REGISTER;
+	entry->value = PCH_OC_WDT_CTL_RLD;
+	entry->mask =
+		PCH_OC_WDT_CTL_RLD | PCH_OC_WDT_CTL_ICCSURV_STS | PCH_OC_WDT_CTL_NO_ICCSURV_STS;
 	entry->register_region.space_id = ACPI_ADDRESS_SPACE_IO;
-	entry->register_region.addrl = PCH_OC_WDT_CTL + 3;
-	entry->register_region.access_size = ACPI_WDAT_ACCESS_SIZE_BYTE;
+	entry->register_region.addrl = PCH_OC_WDT_CTL;
+	entry->register_region.access_size = ACPI_WDAT_ACCESS_SIZE_DWORD;
 
 	return true;
 }
@@ -94,23 +96,28 @@ unsigned long acpi_soc_fill_wdat(acpi_wdat_t *wdat, unsigned long current)
 	entry++;
 
 	/* Get boot status */
-	if (!fill_wdat_boot_status_entry(entry, ACPI_WDAT_GET_STATUS,
-					 ACPI_WDAT_READ_VALUE, 1))
+	if (!fill_wdat_boot_status_entry(entry, ACPI_WDAT_GET_STATUS, ACPI_WDAT_READ_VALUE,
+					 PCH_OC_WDT_CTL_SCRATCH_MASK,
+					 PCH_OC_WDT_CTL_SCRATCH_MASK))
+
 		goto out_err;
 
 	entry++;
 
 	/* Set boot status */
 	if (!fill_wdat_boot_status_entry(entry, ACPI_WDAT_SET_STATUS,
-					 ACPI_WDAT_WRITE_VALUE,
-					 0))
+					 ACPI_WDAT_WRITE_VALUE | ACPI_WDAT_PRESERVE_REGISTER, 0,
+					 PCH_OC_WDT_CTL_SCRATCH_MASK
+						 | PCH_OC_WDT_CTL_ICCSURV_STS
+						 | PCH_OC_WDT_CTL_NO_ICCSURV_STS))
+
 		goto out_err;
 
 	entry++;
 
 	/* Get running status */
-	if (!fill_wdat_run_state_entry(entry, ACPI_WDAT_GET_RUNNING_STATE,
-				       ACPI_WDAT_READ_VALUE, PCH_OC_WDT_CTL_EN))
+	if (!fill_wdat_run_state_entry(entry, ACPI_WDAT_GET_RUNNING_STATE, ACPI_WDAT_READ_VALUE,
+				       PCH_OC_WDT_CTL_EN, PCH_OC_WDT_CTL_EN))
 		goto out_err;
 
 	entry++;
@@ -118,22 +125,25 @@ unsigned long acpi_soc_fill_wdat(acpi_wdat_t *wdat, unsigned long current)
 	/* Start the watchdog */
 	if (!fill_wdat_run_state_entry(entry, ACPI_WDAT_SET_RUNNING_STATE,
 				       ACPI_WDAT_WRITE_VALUE | ACPI_WDAT_PRESERVE_REGISTER,
-				       PCH_OC_WDT_CTL_EN))
+				       PCH_OC_WDT_CTL_EN,
+				       PCH_OC_WDT_CTL_EN | PCH_OC_WDT_CTL_ICCSURV_STS
+					       | PCH_OC_WDT_CTL_NO_ICCSURV_STS))
 		goto out_err;
 
 	entry++;
 
 	/* Get stopped status */
-	if (!fill_wdat_run_state_entry(entry, ACPI_WDAT_GET_STOPPED_STATE,
-				       ACPI_WDAT_READ_VALUE, 0))
+	if (!fill_wdat_run_state_entry(entry, ACPI_WDAT_GET_STOPPED_STATE, ACPI_WDAT_READ_VALUE,
+				       0, PCH_OC_WDT_CTL_EN))
 		goto out_err;
 
 	entry++;
 
 	/* Stop the watchdog */
 	if (!fill_wdat_run_state_entry(entry, ACPI_WDAT_SET_STOPPED_STATE,
-				       ACPI_WDAT_WRITE_VALUE | ACPI_WDAT_PRESERVE_REGISTER,
-				       0))
+				       ACPI_WDAT_WRITE_VALUE | ACPI_WDAT_PRESERVE_REGISTER, 0,
+				       PCH_OC_WDT_CTL_EN | PCH_OC_WDT_CTL_ICCSURV_STS
+					       | PCH_OC_WDT_CTL_NO_ICCSURV_STS))
 		goto out_err;
 
 	entry++;
