0d88b56cda91fe20df3f02ae7b4c428d072c3fd7
Michał Żygowski <michal.zygowski@3mdeb.com>
2024-04-17 16:07:01 +0200

    util/txesbmantool: Add utility to generate TXE SB manifests
    Print key hashes when displaying manifests as it may be useful when
    obtaining information from binary about the used keys and what hash
    should be provisioned in TXE.
    
    Change-Id: I288fbb3db6b42e231977683145ce67e8fbd98dfc
    Upstream-Status: Pending
    Co-authored-by: Krystian Hebel <krystian.hebel@3mdeb.com>
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

---
+++ util/txesbmantool/Makefile.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: BSD-3-Clause
+
+txesbmantoolsrc = txesbmantool.c
+
+TXESBMANTOOLCFLAGS  = -Wall -Wextra -Wmissing-prototypes -Werror
+TXESBMANTOOLCFLAGS += $(shell $(HOSTPKGCONFIG) --libs wolfssl)
+
+$(objutil)/txesbmantool/txesbmantool: $(addprefix $(top)/util/txesbmantool/,$(txesbmantoolsrc))
+	printf "   TXESBMANTOOL\n"
+	mkdir -p $(objutil)/txesbmantool
+	$(HOSTCC) $(HOSTCFLAGS) -o $@ $< $(TXESBMANTOOLCFLAGS)
