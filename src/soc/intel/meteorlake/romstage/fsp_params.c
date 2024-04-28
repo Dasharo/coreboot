d09385e24e9584d4ddb59ae5e014e5b1b218e25b
Michał Kopeć <michal.kopec@3mdeb.com>
2024-04-28 14:22:23 +0200

    src/soc/intel/meteorlake: hax to make it boot
    Change-Id: I183e195353a922dae59540dc87155ee89d28d0e4
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>

--- src/soc/intel/meteorlake/romstage/fsp_params.c
+++ src/soc/intel/meteorlake/romstage/fsp_params.c
@@ -445,51 +445,51 @@ static void soc_memory_init_params(FSP_M_CONFIG *m_cfg,
 /* Tear down legacy VGA mode before exiting FSP-M. */
 #define VGA_INIT_CONTROL_TEAR_DOWN	BIT(1)
 
-static void fill_fspm_sign_of_life(FSP_M_CONFIG *m_cfg,
-				   FSPM_ARCH_UPD *arch_upd)
-{
-	void *vbt;
-	size_t vbt_size;
-	uint32_t vga_init_control = 0;
-	uint8_t sol_type;
-
-	/* Memory training.  */
-	if (!arch_upd->NvsBufferPtr) {
-		vga_init_control = VGA_INIT_CONTROL_ENABLE |
-			VGA_INIT_CONTROL_TEAR_DOWN;
-		sol_type = ELOG_FW_EARLY_SOL_MRC;
-	}
-
-	if (is_cse_fw_update_required()) {
-		vga_init_control = VGA_INIT_CONTROL_ENABLE;
-		sol_type = ELOG_FW_EARLY_SOL_CSE_SYNC;
-	}
-
-	if (!vga_init_control)
-		return;
-
-	const char *text = ux_locales_get_text(UX_MEMORY_TRAINING_DESC);
-	/* No localized text found; fallback to built-in English. */
-	if (!text)
-		text = "Your device is finishing an update. "
-		       "This may take 1-2 minutes.\n"
-		       "Please do not turn off your device.";
-
-	vbt = cbfs_map("vbt.bin", &vbt_size);
-	if (!vbt) {
-		printk(BIOS_ERR, "Could not load vbt.bin\n");
-		return;
-	}
-
-	printk(BIOS_INFO, "Enabling FSP-M Sign-of-Life\n");
-	elog_add_event_byte(ELOG_TYPE_FW_EARLY_SOL, sol_type);
-
-	m_cfg->VgaInitControl = vga_init_control;
-	m_cfg->VbtPtr = (UINT32)vbt;
-	m_cfg->VbtSize = vbt_size;
-	m_cfg->LidStatus = CONFIG(VBOOT_LID_SWITCH) ? get_lid_switch() : CONFIG(RUN_FSP_GOP);
-	m_cfg->VgaMessage = (UINT32)text;
-}
+//static void fill_fspm_sign_of_life(FSP_M_CONFIG *m_cfg,
+//				   FSPM_ARCH_UPD *arch_upd)
+//{
+//	void *vbt;
+//	size_t vbt_size;
+//	uint32_t vga_init_control = 0;
+//	uint8_t sol_type;
+//
+//	/* Memory training.  */
+//	if (!arch_upd->NvsBufferPtr) {
+//		vga_init_control = VGA_INIT_CONTROL_ENABLE |
+//			VGA_INIT_CONTROL_TEAR_DOWN;
+//		sol_type = ELOG_FW_EARLY_SOL_MRC;
+//	}
+//
+//	if (is_cse_fw_update_required()) {
+//		vga_init_control = VGA_INIT_CONTROL_ENABLE;
+//		sol_type = ELOG_FW_EARLY_SOL_CSE_SYNC;
+//	}
+//
+//	if (!vga_init_control)
+//		return;
+//
+//	const char *text = ux_locales_get_text(UX_MEMORY_TRAINING_DESC);
+//	/* No localized text found; fallback to built-in English. */
+//	if (!text)
+//		text = "Your device is finishing an update. "
+//		       "This may take 1-2 minutes.\n"
+//		       "Please do not turn off your device.";
+//
+//	vbt = cbfs_map("vbt.bin", &vbt_size);
+//	if (!vbt) {
+//		printk(BIOS_ERR, "Could not load vbt.bin\n");
+//		return;
+//	}
+//
+//	printk(BIOS_INFO, "Enabling FSP-M Sign-of-Life\n");
+//	elog_add_event_byte(ELOG_TYPE_FW_EARLY_SOL, sol_type);
+//
+//	m_cfg->VgaInitControl = vga_init_control;
+//	m_cfg->VbtPtr = (UINT32)vbt;
+//	m_cfg->VbtSize = vbt_size;
+//	m_cfg->LidStatus = CONFIG(VBOOT_LID_SWITCH) ? get_lid_switch() : CONFIG(RUN_FSP_GOP);
+//	m_cfg->VgaMessage = (UINT32)text;
+//}
 
 void platform_fsp_memory_init_params_cb(FSPM_UPD *mupd, uint32_t version)
 {
@@ -517,8 +517,8 @@ void platform_fsp_memory_init_params_cb(FSPM_UPD *mupd, uint32_t version)
 
 	soc_memory_init_params(m_cfg, config);
 
-	if (CONFIG(SOC_INTEL_METEORLAKE_SIGN_OF_LIFE))
-		fill_fspm_sign_of_life(m_cfg, arch_upd);
+	//if (CONFIG(SOC_INTEL_METEORLAKE_SIGN_OF_LIFE))
+	//	fill_fspm_sign_of_life(m_cfg, arch_upd);
 
 	mainboard_memory_init_params(mupd);
 }
