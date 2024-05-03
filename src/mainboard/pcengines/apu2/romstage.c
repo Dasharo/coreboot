f4e34dc0a489a17702e92c763de4d1d3f1a5ab7e
Piotr Król <piotr.krol@3mdeb.com>
2024-05-03 13:12:57 +0200

    mainboard/pcengines: remove GPIO_CONFIG_DEV
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- src/mainboard/pcengines/apu2/romstage.c
+++ src/mainboard/pcengines/apu2/romstage.c
@@ -25,7 +25,6 @@
 #define SIO_PORT 0x2e
 #define SERIAL1_DEV PNP_DEV(SIO_PORT, NCT5104D_SP1)
 #define SERIAL2_DEV PNP_DEV(SIO_PORT, NCT5104D_SP2)
-#define GPIO_CONFIG_DEV  PNP_DEV(SIO_PORT, NCT5104D_GPIO_WDT)
 
 static void early_lpc_init(void);
 
