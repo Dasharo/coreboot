6fffbcc6069587429dee5240a7095ee97a77e48c
Piotr Król <piotr.krol@3mdeb.com>
2024-05-03 13:10:32 +0200

    mainboard/pcengines: send commands to the LPC MCU
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/mainboard/pcengines/apu2/bootblock.c
+++ src/mainboard/pcengines/apu2/bootblock.c
@@ -6,6 +6,8 @@
 #include <superio/nuvoton/common/nuvoton.h>
 #include <superio/nuvoton/nct5104d/nct5104d.h>
 
+#include "bios_knobs.h"
+
 #define SIO_PORT 0x2e
 #define SERIAL1_DEV PNP_DEV(SIO_PORT, NCT5104D_SP1)
 #define SERIAL2_DEV PNP_DEV(SIO_PORT, NCT5104D_SP2)
@@ -15,10 +17,10 @@ void bootblock_mainboard_early_init(void)
 	hudson_lpc_port80();
 	hudson_clk_output_48Mhz();
 
-	/* COM2 on apu5 is reserved so only COM1 should be supported */
-	if ((CONFIG_UART_FOR_CONSOLE == 1) &&
-		!CONFIG(BOARD_PCENGINES_APU5))
-		nuvoton_enable_serial(SERIAL2_DEV, CONFIG_TTYS0_BASE);
-	else if (CONFIG_UART_FOR_CONSOLE == 0)
-		nuvoton_enable_serial(SERIAL1_DEV, CONFIG_TTYS0_BASE);
+	/* Enable UARTB for LPC MCU */
+	if (CONFIG(BOARD_PCENGINES_APU5))
+		nuvoton_enable_serial(SERIAL2_DEV, 0x2f8);
+
+	if ((check_com2() || (CONFIG_UART_FOR_CONSOLE == 1)))
+		nuvoton_enable_serial(SERIAL2_DEV, 0x2f8);
 }
