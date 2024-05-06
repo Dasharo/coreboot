1a121db4770aaed2836495787c287c423d73b2ce
Michał Kopeć <michal.kopec@3mdeb.com>
2024-05-06 11:12:44 +0200

    meteorlake/romstage/fsp_params.c: wire up DMA protection
    Upstream-Status: Inappropriate [Dasharo downstream]
    Change-Id: I500f4b502e1ac58e437e53bd0e4cb934c7bb3bbc
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>

--- src/soc/intel/meteorlake/romstage/fsp_params.c
+++ src/soc/intel/meteorlake/romstage/fsp_params.c
@@ -8,6 +8,7 @@
 #include <cpu/intel/cpu_ids.h>
 #include <cpu/x86/msr.h>
 #include <cpu/x86/mtrr.h>
+#include <dasharo/options.h>
 #include <device/device.h>
 #include <device/pci.h>
 #include <drivers/wifi/generic/wifi.h>
@@ -368,6 +369,8 @@ static void fill_fspm_vtd_params(FSP_M_CONFIG *m_cfg,
 
 	/* Change VmxEnable UPD value according to ENABLE_VMX Kconfig */
 	m_cfg->VmxEnable = CONFIG(ENABLE_VMX);
+
+	m_cfg->PreBootDmaMask = CONFIG(ENABLE_EARLY_DMA_PROTECTION) && dma_protection_enabled();
 }
 
 static void fill_fspm_trace_params(FSP_M_CONFIG *m_cfg,
