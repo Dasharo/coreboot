a129a22104e40c278fa5a1a096bb9a409f4d7d6b
Krystian Hebel <krystian.hebel@3mdeb.com>
2024-05-15 18:08:30 +0200

    mb/qemu-{i440fx,q35}/rom_media.c: add code for writable flash
    Depending on how firmware image was passed to QEMU, it may behave as:
    - ROM - memory mapped reads, writes are ignored (FW image mounted with
      '-bios');
    - RAM - memory mapped reads and writes (FW image mounted with e.g.
      '-device loader');
    - flash - memory mapped reads, write and erase possible through
      commands. Contrary to physical flash devices erase is not required
      before writing, but it also doesn't hurt. Flash may be split into
      read-only and read-write parts, like OVMF_CODE.fd and OVMF_VARS.fd.
      Combined size of system firmware must not exceed 8 MiB (FW image(s)
      mounted with '-drive if=pflash').
    
    This function detects which of the above applies and fills
    region_device_ops accordingly.
    
    Tested by starting QEMU with firmware passed as '-drive if=pflash',
    '-drive if=pflash,readonly=on' and '-bios'. When started with firmware
    passed through '-device loader', coreboot complains about missing or
    corrupted FMAP, but this is the same behavior as without this change.
    
    Change-Id: I3ab9f22c6165064a769881d4be5eab13a0a2f519
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>

---
+++ src/mainboard/emulation/qemu-i440fx/rom_media.c
@@ -0,0 +1,184 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+/* Inspired by OvmfPkg/QemuFlashFvbServicesRuntimeDxe/QemuFlash.c from edk2 */
+
+#include <boot_device.h>
+#include <console/console.h>
+#include <commonlib/helpers.h>
+#include <commonlib/region.h>
+#include <string.h>
+
+#define WRITE_BYTE_CMD		0x10
+#define BLOCK_ERASE_CMD		0x20
+#define CLEAR_STATUS_CMD	0x50
+#define READ_STATUS_CMD		0x70
+#define BLOCK_ERASE_CONFIRM_CMD	0xD0
+#define READ_ARRAY_CMD		0xFF
+
+#define CLEARED_ARRAY_STATUS	0x00
+
+#define QEMU_FLASH_BLOCK_SIZE	0x1000
+
+static ssize_t qemu_writeat(const struct region_device *rd, const void *b,
+				size_t offset, size_t size)
+{
+	const struct mem_region_device *mdev;
+	size_t i;
+	volatile char *ptr;
+	const char *buf = b;
+
+	mdev = container_of(rd, __typeof__(*mdev), rdev);
+	ptr = &mdev->base[offset];
+
+	for (i = 0; i < size; i++) {
+		*ptr = WRITE_BYTE_CMD;
+		*ptr = buf[i];
+		ptr++;
+	}
+
+	/* Restore flash to read mode. */
+	if (size > 0) {
+		*(ptr - 1) = READ_ARRAY_CMD;
+	}
+
+	return size;
+}
+
+static ssize_t qemu_eraseat(const struct region_device *rd, size_t offset,
+				size_t size)
+{
+	const struct mem_region_device *mdev;
+	size_t i;
+	volatile char *ptr;
+
+	mdev = container_of(rd, __typeof__(*mdev), rdev);
+	ptr = &mdev->base[offset];
+
+	if (!IS_ALIGNED(offset, QEMU_FLASH_BLOCK_SIZE)) {
+		printk(BIOS_ERR, "%s: erased offset isn't multiple of block size\n",
+		       __func__);
+		return -1;
+	}
+
+	if (!IS_ALIGNED(size, QEMU_FLASH_BLOCK_SIZE)) {
+		printk(BIOS_ERR, "%s: erased size isn't multiple of block size\n",
+		       __func__);
+		return -1;
+	}
+
+	for (i = 0; i < size; i += QEMU_FLASH_BLOCK_SIZE) {
+		*ptr = BLOCK_ERASE_CMD;
+		*ptr = BLOCK_ERASE_CONFIRM_CMD;
+		ptr += QEMU_FLASH_BLOCK_SIZE;
+	}
+
+	/* Restore flash to read mode. */
+	if (size > 0) {
+		*(ptr - 1) = READ_ARRAY_CMD;
+	}
+
+	return size;
+}
+
+static struct region_device_ops flash_ops;
+static struct mem_region_device boot_dev;
+
+/*
+ * Depending on how firmware image was passed to QEMU, it may behave as:
+ *
+ * - ROM - memory mapped reads, writes are ignored (FW image mounted with
+ *   '-bios');
+ * - RAM - memory mapped reads and writes (FW image mounted with e.g.
+ *   '-device loader');
+ * - flash - memory mapped reads, write and erase possible through commands.
+ *   Contrary to physical flash devices erase is not required before writing,
+ *   but it also doesn't hurt. Flash may be split into read-only and read-write
+ *   parts, like OVMF_CODE.fd and OVMF_VARS.fd. Combined size of system firmware
+ *   must not exceed 8 MiB (FW image(s) mounted with '-drive if=pflash').
+ *
+ * This function detects which of the above applies and fills region_device_ops
+ * accordingly.
+ */
+void boot_device_init(void)
+{
+	volatile char *ptr;
+	char original, readback;
+	static bool initialized = false;
+
+	if (initialized)
+		return;
+
+	/*
+	 * mmap, munmap and readat are always identical to mem_rdev_rw_ops, other
+	 * functions may vary.
+	 */
+	flash_ops = mem_rdev_rw_ops;
+
+	boot_dev.base = (void *)(uintptr_t)(0x100000000ULL - CONFIG_ROM_SIZE);
+	boot_dev.rdev.ops = &flash_ops;
+	boot_dev.rdev.region.size = CONFIG_ROM_SIZE;
+
+	/* Detect what type of flash we're dealing with. */
+	ptr = (volatile char *)((uintptr_t)boot_dev.base + 0x10000);
+
+	/* Find first byte different than any of the commands, simplified. */
+	original = *ptr;
+	while (original == (char)0xFF || (original & 0x0F) == 0)
+		original = *++ptr;
+
+	*ptr = CLEAR_STATUS_CMD;
+	readback = *ptr;
+	if ((original != CLEAR_STATUS_CMD) && (readback == CLEAR_STATUS_CMD))
+	{
+		printk(BIOS_DEBUG, "QEMU flash behaves as RAM\n");
+		/* Restore original content. */
+		*ptr = original;
+	} else {
+		/* In case original value was CLEAR_STATUS_CMD, RAM is checked again. */
+		*ptr = READ_STATUS_CMD;
+		readback = *ptr;
+		if (readback == original) {
+			printk(BIOS_DEBUG, "QEMU flash behaves as ROM\n");
+			/* ROM means no writing nor erasing. */
+			flash_ops.writeat = NULL;
+			flash_ops.eraseat = NULL;
+		} else if (readback == READ_STATUS_CMD) {
+			printk(BIOS_DEBUG, "QEMU flash behaves as RAM\n");
+			/* Restore original content. */
+			*ptr = original;
+		} else if (readback == CLEARED_ARRAY_STATUS) {
+			/* Try writing original value to test whether flash is writable. */
+			*ptr = WRITE_BYTE_CMD;
+			*ptr = original;
+			*ptr = READ_STATUS_CMD;
+			readback = *ptr;
+			*ptr = READ_ARRAY_CMD;
+			if (readback & 0x10 /* programming error */) {
+				printk(BIOS_DEBUG,
+				       "QEMU flash behaves as write-protected flash\n");
+				flash_ops.writeat = NULL;
+				flash_ops.eraseat = NULL;
+			} else {
+				printk(BIOS_DEBUG, "QEMU flash behaves as writable flash\n");
+				flash_ops.writeat = qemu_writeat;
+				flash_ops.eraseat = qemu_eraseat;
+			}
+		} else {
+			printk(BIOS_ERR, "Unexpected QEMU flash behavior\n");
+			/*
+			 * This shouldn't happen and first byte of flash may already be
+			 * corrupted by testing, but don't take any further risk.
+			 */
+			flash_ops.writeat = NULL;
+			flash_ops.eraseat = NULL;
+		}
+	}
+
+	initialized = true;
+}
+
+/* boot_device_ro() is defined in arch/x86/mmap_boot.c */
+const struct region_device *boot_device_rw(void)
+{
+	return &boot_dev.rdev;
+}
