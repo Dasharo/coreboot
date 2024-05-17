Dasharo/coreboot

commit 8cab580c861988aba2754939a1748dc41966cc8c

vendorcode/dasharo/options.c: fix building for AMD
Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>

src/vendorcode/dasharo/options.c
@@ -3,12 +3,15 @@
 #include <console/console.h>
 #include <dasharo/options.h>
 #include <drivers/efi/efivars.h>
-#include <intelblocks/cse.h>
 #include <option.h>
 #include <soc/intel/common/reset.h>
 #include <smmstore.h>
 #include <types.h>
 #include <uuid.h>
+
+#if !CONFIG(SOC_AMD_COMMON)
+#include <intelblocks/cse.h>
+#endif
 
 static const EFI_GUID dasharo_system_features_guid = {
 	0xd15b327e, 0xff2d, 0x4fc1, { 0xab, 0xf6, 0xc1, 0x2b, 0xd0, 0x8c, 0x13, 0x59 }
@@ -151,6 +154,7 @@ bool dma_protection_enabled(void)
 	return iommu_var.iommu_enable;
 }
 
+#if !CONFIG(SOC_AMD_COMMON)
 static void disable_me_hmrfpo(uint8_t *var)
 {
 	int hmrfpo_sts = cse_hmrfpo_get_status();
@@ -212,6 +216,12 @@ uint8_t cse_get_me_disable_mode(void)
 
 	return var;
 }
+#else
+uint8_t cse_get_me_disable_mode(void)
+{
+	return 0;
+}
+#endif
 
 bool is_smm_bwp_permitted(void)
 {

commit ecc3ff919288f70a4366341a2a7d8b5a6b704956

vendorcode/dasharo/options.c: use proper Kconfig option
SOC_AMD_COMMON doesn't make sense, SOC_INTEL_COMMON_BLOCK_CSE is much
better for guarding functions that depend on CSE.

Fixes: 8cab580c8619 vendorcode/dasharo/options.c: fix building for AMD

Change-Id: I8976322ebe9e7574cdcb21012b3980391d1b3542
Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>

src/vendorcode/dasharo/options.c
@@ -9,7 +9,7 @@
 #include <types.h>
 #include <uuid.h>
 
-#if !CONFIG(SOC_AMD_COMMON)
+#if CONFIG(SOC_INTEL_COMMON_BLOCK_CSE)
 #include <intelblocks/cse.h>
 #endif
 
@@ -205,7 +205,7 @@ bool dma_protection_enabled(void)
 	return iommu_var.iommu_enable;
 }
 
-#if !CONFIG(SOC_AMD_COMMON)
+#if CONFIG(SOC_INTEL_COMMON_BLOCK_CSE)
 static void disable_me_hmrfpo(uint8_t *var)
 {
 	int hmrfpo_sts = cse_hmrfpo_get_status();