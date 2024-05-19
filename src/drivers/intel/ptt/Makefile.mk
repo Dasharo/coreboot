7fa65d2fa243290e286182a808701cbbfa34c8d6
Michał Żygowski <michal.zygowski@3mdeb.com>
2024-05-19 23:56:35 +0200

    src/drivers/intel/ptt/Makefile.mk: Compile PTT in bootblock
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/drivers/intel/ptt/Makefile.mk
+++ src/drivers/intel/ptt/Makefile.mk
@@ -1,5 +1,6 @@
 ## SPDX-License-Identifier: GPL-2.0-only
 
+bootblock-$(CONFIG_HAVE_INTEL_PTT) += ptt.c
 romstage-$(CONFIG_HAVE_INTEL_PTT) += ptt.c
 ramstage-$(CONFIG_HAVE_INTEL_PTT) += ptt.c
 postcar-$(CONFIG_HAVE_INTEL_PTT) += ptt.c
