7335f994a10568a740d460cff091acec058ba87a
Michał Kopeć <michal.kopec@3mdeb.com>
2024-05-21 15:04:10 +0200

    clevo/mtl-h/gpio_early.c: configure GPIOs for FSP-M SOL
    Change-Id: I8d7d8f3f50e9cf05a12b152f5f12e6e2cc19e652
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>

--- src/mainboard/clevo/mtl-h/gpio_early.c
+++ src/mainboard/clevo/mtl-h/gpio_early.c
@@ -17,7 +17,25 @@ static const struct pad_config early_gpio_table[] = {
 	_PAD_CFG_STRUCT(GPP_C01, PAD_FUNC(NF1) | PAD_RESET(DEEP) | PAD_TRIG(OFF) | PAD_BUF(TX_RX_DISABLE) | (1 << 1), PAD_PULL(UP_20K) | PAD_IOSSTATE(IGNORE)),
 };
 
+/* Pins needed for graphical FSP-M SOL */
+static const struct pad_config early_vga_gpio_table[] = {
+	/* GPP_B11 - DDSP_HPD2 */
+	PAD_CFG_NF(GPP_B11, NONE, DEEP, NF2),
+	/* GPP_C20 - DDP2_CTRLCLK */
+	PAD_CFG_NF(GPP_C20, NONE, DEEP, NF2),
+	/* GPP_C21 - DDP2_CTRLDATA */
+	PAD_CFG_NF(GPP_C21, NONE, DEEP, NF2),
+	/* GPP_E14 - DDSP_HPDA */
+	PAD_CFG_NF(GPP_E14, NONE, DEEP, NF1),
+	/* GPP_D00 - GPIO */
+	PAD_CFG_GPO(GPP_D00, 1, DEEP), /* SB_BLON */
+};
+
 void mainboard_configure_early_gpios(void)
 {
 	gpio_configure_pads(early_gpio_table, ARRAY_SIZE(early_gpio_table));
+
+	/* XXX: Doesn't actually work with any FSP bins tested so far */
+	if (CONFIG(SOC_INTEL_METEORLAKE_SIGN_OF_LIFE))
+		gpio_configure_pads(early_vga_gpio_table, ARRAY_SIZE(early_vga_gpio_table));
 }
