14b320d90a17b287a2c4293569ae5d9a71cf260c
Michał Kopeć <michal.kopec@3mdeb.com>
2024-05-24 10:37:12 +0200

    soc/intel/meteorlake/Makefile.mk: include missing MemInfoHob from vendorcode
    Change-Id: If7203621e7767bae8ad791947e61e5aefb31bc19
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>

--- src/soc/intel/meteorlake/Makefile.mk
+++ src/soc/intel/meteorlake/Makefile.mk
@@ -60,6 +60,9 @@ smm-y += xhci.c
 CPPFLAGS_common += -I$(src)/soc/intel/meteorlake
 CPPFLAGS_common += -I$(src)/soc/intel/meteorlake/include
 
+# Include the missing MemInfoHob.h from vendorcode
+CPPFLAGS_common += -I$(src)/vendorcode/intel/fsp/fsp2_0/meteorlake
+
 CFLAGS_common += -fshort-wchar
 
 cpu_microcode_bins += 3rdparty/intel-microcode/intel-ucode/06-aa-04
