c141a816a7f919f14b1c0b455a5e6710b8e5ff1c
Michał Kopeć <michal.kopec@3mdeb.com>
2024-06-05 12:14:36 +0200

    mainboard/clevo/mtl-h: move tcc setting to ramstage
    Change-Id: Ia5ac07ef7ab75049787d50c363ac93b6597b3a2a
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>

--- src/mainboard/clevo/mtl-h/romstage.c
+++ src/mainboard/clevo/mtl-h/romstage.c
@@ -3,26 +3,6 @@
 #include <soc/meminit.h>
 #include <soc/romstage.h>
 
-#if CONFIG(EDK2_CPU_THROTTLING_THRESHOLD_OPTION)
-static void set_cpu_throttling_threshold(void)
-{
-	config_t *cfg = config_of_soc();
-
-	// read the CpuThrottlingThreshold efivar
-	uint8_t cpu_throttling_threshold;
-	cpu_throttling_threshold = get_cpu_throttling_threshold();
-	printk(BIOS_DEBUG, "CPU throttling threshold: %d\n", cpu_throttling_threshold);
-
-	// read tjmax from config
-	uint8_t tjmax;
-	tjmax = CONFIG_CPU_MAX_TEMPERATURE;
-	printk(BIOS_DEBUG, "CPU max. temperature (TjMax): %d\n", tjmax);
-
-	cfg->tcc_offset = tjmax - cpu_throttling_threshold;
-	printk(BIOS_DEBUG, "CPU tcc_offset set to: %d\n", cfg->tcc_offset);
-}
-#endif
-
 void mainboard_memory_init_params(FSPM_UPD *mupd)
 {
 	const bool half_populated = false;
@@ -57,9 +37,6 @@ void mainboard_memory_init_params(FSPM_UPD *mupd)
 			},
 		},
 	};
-
-	if (CONFIG(EDK2_CPU_THROTTLING_THRESHOLD_OPTION))
-		set_cpu_throttling_threshold();
 
 	memcfg_init(mupd, &mem_config, &dimm_module_spd_info, half_populated);
 }
