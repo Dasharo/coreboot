9b2da25e07ec4540ae3b88d5cd46b2b35df8f13e
Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
2024-07-05 00:45:52 +0300

    security/tpm: replace TPM_MEASURE_ALGO with tpm_log_alg()
    No functional changes.  This replaces a macro with an inline function to
    make code more readable and more convenient to extend in the future.
    
    Upstream-Status: Pending
    Change-Id: I456bc3bb749a9b58fba72f5562195525e55290bf
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/security/tpm/tspi/crtm.h
+++ src/security/tpm/tspi/crtm.h
@@ -9,33 +9,6 @@
 #include <types.h>
 #include <vb2_sha.h>
 
-#if CONFIG(TPM_LOG_CB)
-#  define TPM_MEASURE_ALGO (tlcl_get_family() == TPM_1 ? VB2_HASH_SHA1 : VB2_HASH_SHA256)
-#elif CONFIG(TPM_LOG_TPM1)
-#  define TPM_MEASURE_ALGO VB2_HASH_SHA1
-#elif CONFIG(TPM_LOG_TPM2)
-#  if CONFIG(TPM_HASH_SHA1)
-#    define TPM_MEASURE_ALGO VB2_HASH_SHA1
-#  endif
-#  if CONFIG(TPM_HASH_SHA256)
-#    define TPM_MEASURE_ALGO VB2_HASH_SHA256
-#  endif
-#  if CONFIG(TPM_HASH_SHA384)
-#    define TPM_MEASURE_ALGO VB2_HASH_SHA384
-#  endif
-#  if CONFIG(TPM_HASH_SHA512)
-#    define TPM_MEASURE_ALGO VB2_HASH_SHA512
-#  endif
-#endif
-
-#if !defined(TPM_MEASURE_ALGO)
-#  if !CONFIG(TPM_MEASURED_BOOT)
-#    define TPM_MEASURE_ALGO VB2_HASH_INVALID
-#  else
-#    error "Misconfiguration: failed to determine TPM hashing algorithm"
-#  endif
-#endif
-
 /**
  * Measure digests cached in TPM log entries into PCRs
  */
