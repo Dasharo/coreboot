aee011e1b5bababaade18a48e24833051b51eb79
Michał Żygowski <michal.zygowski@3mdeb.com>
2024-07-17 15:49:14 +0200

    mainboard/protectli/vault_jsl: Eliminate differences in SMBIOS
    Eliminate differences in SMBIOS fields compared to proprietary firmware
    to make Windows Update fetch the updates for the board.
    
    TEST=Fresh install Windows 11 on V1610 and see that drivers are found
    automatically for the board.
    
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/mainboard/protectli/vault_jsl/mainboard.c
+++ src/mainboard/protectli/vault_jsl/mainboard.c
@@ -3,10 +3,157 @@
 #include <arch/mmio.h>
 #include <device/device.h>
 #include <pc80/i8254.h>
+#include <device/pci_ids.h>
+#include <smbios.h>
 #include <soc/iomap.h>
+#include <soc/pci_devs.h>
 #include <soc/ramstage.h>
 
 #define FSP_PCH_PCIE_ASPM_L1 2
+
+smbios_enclosure_type smbios_mainboard_enclosure_type(void)
+{
+	return SMBIOS_ENCLOSURE_LOW_PROFILE_DESKTOP;
+}
+
+u8 smbios_mainboard_feature_flags(void)
+{
+	return SMBIOS_FEATURE_FLAGS_HOSTING_BOARD | SMBIOS_FEATURE_FLAGS_REPLACEABLE;
+}
+
+const char *smbios_system_sku(void)
+{
+	return CONFIG_MAINBOARD_PART_NUMBER "-01";
+}
+
+const char *smbios_chassis_sku(void)
+{
+	return CONFIG_MAINBOARD_PART_NUMBER "-001";
+}
+
+ const char *smbios_chassis_version(void)
+{
+	return "1.2";
+}
+
+static int smbios_type41_write_ethernet(struct device *parent, int *handle,
+					unsigned long *current)
+{
+	struct device *eth;
+	static u8 instance = 0;
+
+	if (!parent || !parent->enabled)
+		return 0;
+
+	eth = pcidev_path_behind(parent->link_list, PCI_DEVFN(0, 0));
+	if (!eth || !eth->enabled)
+		return 0;
+
+	/* Only i226 Ehternet Controller */
+	if (eth->vendor != PCI_VID_INTEL || eth->device != 0x125c)
+		return 0;
+
+	return smbios_write_type41(
+		current, handle,
+		"Onboard - Ethernet",		/* name */
+		instance++,			/* instance */
+		0,				/* segment */
+		eth->bus->secondary,		/* bus */
+		PCI_SLOT(eth->path.pci.devfn),	/* device */
+		PCI_FUNC(eth->path.pci.devfn),	/* function */
+		SMBIOS_DEVICE_TYPE_ETHERNET);	/* device type */
+}
+
+static int smbios_type41_write_ethernet_behind_switch(struct device *root_port, int *handle,
+						      unsigned long *current)
+{
+	struct device *switch_dev;
+	struct device *child;
+	int len = 0;
+
+	if (!root_port || !root_port->enabled)
+		return 0;
+
+	switch_dev = pcidev_path_behind(root_port->link_list, PCI_DEVFN(0, 0));
+	if (!switch_dev || !switch_dev->enabled)
+		return 0;
+
+	/* Only ASMedia switch */
+	if (switch_dev->vendor != 0x1b21 || switch_dev->device != 0x2806)
+		return 0;
+
+	/* After upstream port there is also one downstream port */
+	switch_dev = pcidev_path_behind(switch_dev->link_list, PCI_DEVFN(0, 0));
+	if (!switch_dev || !switch_dev->enabled)
+		return 0;
+
+	/* Only ASMedia switch */
+	if (switch_dev->vendor != 0x1b21 || switch_dev->device != 0x2806)
+		return 0;
+
+	/* Loop through all downstream ports of the switch */
+	for (child = switch_dev->bus->children; child; child = child->sibling) {
+		printk(BIOS_DEBUG, "Switch DS port %s [%04x:%04x]\n",
+			dev_path(child), child->vendor, child->device);
+		len += smbios_type41_write_ethernet(child, handle, current);
+	}
+
+	return len;
+}
+
+static int mainboard_smbios_data(struct device *dev, int *handle,
+				 unsigned long *current)
+{
+	int len = 0;
+
+	len += smbios_write_type41(
+		current, handle,
+		"Onboard - USB Controller",	/* name */
+		0,				/* instance */
+		0,				/* segment */
+		0,				/* bus */
+		PCH_DEV_SLOT_XHCI,		/* device */
+		0,				/* function */
+		SMBIOS_DEVICE_TYPE_OTHER);	/* device type */
+
+	len += smbios_write_type41(
+		current, handle,
+		"Onboard - HECI",		/* name */
+		0,				/* instance */
+		0,				/* segment */
+		0,				/* bus */
+		PCH_DEV_SLOT_CSE,		/* device */
+		0,				/* function */
+		SMBIOS_DEVICE_TYPE_OTHER);	/* device type */
+
+	len += smbios_write_type41(
+		current, handle,
+		"Onboard - eMMC",		/* name */
+		0,				/* instance */
+		0,				/* segment */
+		0,				/* bus */
+		PCH_DEV_SLOT_STORAGE,		/* device */
+		0,				/* function */
+		SMBIOS_DEVICE_TYPE_OTHER);	/* device type */
+
+	if (!CONFIG(BOARD_PROTECTLI_V1610)) {
+		len += smbios_type41_write_ethernet(PCH_DEV_PCIE1, handle, current);
+		len += smbios_type41_write_ethernet(PCH_DEV_PCIE2, handle, current);
+	} else {
+		len += smbios_type41_write_ethernet_behind_switch(PCH_DEV_PCIE1, handle,
+								  current);
+		len += smbios_type41_write_ethernet(PCH_DEV_PCIE5, handle, current);
+	}
+	len += smbios_type41_write_ethernet(PCH_DEV_PCIE6, handle, current);
+	len += smbios_type41_write_ethernet(PCH_DEV_PCIE7, handle, current);
+
+	return len;
+}
+
+static void mainboard_enable(struct device *dev)
+{
+	dev->ops->get_smbios_data = mainboard_smbios_data;
+}
 
 void mainboard_silicon_init_params(FSP_S_CONFIG *params)
 {
@@ -73,4 +220,5 @@ static void mainboard_final(void *unused)
 
 struct chip_operations mainboard_ops = {
 	.final = mainboard_final,
+	.enable_dev = mainboard_enable,
 };
