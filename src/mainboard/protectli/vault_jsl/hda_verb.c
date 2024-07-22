5368a3783194f5767e66998cb3f2189ce21bb071
Michał Żygowski <michal.zygowski@3mdeb.com>
2024-07-22 10:49:17 +0200

    src/mainboard/protectli/vault_jsl/hda_verb.c: Add Intel-specific verbs
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/mainboard/protectli/vault_jsl/hda_verb.c
+++ src/mainboard/protectli/vault_jsl/hda_verb.c
@@ -6,13 +6,21 @@ const u32 cim_verb_data[] = {
 	/* JasperLake HDMI */
 	0x8086281a, /* Vendor ID */
 	0x80860101, /* Subsystem ID */
-	6, /* Number of entries */
+	8, /* Number of entries */
 	AZALIA_SUBVENDOR(2, 0x80860101),
+	0x00278111,
+	0x00278111,
+	0x00278111,
+	0x00278111,
 	AZALIA_PIN_CFG(2, 0x04, 0x18560010),
 	AZALIA_PIN_CFG(2, 0x06, 0x18560010),
 	AZALIA_PIN_CFG(2, 0x08, 0x18560010),
 	AZALIA_PIN_CFG(2, 0x0a, 0x18560010),
-	AZALIA_PIN_CFG(2, 0x0b, 0x18560010)
+	AZALIA_PIN_CFG(2, 0x0b, 0x18560010),
+	0x00278100,
+	0x00278100,
+	0x00278100,
+	0x00278100
 };
 
 const u32 pc_beep_verbs[] = {};
