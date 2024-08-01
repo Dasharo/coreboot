558a33563a2ebd5b469f9667224b779ac8a1b551
Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
2024-08-01 20:12:57 +0300

    vendorcode/intel: allow selecting EDK headers manually
    EFI-specific options depend on CONFIG_UDK_BASE which gets set by
    CONFIG_*_BINDING selected by SOCs.  This prevents enabling those options
    for boards which don't require EDK bindings.
    
    Make version of the bindings freely selectable when SOC doesn't request
    a specific version and EDK2 payload is used.
    
    This looks somewhat ugly and duplicated because otherwise kconfig will
    detect a loop in configuration options and refuse to do anything.
    
    Change-Id: I964142ef64967b4c9b329025ebcad701e34a5035
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>

--- src/vendorcode/intel/Makefile.mk
+++ src/vendorcode/intel/Makefile.mk
@@ -1,6 +1,6 @@
 ## SPDX-License-Identifier: GPL-2.0-only
 
-ifeq ($(CONFIG_UEFI_2_4_BINDING),y)
+ifeq ($(CONFIG_UEFI_2_4_BINDING)$(CONFIG_USE_UEFI_2_4_BINDING),y)
 # ProccessorBind.h provided in Ia32 directory. Types are derived from ia32.
 # It's possible to provide our own ProcessorBind.h using posix types. However,
 # ProcessorBind.h isn't just about types. There's compiler definitions as well
@@ -9,24 +9,24 @@ ifeq ($(CONFIG_UEFI_2_4_BINDING),y)
 CPPFLAGS_x86_32 += -I$(src)/vendorcode/intel/edk2/uefi_2.4/MdePkg/Include/Ia32
 CPPFLAGS_x86_64 += -I$(src)/vendorcode/intel/edk2/uefi_2.4/MdePkg/Include/X64
 CPPFLAGS_common += -I$(src)/vendorcode/intel/edk2/uefi_2.4/MdePkg/Include
-else ifeq ($(CONFIG_UDK_2017_BINDING),y)
+else ifeq ($(CONFIG_UDK_2017_BINDING)$(CONFIG_USE_UDK_2017_BINDING),y)
 CPPFLAGS_x86_32 += -I$(src)/vendorcode/intel/edk2/UDK2017/MdePkg/Include/Ia32
 CPPFLAGS_x86_64 += -I$(src)/vendorcode/intel/edk2/UDK2017/MdePkg/Include/X64
 CPPFLAGS_common += -I$(src)/vendorcode/intel/edk2/UDK2017/MdePkg/Include
 CPPFLAGS_common += -I$(src)/vendorcode/intel/edk2/UDK2017/IntelFsp2Pkg/Include
 CPPFLAGS_common += -I$(src)/vendorcode/intel/edk2/UDK2017/MdeModulePkg/Include
-else ifeq ($(CONFIG_UDK_202005_BINDING),y)
+else ifeq ($(CONFIG_UDK_202005_BINDING)$(CONFIG_USE_UDK_202005_BINDING),y)
 CPPFLAGS_x86_32 += -I$(src)/vendorcode/intel/edk2/edk2-stable202005/MdePkg/Include/Ia32
 CPPFLAGS_x86_64 += -I$(src)/vendorcode/intel/edk2/edk2-stable202005/MdePkg/Include/X64
 CPPFLAGS_common += -I$(src)/vendorcode/intel/edk2/edk2-stable202005/MdePkg/Include
 CPPFLAGS_common += -I$(src)/vendorcode/intel/edk2/edk2-stable202005/IntelFsp2Pkg/Include
 CPPFLAGS_common += -I$(src)/vendorcode/intel/edk2/edk2-stable202005/MdeModulePkg/Include
-else ifeq ($(CONFIG_UDK_202111_BINDING),y)
+else ifeq ($(CONFIG_UDK_202111_BINDING)$(CONFIG_USE_UDK_202111_BINDING),y)
 CPPFLAGS_x86_32 += -I$(src)/vendorcode/intel/edk2/edk2-stable202111/MdePkg/Include/Ia32
 CPPFLAGS_x86_64 += -I$(src)/vendorcode/intel/edk2/edk2-stable202111/MdePkg/Include/X64
 CPPFLAGS_common += -I$(src)/vendorcode/intel/edk2/edk2-stable202111/MdePkg/Include
 CPPFLAGS_common += -I$(src)/vendorcode/intel/edk2/edk2-stable202111/IntelFsp2Pkg/Include
-else ifeq ($(CONFIG_UDK_202302_BINDING),y)
+else ifeq ($(CONFIG_UDK_202302_BINDING)$(CONFIG_USE_UDK_202302_BINDING),y)
 CPPFLAGS_x86_32 += -I$(src)/vendorcode/intel/edk2/edk2-stable202302/MdePkg/Include/Ia32
 CPPFLAGS_x86_64 += -I$(src)/vendorcode/intel/edk2/edk2-stable202302/MdePkg/Include/X64
 CPPFLAGS_common += -I$(src)/vendorcode/intel/edk2/edk2-stable202302/MdePkg/Include
