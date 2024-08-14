b3dfc47aea0ec0c98df79b473d7715b5a2d5fa7f
Michał Kopeć <michal.kopec@3mdeb.com>
2024-08-14 16:15:15 +0200

    mb/novacustom/mtl-h: add dGPU ASL code
    Based on Google Brox / Hades boards, but heavily modified for our
    purposes.
    
    Upstream-Status: Pending
    Change-Id: I05ad1edfe4bc9c88bd75c3d043edca4ded1dbaef
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>

--- src/mainboard/novacustom/mtl-h/romstage.c
+++ src/mainboard/novacustom/mtl-h/romstage.c
@@ -27,6 +27,10 @@ void mainboard_memory_init_params(FSPM_UPD *mupd)
 		},
 	};
 
+	mainboard_configure_gpios();
+	mupd->FspmConfig.PrimaryDisplay = 0x0; // iGPU
+
+	/* The values come from Clevo firmware. */
 	mupd->FspmConfig.AcLoadline[0] = 190;
 	mupd->FspmConfig.AcLoadline[1] = 310;
 	mupd->FspmConfig.AcLoadline[2] = 490;
