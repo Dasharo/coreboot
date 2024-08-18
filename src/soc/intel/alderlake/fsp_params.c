abde7f1eadc972985e964f9b6270d555a239b126
Michał Żygowski <michal.zygowski@3mdeb.com>
2024-08-18 12:52:46 +0200

    soc/intel/{common,alderlake}: Add missing ADL-N SKUs
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/soc/intel/alderlake/fsp_params.c
+++ src/soc/intel/alderlake/fsp_params.c
@@ -539,6 +539,11 @@ static uint16_t get_vccin_aux_imon_iccmax(void)
 	case PCI_DID_INTEL_ADL_N_ID_2:
 	case PCI_DID_INTEL_ADL_N_ID_3:
 	case PCI_DID_INTEL_ADL_N_ID_4:
+	case PCI_DID_INTEL_ADL_N_ID_5:
+	case PCI_DID_INTEL_ADL_N_ID_6:
+	case PCI_DID_INTEL_ADL_N_ID_7:
+	case PCI_DID_INTEL_ADL_N_ID_8:
+	case PCI_DID_INTEL_ADL_N_ID_9:
 		return ICC_MAX_ID_ADL_N_MA;
 	case PCI_DID_INTEL_ADL_S_ID_1:
 	case PCI_DID_INTEL_ADL_S_ID_3:
