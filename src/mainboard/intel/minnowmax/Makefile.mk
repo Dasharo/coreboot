da19e339065cf48f2c26682b07418026799cf7c0
Filip Lewiński <filip.lewinski@3mdeb.com>
2024-08-20 10:55:28 +0200

    src/mainboard/intel/minnowmax/w25q64.c: add
    Required for proper BIOS lock functionality, implements
    mainboard_get_spi_config
    
    Signed-off-by: Filip Lewiński <filip.lewinski@3mdeb.com>

--- src/mainboard/intel/minnowmax/Makefile.mk
+++ src/mainboard/intel/minnowmax/Makefile.mk
@@ -2,6 +2,7 @@
 
 ramstage-y += gpio.c
 ramstage-y += irqroute.c
+ramstage-y += w25q64.c
 
 #SPD_SOURCES  = micron_1GiB_1066MHz_dimm_MT41K256M16HA-125 # 0b0000
 #SPD_SOURCES += micron_2GiB_1066MHz_dimm_MT41K256M16HA-125 # 0b0001
