53e4195bb30680b58b563f521090348636a2e5b0
Filip Lewiński <filip.lewinski@3mdeb.com>
2024-10-23 13:03:46 +0200

    mb/protectli/vault_adl_n/variants: Add VP2430 variant
    Change-Id: I2ad93b4c35c9f2483275fe484fcd82c590feb13f
    Upstream-Status: Pending
    Signed-off-by: Filip Lewiński <filip.lewinski@3mdeb.com>
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/mainboard/protectli/vault_adl_n/Makefile.mk
+++ src/mainboard/protectli/vault_adl_n/Makefile.mk
@@ -1,11 +1,16 @@
 ## SPDX-License-Identifier: GPL-2.0-only
 
+CPPFLAGS_common += -I$(src)/mainboard/$(MAINBOARDDIR)/include
+
 bootblock-y += bootblock.c
 
-romstage-y += gpio.c
+romstage-y += variants/$(VARIANT_DIR)/gpio.c
 romstage-y += romstage_fsp_params.c
 
 ramstage-y += mainboard.c
+ramstage-y += hda_verb.c
 
 all-y += die.c
 smm-y += die.c
+
+subdirs-y += variants/$(VARIANT_DIR)
