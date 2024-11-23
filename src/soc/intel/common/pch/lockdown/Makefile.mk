6789a121075d7f28624deaee97b58cdb3235fb2e
Michał Żygowski <michal.zygowski@3mdeb.com>
2024-11-23 22:43:10 +0100

    soc/intel/lockdown: Allow locking down SPI and LPC in SMM
    Heads payload uses APM_CNT_FINALIZE SMI to set and lock down the SPI
    controller with PR0 flash protection for pre-Skylake platforms.
    
    Add new option to skip LPC and FAST SPI lock down in coreboot and move
    it to APM_CNT_FINALIZE SMI handler. Reuse the INTEL_CHIPSET_LOCKDOWN
    option to prevent issuing APM_CNT_FINALIZE SMI on normal boot path,
    like it was done on pre-Skylake platforms. As the locking on modern
    SOCs became more complicated, separate the SPI and LPC locking into
    new modules to make linking to SMM easier.
    
    The expected configuration to leverage the feature is to unselect
    INTEL_CHIPSET_LOCKDOWN and select SOC_INTEL_COMMON_SPI_LOCKDOWN_SMM.
    
    Testing various microarchitectures happens on heads repository:
    https://github.com/linuxboot/heads/pull/1818
    
    TEST=Lock the SPI flash using APM_CNT_FINALIZE in heads on Alder Lake
    (Protectli VP66xx) and Comet Lake (Protectli VP46xx) platforms. Check
    if flash is unlocked in the heads recovery console. Check if flash is
    locked in the kexec'ed OS.
    
    Upstream-Status: Submitted [CB:85278]
    Change-Id: Icbcc6fcde90e5b0a999aacb720e2e3dc2748c838
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/soc/intel/common/pch/lockdown/Makefile.mk
+++ src/soc/intel/common/pch/lockdown/Makefile.mk
@@ -1,2 +1,7 @@
 ## SPDX-License-Identifier: GPL-2.0-only
 ramstage-$(CONFIG_SOC_INTEL_COMMON_PCH_LOCKDOWN) += lockdown.c
+ramstage-$(CONFIG_SOC_INTEL_COMMON_PCH_LOCKDOWN) += lockdown_lpc.c
+ramstage-$(CONFIG_SOC_INTEL_COMMON_PCH_LOCKDOWN) += lockdown_spi.c
+
+smm-$(CONFIG_SOC_INTEL_COMMON_SPI_LOCKDOWN_SMM) += lockdown_lpc.c
+smm-$(CONFIG_SOC_INTEL_COMMON_SPI_LOCKDOWN_SMM) += lockdown_spi.c
