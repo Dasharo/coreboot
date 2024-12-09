ab0f8bb91b57a2de749ccab65c5f54584cac4d0f
Filip Lewiński <filip.lewinski@3mdeb.com>
2024-12-09 18:18:08 +0100

    build.sh: correct FW_VERSION parsing for OptiPlex SeaBIOS/UEFI
    Change-Id: Ia479f99be923986af1d91eb0100a04858ee1f922
    Upstream-Status: Inappropriate [Dasharo downstream]
    Signed-off-by: Filip Lewiński <filip.lewinski@3mdeb.com>

--- build.sh
+++ build.sh
@@ -33,15 +33,26 @@ SDKVER="2024-02-18_732134932b"
 
 
 function build_optiplex_9010 {
-  local CONFIG=$1
-  local FW_VERSION=$2
+  DEFCONFIG=$1
+  # Determine FW flavor first (uefi or seabios)
+  if [[ ${DEFCONFIG} == *"uefi"* ]]; then
+    FW_FLAVOR="uefi"
+  else
+    FW_FLAVOR="seabios"
+  fi
+
+  # Get FW version
+  FW_VERSION=$(cat ${DEFCONFIG} | grep CONFIG_LOCALVERSION | cut -d '=' -f 2 | tr -d '"')
+  
+  # Combine FW flavor with version
+  FW_VERSION="${FW_FLAVOR}_${FW_VERSION}"
 
   docker run --rm -t -u $UID -v $PWD:/home/coreboot/coreboot \
     -v $HOME/.ssh:/home/coreboot/.ssh \
     -w /home/coreboot/coreboot coreboot/coreboot-sdk:$SDKVER \
     /bin/bash -c "make distclean"
 
-  cp "${CONFIG}" .config
+  cp "${DEFCONFIG}" .config
 
   git submodule update --init --checkout
 
@@ -326,11 +337,11 @@ case "$CMD" in
         ;;
     "optiplex_9010_uefi")
         BOARD="optiplex_9010"
-        build_optiplex_9010 "configs/config.dell_optiplex_9010_sff_uefi_txt" "v0.1.1_uefi"
+        build_optiplex_9010 "configs/config.dell_optiplex_9010_sff_uefi_txt"
         ;;
     "optiplex_9010_seabios")
         BOARD="optiplex_9010"
-        build_optiplex_9010 "configs/config.dell_optiplex_9010_sff_txt" "v0.1.0-rc1_seabios"
+        build_optiplex_9010 "configs/config.dell_optiplex_9010_sff_txt"
         ;;
     "qemu" | "QEMU" | "q35" | "Q35" )
         build_qemu
