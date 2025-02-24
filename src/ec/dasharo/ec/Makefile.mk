944540090eb1d055782ea3a6e1973d41d9f465c9
Michał Kopeć <michal.kopec@3mdeb.com>
2025-02-24 20:20:53 +0200

    ec/dasharo: apply EC firmware update in ramstage
    An update image is loaded into memory because it can be too large to fit
    into CBFS cache.
    
    An update is attempted several times, skipping writing sectors that are
    already in expected state.  A failure triggers vboot error and reboots.
    
    Upstream-Status: Pending
    Change-Id: I541337084f5f533dc4f100ffb9016e18222d2ab1
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/ec/dasharo/ec/Makefile.mk
+++ src/ec/dasharo/ec/Makefile.mk
@@ -6,4 +6,9 @@ all-y += dasharo_ec.c
 all-y += buttons.c
 smm-$(CONFIG_DEBUG_SMI) += dasharo_ec.c
 
+cbfs-files-$(CONFIG_EC_DASHARO_EC_UPDATE) += ec.rom
+ec.rom-file := $(call strip_quotes,$(CONFIG_EC_DASHARO_EC_UPDATE_FILE))
+ec.rom-compression := $(CBFS_COMPRESS_FLAG)
+ec.rom-type := raw
+
 endif
