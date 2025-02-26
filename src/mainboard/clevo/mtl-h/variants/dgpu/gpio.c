b3dd50bf55556dc4b3d1e4fa67a5a0d8d750b47d
Wiktor Mowinski <wiktor.mowinski@3mdeb.com>
2025-02-26 16:39:17 +0100

    src/mainboard/clevo/mtl-h/variants/dgpu/gpio.c: disable dgpu initialization
    Signed-off-by: Wiktor Mowinski <wiktor.mowinski@3mdeb.com>

--- src/mainboard/clevo/mtl-h/variants/dgpu/gpio.c
+++ src/mainboard/clevo/mtl-h/variants/dgpu/gpio.c
@@ -315,7 +315,7 @@ void mainboard_configure_gpios(void)
 	gpio_configure_pads(gpio_table, ARRAY_SIZE(gpio_table));
 
 	/* dGPU power on sequence */
-	mdelay(4);
+	/*mdelay(4);
 	gpio_set(DGPU_PWR_EN, 1);
 	result = wait_ms(200, gpio_get(DGPU_PWRGD) == 1);
 	if (result) {
@@ -325,5 +325,5 @@ void mainboard_configure_gpios(void)
 		printk(BIOS_ERR, "dGPU failed to power on, turning off\n");
 		gpio_set(DGPU_PWR_EN, 0);
 	}
-	mdelay(50);
+	mdelay(50);*/
 }
