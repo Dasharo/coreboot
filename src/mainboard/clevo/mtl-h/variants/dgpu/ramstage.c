df53268fffd7b9e153b678ddb6a9f1e72a1a90e5
Filip Lewiński <filip.lewinski@3mdeb.com>
2025-03-11 17:40:15 +0100

    mainboard/clevo/mtl-h/variants/dgpu/ramstage.c: add dgpu PCI device disable logic
    Signed-off-by: Filip Lewiński <filip.lewinski@3mdeb.com>

--- src/mainboard/clevo/mtl-h/variants/dgpu/ramstage.c
+++ src/mainboard/clevo/mtl-h/variants/dgpu/ramstage.c
@@ -2,10 +2,14 @@
 
 #include <mainboard/variants.h>
 #include <gpio.h>
+#include <dasharo/options.h>
 
 void variant_devtree_update(void)
 {
 	struct device *i2c_amp_dev = pcidev_on_root(0x19, 1);
 	bool have_smartamp = !gpio_get(GPP_E00);
 	i2c_amp_dev->enabled = have_smartamp;
+
+	struct device *dgpu_dev = pcidev_on_root(0x01, 0);
+	dgpu_dev->enabled = dasharo_is_dgpu_enabled();
 }
