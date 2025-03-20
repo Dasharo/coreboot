a14113b43913f49643166b3d9f817c3ed5af4497
Filip Lewiński <filip.lewinski@3mdeb.com>
2025-03-20 12:20:45 +0100

    mainboard/clevo/mtl-h/variants/dgpu/ramstage.c: fix possible null pointer dereference
    Signed-off-by: Filip Lewiński <filip.lewinski@3mdeb.com>

--- src/mainboard/clevo/mtl-h/variants/dgpu/ramstage.c
+++ src/mainboard/clevo/mtl-h/variants/dgpu/ramstage.c
@@ -11,12 +11,16 @@
 void variant_devtree_update(void)
 {
 	struct device *i2c_amp_dev = pcidev_on_root(0x19, 1);
-	bool have_smartamp = !gpio_get(GPP_E00);
-	i2c_amp_dev->enabled = have_smartamp;
+	if (i2c_amp_dev) {
+		bool have_smartamp = !gpio_get(GPP_E00);
+		i2c_amp_dev->enabled = have_smartamp;
+	}
 
 	struct device *dgpu_dev = pcidev_on_root(0x01, 0);
-	dgpu_dev->enabled = dasharo_is_dgpu_enabled() != 0;
-	printk(BIOS_DEBUG, "dgpu_dev->enabled: %d\n", dgpu_dev->enabled);
+	if (dgpu_dev) {
+	        dgpu_dev->enabled = dasharo_is_dgpu_enabled() != 0;
+	        printk(BIOS_DEBUG, "dgpu_dev->enabled: %d\n", dgpu_dev->enabled);
+	}
 }
 
 void variant_final(void)
