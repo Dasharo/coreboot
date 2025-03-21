c04157769fb2529e7118f8c289643a28502825c4
Jakub Redmerski <jakub.redmerski@3mdeb.com>
2025-03-21 17:43:12 +0100

    build.sh update: add novacustom laptops to build.sh
    Change-Id: Ied54e079fe913e724e5da822d1051dc340b29884
    Upstream-Status: Inappropriate [Dasharo downstream]
    Signed-off-by: Jakub Redmerski <jakub.redmerski@3mdeb.com>

--- build.sh
+++ build.sh
@@ -18,6 +18,14 @@ usage() {
   echo -e "\tV1210                  - build Dasharo for Protectli V1210"
   echo -e "\tV1410                  - build Dasharo for Protectli V1410"
   echo -e "\tV1610                  - build Dasharo for Protectli V1610"
+  echo -e "\tns5x_adl               - build Dasharo for Novacustom NS5x_ADL"
+  echo -e "\tns5x_tgl               - build Dasharo for Novacustom NS5x_TGL"
+  echo -e "\tnv4x_adl               - build Dasharo for Novacustom NV4x_ADL"
+  echo -e "\tnv4x_tgl               - build Dasharo for Novacustom NV4x_TGL"
+  echo -e "\tv540tnx                - build Dasharo for Novacustom V540TNx"
+  echo -e "\tv540tu                 - build Dasharo for Novacustom V540TU"
+  echo -e "\tv560tnx                - build Dasharo for Novacustom V560TNx"
+  echo -e "\tv560tu                 - build Dasharo for Novacustom V560TU"
   echo -e "\tapu2                   - build Dasharo for PC Engines APU2"
   echo -e "\tapu3                   - build Dasharo for PC Engines APU3"
   echo -e "\tapu4                   - build Dasharo for PC Engines APU4"
@@ -43,7 +51,7 @@ function build_optiplex_9010 {
 
   # Get FW version
   FW_VERSION=$(cat ${DEFCONFIG} | grep CONFIG_LOCALVERSION | cut -d '=' -f 2 | tr -d '"')
-  
+
   # Combine FW flavor with version
   FW_VERSION="${FW_FLAVOR}_${FW_VERSION}"
 
@@ -161,6 +169,87 @@ function build_v1x10 {
   if [ $? -eq 0 ]; then
     echo "Result binary placed in $PWD/protectli_$1_${FW_VERSION}.rom"
     sha256sum protectli_$1_${FW_VERSION}.rom > protectli_$1_${FW_VERSION}.rom.sha256
+  else
+    echo "Build failed!"
+    exit 1
+  fi
+}
+
+function build_novacustom {
+  DEFCONFIG="configs/config.novacustom_$1"
+  FW_VERSION=$(cat ${DEFCONFIG} | grep CONFIG_LOCALVERSION | cut -d '=' -f 2 | tr -d '"')
+
+  if [ ! -d 3rdparty/dasharo-blobs/novacustom ]; then
+    git submodule update --init --checkout
+  fi
+
+  docker run --rm -t -u $UID -v $PWD:/home/coreboot/coreboot \
+    -v $HOME/.ssh:/home/coreboot/.ssh \
+    -w /home/coreboot/coreboot coreboot/coreboot-sdk:$SDKVER \
+    /bin/bash -c "make distclean"
+
+  cp $DEFCONFIG .config
+
+  echo "Building Dasharo for Novacustom $1 (version $FW_VERSION)"
+
+  docker run --rm -t -u $UID -v $PWD:/home/coreboot/coreboot \
+    -v $HOME/.ssh:/home/coreboot/.ssh \
+    -w /home/coreboot/coreboot coreboot/coreboot-sdk:$SDKVER \
+    /bin/bash -c "make olddefconfig && make -j$(nproc)"
+
+  # Obtain LAN ROM blob from release binary
+  wget -O UEFIExtract_NE_A68_x64_linux.zip https://github.com/LongSoft/UEFITool/releases/download/A68/UEFIExtract_NE_A68_x64_linux.zip
+  unzip -o UEFIExtract_NE_A68_x64_linux.zip
+  wget -O novacustom_v54x_mtl_v0.9.0.rom https://dl.3mdeb.com/open-source-firmware/Dasharo/novacustom_v54x_mtl/v0.9.0/novacustom_v54x_mtl_v0.9.0.rom
+  cp build/coreboot.rom novacustom_$1_${FW_VERSION}.rom
+
+  if [ $? -eq 0 ]; then
+    echo "Result binary placed in $PWD/novacustom_$1_${FW_VERSION}.rom"
+    sha256sum novacustom_$1_${FW_VERSION}.rom > novacustom_$1_${FW_VERSION}.rom.sha256
+  else
+    echo "Build failed!"
+    exit 1
+  fi
+}
+
+
+function build_novacustom_v5x0tu {
+  DEFCONFIG="configs/config.novacustom_$1"
+  FW_VERSION=$(cat ${DEFCONFIG} | grep CONFIG_LOCALVERSION | cut -d '=' -f 2 | tr -d '"')
+
+  if [ ! -d 3rdparty/dasharo-blobs/novacustom ]; then
+    git submodule update --init --checkout
+  fi
+
+  docker run --rm -t -u $UID -v $PWD:/home/coreboot/coreboot \
+    -v $HOME/.ssh:/home/coreboot/.ssh \
+    -w /home/coreboot/coreboot coreboot/coreboot-sdk:$SDKVER \
+    /bin/bash -c "make distclean"
+
+  cp $DEFCONFIG .config
+
+  echo "Building Dasharo for Novacustom $1 (version $FW_VERSION)"
+
+  docker run --rm -t -u $UID -v $PWD:/home/coreboot/coreboot \
+    -v $HOME/.ssh:/home/coreboot/.ssh \
+    -w /home/coreboot/coreboot coreboot/coreboot-sdk:$SDKVER \
+    /bin/bash -c "make olddefconfig && make -j$(nproc)"
+
+  # Obtain LAN ROM blob from release binary
+  wget -O UEFIExtract_NE_A68_x64_linux.zip https://github.com/LongSoft/UEFITool/releases/download/A68/UEFIExtract_NE_A68_x64_linux.zip
+  unzip -o UEFIExtract_NE_A68_x64_linux.zip
+  wget -O novacustom_v54x_mtl_v0.9.0.rom https://dl.3mdeb.com/open-source-firmware/Dasharo/novacustom_v54x_mtl/v0.9.0/novacustom_v54x_mtl_v0.9.0.rom
+  cp build/coreboot.rom novacustom_$1_${FW_VERSION}.rom
+
+  # Extract and transfer LAN ROM blob
+  make -C util/cbfstool
+  util/cbfstool/cbfstool novacustom_v54x_mtl_v0.9.0.rom extract -r COREBOOT -f payload -n fallback/payload -m x86
+  ./uefiextract payload DEB917C0-C56A-4860-A05B-BF2F22EBB717
+  cp payload.dump/2\ 8C8CE578-8A3D-4F1C-9935-896185C32DD3/82\ DEB917C0-C56A-4860-A05B-BF2F22EBB717/1\ PE32\ image\ section/body.bin 3rdparty/blobs/mainboard/clevo/mtl-h/LanRom.efi
+
+  if [ $? -eq 0 ]; then
+    echo "Result binary placed in $PWD/novacustom_$1_${FW_VERSION}.rom"
+    sha256sum novacustom_$1_${FW_VERSION}.rom > novacustom_$1_${FW_VERSION}.rom.sha256
   else
     echo "Build failed!"
     exit 1
@@ -322,6 +411,38 @@ case "$CMD" in
         ;;
     "v1610" | "V1610" )
         build_v1x10 "v1610"
+        ;;
+    "v540tnx" | "V540TNx" | "V540TNX" )
+        BOARD="v540tnx"
+        build_novacustom "v540tnx"
+        ;;
+    "v560tnx" | "V560TNx" | "V560TNX" )
+        BOARD="v560tnx"
+        build_novacustom "v560tnx"
+        ;;
+    "nv4x_adl" | "NV4x_ADL" | "NV4X_ADL" )
+        BOARD="nv4x_adl"
+        build_novacustom "nv4x_adl"
+        ;;
+    "nv4x_tgl" | "NV4x_tgl" | "NV4X_TGL" )
+        BOARD="nv4x_tgl"
+        build_novacustom "nv4x_tgl"
+        ;;
+    "ns5x_adl" | "NS5x_ADL" | "NS5X_ADL" )
+        BOARD="ns5x_adl"
+        build_novacustom "ns5x_adl"
+        ;;
+    "ns5x_tgl" | "NS5x_TGL" | "NS5X_TGL" )
+        BOARD="ns5x_tgl"
+        build_novacustom "ns5x_tgl"
+        ;;
+    "v560tu" | "V560TU" )
+        BOARD="v560tu"
+        build_novacustom_v5x0tu "v560tu"
+        ;;
+    "v540tu " | "V540TU " )
+        BOARD="v540tu"
+        build_novacustom_v5x0tu "v540tu "
         ;;
     "apu2" | "APU2" )
         build_pcengines "apu2"
