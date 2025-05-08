73e5b8c9541b6522a0ca2928da2608036c4d6a21
Michał Kopeć <michal.kopec@3mdeb.com>
2025-05-08 13:04:14 +0200

    mb/{clevo,novacustom}: Add SMBIOS DIMM locators
    Upstream-Status: Pending
    Change-Id: Ie3aaa65b272f097c7fa0805dd254e8999a776561
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/mainboard/novacustom/mtl-h/variants/igpu/ramstage.c
+++ src/mainboard/novacustom/mtl-h/variants/igpu/ramstage.c
@@ -1,8 +1,19 @@
-/* SPDX-License-Identifier: CC-PDDC */
+/* SPDX-License-Identifier: GPL-2.0-only */
 
-/* Please update the license if adding licensable material. */
+#include <smbios.h>
 
-/*
- * This is a placeholder, if there ever is something variant specific to
- * add to ramstage, do it here.
- */
+void smbios_fill_dimm_locator(const struct dimm_info *dimm,
+	struct smbios_type17 *t)
+{
+	switch (dimm->ctrlr_num) {
+	case 0:
+		t->device_locator = smbios_add_string(t->eos, "RAM2");
+		break;
+	case 1:
+		t->device_locator = smbios_add_string(t->eos, "RAM1");
+		break;
+	default:
+		t->device_locator = smbios_add_string(t->eos, "UNKNOWN");
+		break;
+	}
+}
