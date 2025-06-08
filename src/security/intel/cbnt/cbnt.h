6594c698fdb94bdd2ebc511eea3d24c38900b3cd
Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
2025-06-08 21:26:28 +0300

    security/intel/cbnt: move union cbnt_biosacm_policy to header
    To make it accessible outside of logging.c file.
    
    Upstream-Status: Pending
    Change-Id: I3bd7fb126fb197c88bec933a88ded59232d520a1
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/security/intel/cbnt/cbnt.h
+++ src/security/intel/cbnt/cbnt.h
@@ -12,6 +12,30 @@
 #define CBNT_BIOSACM_ERRORCODE (CBNT_BASE_ADDRESS + 0x328)
 #define CBNT_BIOSACM_POLICY_STS (CBNT_BASE_ADDRESS + 0x378)
 
+union cbnt_biosacm_policy {
+	struct {
+		uint64_t km_id : 4;
+		uint64_t bp : 9; /* See CBNT_BP_* constants. */
+		uint64_t tpm_type : 2;
+		uint64_t tpm_success : 1;
+		uint64_t: 1;
+		uint64_t pfr : 1;
+		uint64_t bckup_act : 2;
+		uint64_t txt_profile : 5;
+		uint64_t scrub_policy : 2;
+		uint64_t: 2;
+		uint64_t dma_protection : 1;
+		uint64_t: 2;
+		uint64_t scrtm_status : 3;
+		uint64_t cpu_co_signing : 1;
+		uint64_t tpm_startup_locality : 1;
+		uint64_t: 27;
+	} status;
+	uint64_t raw;
+};
+_Static_assert(sizeof(union cbnt_biosacm_policy) == sizeof(uint64_t),
+	       "Wrong size of cbnt_biosacm_policy");
+
 void intel_cbnt_log_registers(void);
 
 #endif
