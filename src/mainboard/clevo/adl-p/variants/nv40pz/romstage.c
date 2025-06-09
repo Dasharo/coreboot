7f1ffc940812e6e575568bda9a66a0209e32a347
Michał Kopeć <michal.kopec@3mdeb.com>
2025-06-09 15:38:20 +0200

    mb/clevo/adl-p: fix up usage of CONFIG(INTEL_TXT) for guarding CBnT
    - Add missing comment to Clevo NS50PU board
    - Don't double-check CONFIG(INTEL_TXT) if it's already guarded by the
      preprocessor
    
    Upstream-Status: Pending
    Change-Id: If5b99655357f055a1b204d1c02c220f9b6a1fc7d
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>

--- src/mainboard/clevo/adl-p/variants/nv40pz/romstage.c
+++ src/mainboard/clevo/adl-p/variants/nv40pz/romstage.c
@@ -30,16 +30,14 @@ void mainboard_memory_init_params(FSPM_UPD *mupd)
 	size_t acm_size = 0;
 	uintptr_t acm_base;
 
-	if (CONFIG(INTEL_TXT)) {
-		intel_txt_log_spad();
+	intel_txt_log_spad();
 
-		if (CONFIG(INTEL_CBNT_LOGGING))
-			intel_cbnt_log_registers();
+	if (CONFIG(INTEL_CBNT_LOGGING))
+		intel_cbnt_log_registers();
 
-		if (CONFIG(INTEL_TXT_LOGGING)) {
-			intel_txt_log_bios_acm_error();
-			txt_dump_chipset_info();
-		}
+	if (CONFIG(INTEL_TXT_LOGGING)) {
+		intel_txt_log_bios_acm_error();
+		txt_dump_chipset_info();
 	}
 
 	acm_base = (uintptr_t)cbfs_map(CONFIG_INTEL_TXT_CBFS_BIOS_ACM, &acm_size);
