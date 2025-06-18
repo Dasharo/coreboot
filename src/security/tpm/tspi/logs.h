736ddd903d756ce5c3512c62c0dcc792c1004801
Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
2025-06-19 01:27:10 +0300

    security/intel/cbnt: synthesize PCR-7 measurement
    BootGuard/CbnT can optionally measure data into PCRs before passing
    control to BIOS which must be taken into account by coreboot for
    Measured Boot to work correctly.  This is done by constructing the data
    the same way BootGuard does, hashing it and creating an entry in the
    event log without extending the corresponding PCR.
    
    Extending PCR-7 is optional and breaks BitLocker on Windows 10, so may
    not be utilized.  However, it doesn't require large amount of code and
    might as well be implemented along with PCR-0 since both use the same
    data structures.
    
    Upstream-Status: Pending
    Change-Id: Ic1bfb016600f9f00b8f2fa1965aedfa99647a8e0
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/security/tpm/tspi/logs.h
+++ src/security/tpm/tspi/logs.h
@@ -70,4 +70,21 @@ static inline uint16_t tpm2_alg_from_vb2_hash(enum vb2_hash_algorithm hash_type)
 	}
 }
 
+static inline enum vb2_hash_algorithm tpm2_alg_to_vb2_hash(uint16_t hash_type)
+{
+	switch (hash_type) {
+	case TPM2_ALG_SHA1:
+		return VB2_HASH_SHA1;
+	case TPM2_ALG_SHA256:
+		return VB2_HASH_SHA256;
+	case TPM2_ALG_SHA384:
+		return VB2_HASH_SHA384;
+	case TPM2_ALG_SHA512:
+		return VB2_HASH_SHA512;
+
+	default:
+		return VB2_HASH_INVALID;
+	}
+}
+
 #endif /* LOGS_H_ */
