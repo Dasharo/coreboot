8a954efeda7aad57a22a71ee7fc15b9d5c3d2366
Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
2025-06-26 21:05:28 +0300

    arch/x86/include/arch/fit_table.h: add
    Extract the definition from src/cpu/intel/fit/fit_table.c, change first
    field to a union and add defines for types.  This provides a basis for
    parsing FIT data from coreboot.
    
    Upstream-Status: Pending
    Change-Id: If857e21864fd8be2f15a35c154e1f22802fafddd
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>

--- src/cpu/intel/fit/fit_table.c
+++ src/cpu/intel/fit/fit_table.c
@@ -1,21 +1,13 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
-#include <stdint.h>
-
-struct fit_entry {
-	uint8_t address[sizeof(uint64_t)];
-	uint32_t size_reserved;
-	uint16_t version;
-	uint8_t  type_checksum_valid;
-	uint8_t  checksum;
-} __packed;
+#include <arch/fit_table.h>
 
 __attribute((used)) static struct fit_entry fit_table[CONFIG_CPU_INTEL_NUM_FIT_ENTRIES + 1] = {
 	[0] = {
-		.address = {'_', 'F', 'I', 'T', '_', ' ', ' ', ' '},
+		.address.bytes = {'_', 'F', 'I', 'T', '_', ' ', ' ', ' '},
 		.size_reserved = 1,
 		.version = 0x100,
-		.type_checksum_valid = 0x80,
+		.type_checksum_valid = FIT_ENTRY_HAS_CHECKSUM | FIT_ENTRY_TYPE_HEADER,
 		.checksum = 0x7d,
 	}
 };
