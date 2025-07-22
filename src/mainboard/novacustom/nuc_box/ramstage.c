f6abc617c70b4994cd4cc0e66f87e023ef3cc40e
Filip Lewiński <filip.lewinski@3mdeb.com>
2025-07-22 14:28:42 +0200

    mb/novacustom/nuc_box/ramstage.c: enable S3 sleep
    Set the s0ix_enable parameter in accordance with chosen sleep mode.
    
    Upstream-Status: Pending
    Signed-off-by: Filip Lewiński <filip.lewinski@3mdeb.com>
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/mainboard/novacustom/nuc_box/ramstage.c
+++ src/mainboard/novacustom/nuc_box/ramstage.c
@@ -1,5 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <dasharo/options.h>
 #include <device/smbus_host.h>
 #include <mainboard/gpio.h>
 #include <soc/ramstage.h>
@@ -9,6 +10,14 @@ static void mainboard_init(void *chip_info)
 	// The DACC feature resets CMOS if the firmware does not send this message
 	printk(BIOS_DEBUG, "Handling DACC\n");
 	do_smbus_write_byte(CONFIG_FIXED_SMBUS_IO_BASE, 0xBA >> 1, 0x0F, 0xAA);
+}
+
+void mainboard_update_soc_chip_config(struct soc_intel_meteorlake_config *config)
+{
+	if (get_sleep_type_option() == SLEEP_TYPE_OPTION_S3)
+		config->s0ix_enable = 0;
+	else
+		config->s0ix_enable = 1;
 }
 
 struct chip_operations mainboard_ops = {
