8457e9c8c81be03dceb0ac6e67ec621dfb5d644d
Filip Lewiński <filip.lewinski@3mdeb.com>
2025-08-28 12:15:20 +0200

    src/mainboard/novacustom/mtl-h/variants/*/gpio.c: allow USB-PD EEPROM probing
    Set ROM_I2C_EN to expose the I2C USB-PD configuration EEPROM for probing
    within OS. This will allow later deployment of a patched binary to it,
    resolving USB-C power supply overdraw issues.
    
    Upstream-Status: Pending
    Change-Id: I6eb1e035fb2f19150ab02be6955df9ab1dcd9e44
    Signed-off-by: Filip Lewiński <filip.lewinski@3mdeb.com>

--- src/mainboard/novacustom/mtl-h/variants/igpu/gpio.c
+++ src/mainboard/novacustom/mtl-h/variants/igpu/gpio.c
@@ -398,8 +398,8 @@ static const struct pad_config gpio_table[] = {
 	PAD_CFG_GPO(GPP_B04, 0, DEEP),
 	/* GPP_B05 - GPIO */
 	PAD_CFG_GPO(GPP_B05, 0, DEEP),
-	/* GPP_B06 - GPIO */
-	PAD_CFG_GPO(GPP_B06, 0, DEEP),
+	/* GPP_B06 - GPIO (ROM_I2C_EN) */
+	PAD_CFG_GPO(GPP_B06, 1, DEEP),
 	/* GPP_B07 - GPIO */
 	PAD_CFG_GPO(GPP_B07, 0, DEEP),
 	/* GPP_B08 - GPIO */
