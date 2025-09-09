c641aa43069a8339ab314a0a409fa67161047c87
Michał Żygowski <michal.zygowski@3mdeb.com>
2025-09-09 11:32:58 +0200

    util/gigabyte/rbutool: Add a tool to pack coreboot as BMC BIOS update
    Gigabyte BMC can perform BIOS updates using RBU files. These files
    are raw BIOS images appended with #GBT#ROM magic string and a 16bit
    image checksum. No additional image verification required from BMC side
    to accept an image.
    
    TEST=Pack Gigabyte MZ33-AR1 coreboot image into RBU file and perform
    udpate via BMC.
    
    Change-Id: I91433239bfb08687c778e149a8da3b5cc55e7695
    Upstream-Status: CB:89116
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

---
+++ util/gigabyte/Makefile.mk
@@ -0,0 +1,17 @@
+## SPDX-License-Identifier: GPL-2.0-only
+TOOLCPPFLAGS += -include $(top)/src/commonlib/bsd/include/commonlib/bsd/compiler.h
+
+RBUTOOL:= $(objutil)/gigabyte/rbutool
+
+$(RBUTOOL): $(dir)/rbutool/rbutool.c
+	printf "    HOSTCC     Creating Gigabyte RBU tool\n"
+	mkdir -p $(objutil)/gigabyte
+	$(HOSTCC) $(TOOLCPPFLAGS) $< -o $@
+
+ifeq ($(CONFIG_VENDOR_GIGABYTE)$(CONFIG_HAVE_GIGABYTE_BMC_BIOS_UPDATE),yy)
+build_complete:: $(RBUTOOL)
+	printf "    TOOL       Creating RBU image\n"
+	cp $(obj)/coreboot.rom $(obj)/coreboot.rbu
+	$(RBUTOOL) -i $(obj)/coreboot.rbu
+
+endif
