74b839782ac7ca203c06c4f4540d62bfa1e6197f
Michał Żygowski <michal.zygowski@3mdeb.com>
2025-10-29 18:59:59 +0100

    acpi/acpigen_pci_root_resource_producer.c: Add VGA I/O resource to domain
    In multi-domain systems (mostly servers), the VGA device may not
    necessarily be located under domain 0. coreboot assumed that the I/O
    range 0x0000-0x0fff belong always to domain 0 where southbridge is
    located. However, VGA cycles, which fall into that range, may be routed
    to different root bridge/domain.
    
    Split the I/O resource, if it overlaps with the VGA I/O range 0x3b0-0x3df
    and the root bridge does not decode VGA. Then, add the VGA I/O resource
    to the root bridge which has the VGA decoding enabled in its bridge
    control register.
    
    Now the VGA resource is defined under the root bridge containing a VGA
    device and Windows does not complain on being unable to find resources
    for the VGA device.
    
    TEST=Windows does not show yellow bang for BMC VGA device on Gigabyte
    MZ33-AR1.
    
    Change-Id: If243808f417710055afb0177621680a78386d816
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/arch/x86/include/arch/vga.h
+++ src/arch/x86/include/arch/vga.h
@@ -3,6 +3,11 @@
 #ifndef ARCH_X86_VGA_H
 #define ARCH_X86_VGA_H
 
+/* VGA IO 0x3B0-0x3BB and 0x3C0-0x3DF */
+#define VGA_IO_BASE	0x3b0
+#define VGA_IO_SIZE	0x30
+#define VGA_IO_LIMIT	(VGA_IO_BASE + VGA_IO_SIZE - 1)
+
 /* VGA MMIO and SMM ASEG share the same address range */
 #define VGA_MMIO_BASE	0xa0000
 #define VGA_MMIO_SIZE	0x20000
