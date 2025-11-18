493151b926dc0f917500e5ee4277b8e424a1e222
Filip Lewiński <filip.lewinski@3mdeb.com>
2025-11-18 12:02:09 +0100

    soc/intel/alderlake/bootblock/bootblock.c: control Top Swap via CMOS option
    Toggle the RTC BUC control bit for Top Swap bootblock selection based on
    the "attempt_slot_b" flag NVRAM option, allowing to select which of the
    BOOTBLOCK or TOP_SWAP regions to boot from.
    
    This means that after an update, the CMOS option can be set to boot from
    the newer TOP_SWAP bootblock. In case of failure, CMOS can be cleared to
    revert to the known-good base BOOTBLOCK.
    
    This is part of ongoing implementation of a redundancy feature proposed
    on the mailing list:
    https://mail.coreboot.org/archives/list/coreboot@coreboot.org/thread/C6JN2PB7K7D67EG7OIKB6BBERZU5YV35/
    
    TEST=Boot VP6650, setting the attempt_slot_b flag to different values,
    observing that it resets/continues booting correctly.
    
    Change-Id: Ib183a1f72ee8585b2c4ad4376344de33ff54cbb9
    Signed-off-by: Filip Lewiński <filip.lewinski@3mdeb.com>

--- src/soc/intel/alderlake/bootblock/bootblock.c
+++ src/soc/intel/alderlake/bootblock/bootblock.c
@@ -1,11 +1,15 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <bootblock_common.h>
+#include <option.h>
 #include <intelblocks/fast_spi.h>
+#include <intelblocks/rtc.h>
 #include <intelblocks/systemagent.h>
 #include <intelblocks/tco.h>
 #include <intelblocks/uart.h>
+#include <reset.h>
 #include <soc/bootblock.h>
+#include <device/pci_ops.h>
 
 asmlinkage void bootblock_c_entry(uint64_t base_timestamp)
 {
@@ -23,6 +27,19 @@ void bootblock_soc_early_init(void)
 		uart_bootblock_init();
 }
 
+static void set_top_swap(void) {
+	uint8_t cmos_slotb_option, topswap_control_bit;
+	cmos_slotb_option = get_uint_option("attempt_slot_b", 0);
+	topswap_control_bit = get_rtc_buc_top_swap_status();
+	printk(BIOS_DEBUG, "Top Swap: CMOS option attempt_slot_b: %d\n", cmos_slotb_option);
+	printk(BIOS_DEBUG, "Top Swap: RTC BUC control bit: %d\n", topswap_control_bit);
+	if (cmos_slotb_option != topswap_control_bit) {
+		configure_rtc_buc_top_swap(cmos_slotb_option);
+		printk(BIOS_DEBUG, "Top Swap: RTC BUC control bit set to: %d, platform reset is necessary\n", get_rtc_buc_top_swap_status());
+		board_reset();
+	}
+}
+
 void bootblock_soc_init(void)
 {
 	report_platform_info();
@@ -30,4 +47,8 @@ void bootblock_soc_init(void)
 
 	/* Programming TCO_BASE_ADDRESS and TCO Timer Halt */
 	tco_configure();
+
+	if (CONFIG(INTEL_TOP_SWAP_OPTION_CONTROL)) {
+		set_top_swap();
+	}
 }
