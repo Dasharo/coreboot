85860ae0f967d323de374c8b39f7708e0e44fbd4
Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
2025-12-31 17:57:06 +0200

    lib/coreboot_table: report "disk capsules" boot in CBMEM
    Add a new CBMEM entry that informs payload about current boot.  For now,
    this is only to indicate that "disk capsules" boot is active and is
    needed because in EDK's early stages can't check for EFI variables.
    However, it's also nice to have a confirmation from coreboot that it
    processed the request.
    
    Change-Id: I6894409542dece959480d7f11784f8c00f069ee7
    Upstream-Status: Inappropriate [Dasharo downstream]
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/commonlib/include/commonlib/coreboot_tables.h
+++ src/commonlib/include/commonlib/coreboot_tables.h
@@ -92,6 +92,7 @@ enum {
 	LB_TAG_CFR_ROOT			= 0x0047,
 	LB_TAG_ROOT_BRIDGE_INFO		= 0x0048,
 	LB_TAG_LOGO			= 0x00a0,
+	LB_TAG_BOOT_INFO		= 0x00a1,
 	/* The following options are CMOS-related */
 	LB_TAG_CMOS_OPTION_TABLE	= 0x00c8,
 	LB_TAG_OPTION			= 0x00c9,
@@ -650,6 +651,16 @@ struct lb_boot_mode {
  */
 struct bootlogo_header {
 	uint64_t size;
+} __packed;
+
+/*
+ * Passing extra information about the current boot to payload.
+ */
+struct lb_boot_info {
+	uint32_t tag;
+	uint32_t size;
+	uint8_t is_disk_capsules_boot; /* Boolean. */
+	uint8_t pad[3];
 } __packed;
 
 #endif
