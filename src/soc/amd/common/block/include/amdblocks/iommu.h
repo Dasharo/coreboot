d76065f1be9719bb1c8d6119f159da8ae77343d6
Michał Żygowski <michal.zygowski@3mdeb.com>
2026-01-15 12:15:09 +0100

    soc/amd/common/block/iommu: Add support for IOMMU VF BARs
    Change-Id: Ifc376f4234c6ae3f8ade927529aa332b1e76adad
    Upstream-Status: Pending
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/soc/amd/common/block/include/amdblocks/iommu.h
+++ src/soc/amd/common/block/include/amdblocks/iommu.h
@@ -3,3 +3,9 @@
 #define IOMMU_CAP_BASE_LO	0x44
 #define   IOMMU_ENABLE		(1 << 0)
 #define IOMMU_CAP_BASE_HI	0x48
+
+#define IOMMU_VF_BASE_LO	0xd0
+#define IOMMU_VF_BASE_HI	0xd4
+
+#define IOMMU_VF_CNTL_BASE_LO	0xd8
+#define IOMMU_VF_CNTL_BASE_HI	0xdc
