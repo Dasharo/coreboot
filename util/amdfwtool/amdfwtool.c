273eacc5e467f859127ad30ef439e2e5443aa1ae
Michał Żygowski <michal.zygowski@3mdeb.com>
2026-01-15 12:20:56 +0100

    util/amdfwtool: Add support for defining SEV NVRAM storage
    Change-Id: Ib2444860efb72f4c7ffa9af36edaa0bb1b065e4a
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- util/amdfwtool/amdfwtool.c
+++ util/amdfwtool/amdfwtool.c
@@ -214,7 +214,7 @@ amd_fw_entry amd_psp_fw_table[] = {
 	{ .type = AMD_ABL5, .level = PSP_BOTH | PSP_LVL2_AB },
 	{ .type = AMD_ABL6, .level = PSP_BOTH | PSP_LVL2_AB },
 	{ .type = AMD_ABL7, .level = PSP_BOTH | PSP_LVL2_AB },
-	{ .type = AMD_SEV_DATA, .level = PSP_LVL2 | PSP_LVL2_AB },
+	{ .type = AMD_SEV_DATA, .level = PSP_BOTH | PSP_LVL2_AB },
 	{ .type = AMD_SEV_CODE, .level = PSP_LVL2 | PSP_LVL2_AB },
 	{ .type = AMD_FW_PSP_WHITELIST, .level = PSP_LVL2 | PSP_LVL2_AB },
 	{ .type = AMD_VBIOS_BTLOADER, .level = PSP_BOTH | PSP_LVL2_AB },
@@ -1176,6 +1176,7 @@ static void integrate_psp_firmwares(context *ctx,
 			pspdir->entries[count].address_mode = 0;
 			count++;
 		} else if (fw_table[i].type == AMD_FW_PSP_NVRAM ||
+		           fw_table[i].type == AMD_SEV_DATA ||
 			   fw_table[i].type == AMD_RPMC_NVRAM) {
 			if (fw_table[i].filename == NULL) {
 				if (fw_table[i].size == 0)
