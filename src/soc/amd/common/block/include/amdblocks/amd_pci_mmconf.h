ce33ba5c28de8db04498724551701dace22cd8c0
Michał Żygowski <michal.zygowski@3mdeb.com>
2026-03-19 14:55:02 +0100

    amdblocks/pci: Move syncing PCI MMCONF with DF to common code
    On Phoenix AM5 platform a similar problem to Turin platform occured.
    If coreboto set a different ECAM_MMCONF_BASE_ADDRESS than PSP in ABL,
    the PCI access did not work. Thankfully the SMN base address and DF
    registers are the same for Phoenix and Turin, which suggests that
    the synchronization of PCI MMCONF address in DF may be in common code.
    
    TEST=Set an arbitrary PCI MMCONF in coreboot above TOM address and see
    bootblock is still giving signs of life on serial console on MSI PRO
    B850-P.
    
    Change-Id: Ibf39bdba4dfd1c461bcd835f07118cf6b0fd1bb6
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/soc/amd/common/block/include/amdblocks/amd_pci_mmconf.h
+++ src/soc/amd/common/block/include/amdblocks/amd_pci_mmconf.h
@@ -3,6 +3,9 @@
 #ifndef AMD_BLOCK_PCI_MMCONF_H
 #define AMD_BLOCK_PCI_MMCONF_H
 
+#include <types.h>
+
 void enable_pci_mmconf(void);
+uint32_t soc_get_df_func0_smn_base(void);
 
 #endif /* AMD_BLOCK_PCI_MMCONF_H */
