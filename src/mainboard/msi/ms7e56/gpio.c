0abd644a346df3e93893318d2324d46168461fe2
Michał Żygowski <michal.zygowski@3mdeb.com>
2026-03-30 12:10:13 +0200

    mainboard/msi/ms7e56: Update mainboard code
    Add IRQ routing and GPIO configuration. Limit the coreboot
    to 16MB, because flashing whole 32MB is time consuming.
    
    Change-Id: I1255076f6d55a5e96a3d43217e2fc4f442d267e8
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/mainboard/msi/ms7e56/gpio.c
+++ src/mainboard/msi/ms7e56/gpio.c
@@ -3,6 +3,14 @@
 #include <gpio.h>
 #include "gpio.h"
 
+/* GPIO pins used by coreboot should be initialized in bootblock */
+
+static const struct soc_amd_gpio gpio_table[] = {
+	/* S0A3 */
+	PAD_NF(GPIO_10, S0A3_GPIO, PULL_NONE)
+};
+
 void mainboard_program_gpios(void)
 {
+	gpio_configure_pads(gpio_table, ARRAY_SIZE(gpio_table));
 }
