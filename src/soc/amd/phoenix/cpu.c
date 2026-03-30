8124f886e02216178266fd976851f9304588eed1
Michał Żygowski <michal.zygowski@3mdeb.com>
2026-03-30 12:01:08 +0200

    soc/amd/phoenix/cpu.c: Add PSP_BASE_MSR programming
    Program PSP_BASE_MSR on BSP if not yet programmed and if CCP MMIO
    has been allocated.
    
    Upstream-Status: Pending
    Change-Id: I2f5d9e2f47244b6bb71a472b145969f9b0d4b40a
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/soc/amd/phoenix/cpu.c
+++ src/soc/amd/phoenix/cpu.c
@@ -3,15 +3,36 @@
 /* TODO: Update for Phoenix */
 
 #include <amdblocks/cpu.h>
+#include <cpu/amd/msr.h>
 #include <cpu/cpu.h>
 #include <device/device.h>
+#include <smp/node.h>
+#include <soc/amd/common/block/psp/psp_def.h>
 #include <soc/cpu.h>
 
 _Static_assert(CONFIG_MAX_CPUS == 16, "Do not override MAX_CPUS. To reduce the number of "
 	"available cores, use the downcore_mode and disable_smt devicetree settings instead.");
 
+static void amd_phoenix_cpu_init(struct device *dev)
+{
+	msr_t ccp_addr_base;
+	uint64_t ccp_mmio_base;
+
+	if (boot_cpu()) {
+		ccp_addr_base = rdmsr(PSP_ADDR_MSR);
+		ccp_mmio_base = get_ccp_mmio_base();
+		if (ccp_addr_base.raw == 0ul && ccp_mmio_base != 0ul) {
+			ccp_addr_base.raw = ccp_mmio_base;
+			wrmsr(PSP_ADDR_MSR, ccp_addr_base);
+			printk(BIOS_SPEW, "Wrote PSP_ADDR_MSR 0x%llx to BSP\n", rdmsr(PSP_ADDR_MSR).raw);
+		}
+	}
+
+	amd_cpu_init(dev);
+}
+
 static struct device_operations cpu_dev_ops = {
-	.init = amd_cpu_init,
+	.init = amd_phoenix_cpu_init,
 };
 
 static struct cpu_device_id cpu_table[] = {
