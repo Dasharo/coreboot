f32f8c83655676a6b0254bfd2a2394e1055e8e5c
Michał Kopeć <michal.kopec@3mdeb.com>
2026-03-31 14:19:34 +0200

    src/mainboard/asrock/turind8ud-2t: enough to boot to ramstage
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>

--- src/mainboard/asrock/turind8ud-2t/bootblock.c
+++ src/mainboard/asrock/turind8ud-2t/bootblock.c
@@ -1,9 +1,30 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
-#include <amdblocks/spi.h>
+#include <amdblocks/espi.h>
 #include <amdblocks/lpc.h>
+#include <amdblocks/spi.h>
 #include <bootblock_common.h>
+#include <device/pnp_type.h>
 #include <gpio.h>
+#include <superio/aspeed/ast2400/ast2400.h>
+#include <superio/aspeed/common/aspeed.h>
+
+void bootblock_mainboard_early_init(void)
+{
+	/* Configure appropriate physical port of SuperIO chip off BMC */
+	const pnp_devfn_t serial1_dev = PNP_DEV(0x2e, AST2400_SUART1);
+
+	/*
+	 * APCBs are configured to enable 0xca2 and 0x3f8 ranges already.
+	 * Eable only SIO and post codes here.
+	 */
+	espi_open_io_window(0x2e, 2);
+	espi_open_io_window(0x80, 1);
+
+	aspeed_enable_serial(serial1_dev, 0x3f8);
+	/* Enable UART function pin */
+	aspeed_enable_uart_pin(serial1_dev);
+}
 
 static const struct soc_amd_gpio gpio_table[] = {
 	PAD_GPO(GPIO_3, LOW),
