d271c2d5b68c71ff0d41ec306573b0810721067e
Michał Kopeć <michal.kopec@3mdeb.com>
2026-04-01 13:11:49 +0200

    src/mainboard/asrock/turind8ud-2t: Serial port fixes
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>

--- src/mainboard/asrock/turind8ud-2t/bootblock.c
+++ src/mainboard/asrock/turind8ud-2t/bootblock.c
@@ -12,13 +12,13 @@
 void bootblock_mainboard_early_init(void)
 {
 	/* Configure appropriate physical port of SuperIO chip off BMC */
-	const pnp_devfn_t serial1_dev = PNP_DEV(0x2e, AST2400_SUART1);
+	const pnp_devfn_t serial1_dev = PNP_DEV(0x4e, AST2400_SUART1);
 
 	/*
 	 * APCBs are configured to enable 0xca2 and 0x3f8 ranges already.
 	 * Eable only SIO and post codes here.
 	 */
-	espi_open_io_window(0x2e, 2);
+	espi_open_io_window(0x4e, 2);
 	espi_open_io_window(0x80, 1);
 
 	aspeed_enable_serial(serial1_dev, 0x3f8);
