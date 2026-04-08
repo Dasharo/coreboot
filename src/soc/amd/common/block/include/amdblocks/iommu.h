ac208b00f5526f2f4f29988285aa28414bf59030
Michał Żygowski <michal.zygowski@3mdeb.com>
2026-04-08 14:30:22 +0200

    treewide: Fix lint checks
    Code developed for AMD boards had lint issues. Those will be fixed
    when they will be submitted upstream.
    
    Upstream-Status: Pending
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/soc/amd/common/block/include/amdblocks/iommu.h
+++ src/soc/amd/common/block/include/amdblocks/iommu.h
@@ -1,5 +1,8 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#ifndef AMD_BLOCK_IOMMU_H
+#define AMD_BLOCK_IOMMU_H
+
 #define IOMMU_CAP_BASE_LO	0x44
 #define   IOMMU_ENABLE		(1 << 0)
 #define IOMMU_CAP_BASE_HI	0x48
@@ -9,3 +12,5 @@
 
 #define IOMMU_VF_CNTL_BASE_LO	0xd8
 #define IOMMU_VF_CNTL_BASE_HI	0xdc
+
+#endif /* AMD_BLOCK_IOMMU_H */
