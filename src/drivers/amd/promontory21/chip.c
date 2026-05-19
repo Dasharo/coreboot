fe9912c1fd8e571a2c5d572bb8faadcec304d4eb
Michał Żygowski <michal.zygowski@3mdeb.com>
2026-05-19 11:50:49 +0200

    drivers/amd/promontory21: Add device_operations for PROM21 devices
    Assing ops for each device of Promontory chipset, so that proper
    ACPI code is generated on boot.
    
    Upstream-Status: Pending
    Change-Id: I018eb0ea45a386ae718b3a38716cfbe2043da55a
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/drivers/amd/promontory21/chip.c
+++ src/drivers/amd/promontory21/chip.c
@@ -4,7 +4,7 @@
 #include <acpi/acpi_device.h>
 #include <device/device.h>
 #include <device/pci.h>
-#include <device/pci_def.h>
+#include <device/pciexp.h>
 #include <static.h>
 
 #include "chip.h"
@@ -14,6 +14,25 @@ static const char *prom21_sata_acpi_name(const struct device *dev)
 	return "STCR";
 }
 
+static const char *prom21_usp_acpi_name(const struct device *dev)
+{
+	return "UP00";
+}
+
+static const char *prom21_dsp_acpi_name(const struct device *dev)
+{
+	char *name;
+
+	if (dev->path.type != DEVICE_PATH_PCI)
+		return NULL;
+
+	name = malloc(ACPI_NAME_BUFFER_SIZE);
+	snprintf(name, ACPI_NAME_BUFFER_SIZE, "DP%02X", dev->path.pci.devfn);
+	name[4] = '\0';
+
+	return name;
+}
+
 struct device_operations prom21_sata_ops = {
 	.read_resources		= pci_dev_read_resources,
 	.set_resources		= pci_dev_set_resources,
@@ -21,6 +40,32 @@ struct device_operations prom21_sata_ops = {
 	.ops_pci		= &pci_dev_ops_pci,
 	.acpi_name		= prom21_sata_acpi_name,
 	.acpi_fill_ssdt		= acpi_device_write_pci_dev,
+};
+
+static struct pci_operations prom21_pcie_ops = {
+	.set_subsystem		= pci_dev_set_subsystem,
+};
+
+struct device_operations amd_prom21_usp_ops = {
+	.read_resources		= pci_bus_read_resources,
+	.set_resources		= pci_dev_set_resources,
+	.enable_resources	= pci_bus_enable_resources,
+	.scan_bus		= pciexp_scan_bridge,
+	.reset_bus		= pci_bus_reset,
+	.acpi_name		= prom21_usp_acpi_name,
+	.acpi_fill_ssdt		= acpi_device_write_pci_dev,
+	.ops_pci		= &prom21_pcie_ops,
+};
+
+struct device_operations amd_prom21_dsp_ops = {
+	.read_resources		= pci_bus_read_resources,
+	.set_resources		= pci_dev_set_resources,
+	.enable_resources	= pci_bus_enable_resources,
+	.scan_bus		= pciexp_scan_bridge,
+	.reset_bus		= pci_bus_reset,
+	.acpi_name		= prom21_dsp_acpi_name,
+	.acpi_fill_ssdt		= acpi_device_write_pci_dev,
+	.ops_pci		= &prom21_pcie_ops,
 };
 
 struct chip_operations drivers_amd_promontory21_ops = {
