2f0f93681415d502dae77470dadbcb9240188020
Michał Żygowski <michal.zygowski@3mdeb.com>
2026-05-20 11:44:33 +0200

    mainboard/msi/ms7e56: Add MSI FlashBIOS support
    Add necessary regions, options and structures to the resutling image
    to make FlashBIOS work. Use the freshly added ROMHOLE as a source
    for the SMBIOS type 2 values.
    
    Upstream-Status: Pending
    Change-Id: If4fe27fc0701c4458d3a10bae68da4adfe8602fa
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/mainboard/msi/ms7e56/Makefile.mk
+++ src/mainboard/msi/ms7e56/Makefile.mk
@@ -4,6 +4,7 @@ bootblock-y += bootblock.c
 bootblock-y += early_gpio.c
 
 ramstage-y += gpio.c
+ramstage-y += smbios.c
 ramstage-y += update_devicetree.c
 
 ifneq ($(wildcard $(src)/mainboard/$(MAINBOARDDIR)/data*.apcb),)
@@ -13,3 +14,17 @@ APCB_SOURCES_68 = $(src)/mainboard/$(MAINBOARDDIR)/data_rec68.apcb
 else
 show_notices:: warn_no_apcb
 endif
+
+$(call src-to-obj,bootblock,$(dir)/msi_id.S): $(obj)/fmap_config.h $(obj)/build.h
+
+bootblock-y += msi_id.S
+
+$(obj)/msi_id.bin: $(obj)/bootblock/mainboard/$(MAINBOARDDIR)/msi_id.o
+	$(OBJCOPY_bootblock) -O binary $< $@
+
+# The MSI ID must be in the last sectors of the image for the MSI FlashBIOS to
+# detect it
+$(call add_intermediate, add_msi_id, $(obj)/msi_id.bin)
+	@printf "    WRITE MSI_ID\n"
+	$(CBFSTOOL) $< write -u -i 255 -r MSI_ID -f $(obj)/msi_id.bin
+	rm $(obj)/msi_id.bin
