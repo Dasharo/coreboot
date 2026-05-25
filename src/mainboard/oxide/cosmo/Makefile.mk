7fc768bca60bfc546da942ef88b8fec91f49875b
Michał Żygowski <michal.zygowski@3mdeb.com>
2026-05-25 17:28:13 +0200

    mb/oxide/cosmo/Makefile.mk: Fix adding APCB
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/mainboard/oxide/cosmo/Makefile.mk
+++ src/mainboard/oxide/cosmo/Makefile.mk
@@ -4,9 +4,9 @@ bootblock-y += bootblock.c
 
 ifneq ($(wildcard $(src)/mainboard/$(MAINBOARDDIR)/data.apcb),)
 ifeq ($(CONFIG_BUILD_WITH_DEBUG_APCB),y)
-APCB_SOURCES_68 = $(src)/mainboard/$(MAINBOARDDIR)/data_debug.apcb
+APCB_SOURCES_RECOVERY = $(src)/mainboard/$(MAINBOARDDIR)/data_debug.apcb
 else
-APCB_SOURCES_68 = $(src)/mainboard/$(MAINBOARDDIR)/data.apcb
+APCB_SOURCES_RECOVERY = $(src)/mainboard/$(MAINBOARDDIR)/data.apcb
 endif
 else
 show_notices:: warn_no_apcb
