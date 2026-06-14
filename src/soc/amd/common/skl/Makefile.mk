1ee244efcc4e9112c9025253c9633a25b1a74dae
Marty E. Plummer <hanetzer@startmail.com>
2026-06-14 10:22:37 -0500

    skl: build from submodule
    Modeled after BL31's code from arm64
    
    Signed-off-by: Marty E. Plummer <hanetzer@startmail.com>

--- src/soc/amd/common/skl/Makefile.mk
+++ src/soc/amd/common/skl/Makefile.mk
@@ -1,3 +1,24 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 ramstage-$(CONFIG_LAUNCH_DRTM_PAYLOAD) += skinit.c
+
+SKL_SOURCE := $(src)/../3rdparty/secure-kernel-loader
+SKL_TARGET := $(SKL_SOURCE)/skl.bin
+
+SKL := $(obj)/skl.bin
+
+$(SKL): $(obj)/build.h
+	printf "    MAKE       $(subst $(obj)/,,$(@))\n"
+	$(MAKE) -C $(SKL_SOURCE) $(SKL_MAKEARGS) CC=$(HOSTCC) DEBUG=y
+	mv $(SKL_TARGET) $@
+
+.PHONY: $(SKL)
+
+SKL_CBFS := $(CONFIG_CBFS_PREFIX)/drtm_payload
+$(SKL_CBFS)-file := $(SKL)
+$(SKL_CBFS)-type := raw
+$(SKL_CBFS)-compression := $(CBFS_PAYLOAD_COMPRESS_FLAG)
+$(SKL_CBFS)-options := $(ADDITIONAL_PAYLOAD_OPTIONS)
+cbfs-files-y += $(SKL_CBFS)
+
+check-ramstage-overlap-files += $(SKL_CBFS)
