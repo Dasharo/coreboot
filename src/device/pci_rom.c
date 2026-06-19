63851238e6f926845118fca929c7cc5ddd0779e9
Michał Żygowski <michal.zygowski@3mdeb.com>
2026-06-19 10:49:47 +0200

    device/pci_rom.c: Skip VFCT creation for AMD iGPU if EDK2 GOP enabled
    Skip creation of VFCT ACPI table if we enabled AMD GOP in EDK2. AMD GOP
    driver will publish the correct, patched VBIOS in the VFCT and add the
    table using the EFI ACPI protocol. Simply publishing the raw VBIOS will
    cause Linux amdgpu driver errors.
    
    TEST=Boot Linux on MSI PRO B850-P and see amdgpu driver initializes
    display properly.
    
    Upstream-Status: Pending
    Change-Id: I88f27d464bcc4f954becf16349dfe6fa23b3fde5
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/device/pci_rom.c
+++ src/device/pci_rom.c
@@ -329,6 +329,9 @@ pci_rom_write_acpi_tables(const struct device *device, unsigned long current,
 
 	/* AMD/ATI uses VFCT */
 	if (device->vendor == PCI_VID_ATI) {
+		if (CONFIG(EDK2_AMD_GOP_DRIVER) && device->on_mainboard)
+			return current;
+
 		acpi_vfct_t *vfct;
 
 		current = ALIGN_UP(current, 8);
