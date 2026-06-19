07e45727a7f102afbd3c2e4af807d3ae54a34925
Filip Lewiński <filip.lewinski@3mdeb.com>
2026-06-19 14:27:10 +0200

    mb/protectli/vault_adl_n: Add inital support for VP3210 and VP3230
    VP3210 and VP3230 are identical boards with different ADL-N SoC SKUs.
    
    Change-Id: I887d373dcd2f9e496d9f70735ef0b0b4aa623c98
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

---
+++ src/mainboard/protectli/vault_adl_n/include/variant/gpio.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef VARIANT_GPIO_H
+#define VARIANT_GPIO_H
+
+const struct pad_config *board_gpio_table(size_t *num);
+
+#endif /* VARIANT_GPIO_H */
