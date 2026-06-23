8dee0f082d06ca8db629422f8253dab46427e1e1
Marty E. Plummer <hanetzer@startmail.com>
2026-06-23 14:50:36 -0500

    amd/common: move skl here
    Signed-off-by: Marty E. Plummer <hanetzer@startmail.com>

--- src/cpu/amd/pi/Makefile.mk
+++ src/cpu/amd/pi/Makefile.mk
@@ -7,6 +7,4 @@ postcar-y += ../../../soc/amd/common/block/cpu/smm/smm_helper.c
 ramstage-y += ../../../soc/amd/common/block/cpu/smm/smm_helper.c
 ramstage-y += ../../../soc/amd/common/block/cpu/smm/smm_relocate.c
 
-ramstage-$(CONFIG_LAUNCH_DRTM_PAYLOAD) += skinit.c
-
 CPPFLAGS_common += -I$(src)/cpu/amd/pi/include
