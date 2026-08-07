d1210a11d750dd975f10f43564fb36259c166918
Michał Żygowski <michal.zygowski@3mdeb.com>
2026-08-07 16:49:12 +0200

    superio/nuvoton/nct6687d: Add driver for SMBus
    Add API to issue SMBus transactions via the NCT6687D chip. It will
    be used to communcate with various components on the board that
    contain this SIO chip.
    
    Upstream-Status: Pending
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/superio/nuvoton/nct6687d/Makefile.mk
+++ src/superio/nuvoton/nct6687d/Makefile.mk
@@ -4,5 +4,9 @@ bootblock-$(CONFIG_SUPERIO_NUVOTON_NCT6687D) += nct6687d_ec.c
 romstage-$(CONFIG_SUPERIO_NUVOTON_NCT6687D) += nct6687d_ec.c
 ramstage-$(CONFIG_SUPERIO_NUVOTON_NCT6687D) += nct6687d_ec.c
 
+bootblock-$(CONFIG_SUPERIO_NUVOTON_NCT6687D) += nct6687d_smbus.c
+romstage-$(CONFIG_SUPERIO_NUVOTON_NCT6687D) += nct6687d_smbus.c
+ramstage-$(CONFIG_SUPERIO_NUVOTON_NCT6687D) += nct6687d_smbus.c
+
 ramstage-$(CONFIG_SUPERIO_NUVOTON_NCT6687D) += nct6687d_hwm.c
 ramstage-$(CONFIG_SUPERIO_NUVOTON_NCT6687D) += superio.c
